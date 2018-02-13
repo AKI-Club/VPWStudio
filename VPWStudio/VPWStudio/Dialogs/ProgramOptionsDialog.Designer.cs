a75051c7d0f5b6d2254720e1039693a974a4c8cb
freem <aj187@ajworld.net>
2018-02-13 16:33:51 -0600

    fix program options dialog form settings

--- VPWStudio/VPWStudio/Dialogs/ProgramOptionsDialog.Designer.cs
+++ VPWStudio/VPWStudio/Dialogs/ProgramOptionsDialog.Designer.cs
@@ -39,20 +39,21 @@
 			// 
 			// buttonOK
 			// 
-			this.buttonOK.Location = new System.Drawing.Point(358, 67);
+			this.buttonOK.Location = new System.Drawing.Point(354, 66);
 			this.buttonOK.Name = "buttonOK";
 			this.buttonOK.Size = new System.Drawing.Size(75, 23);
-			this.buttonOK.TabIndex = 0;
+			this.buttonOK.TabIndex = 2;
 			this.buttonOK.Text = "&OK";
 			this.buttonOK.UseVisualStyleBackColor = true;
 			this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
 			// 
 			// buttonCancel
 			// 
-			this.buttonCancel.Location = new System.Drawing.Point(439, 67);
+			this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+			this.buttonCancel.Location = new System.Drawing.Point(435, 66);
 			this.buttonCancel.Name = "buttonCancel";
 			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
-			this.buttonCancel.TabIndex = 1;
+			this.buttonCancel.TabIndex = 3;
 			this.buttonCancel.Text = "&Cancel";
 			this.buttonCancel.UseVisualStyleBackColor = true;
 			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
@@ -62,7 +63,7 @@
 			this.tableLayoutPanel1.ColumnCount = 3;
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 19.38534F));
 			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80.61465F));
-			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 78F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 79F));
 			this.tableLayoutPanel1.Controls.Add(this.labelEmuPath, 0, 0);
 			this.tableLayoutPanel1.Controls.Add(this.tbEmuPath, 1, 0);
 			this.tableLayoutPanel1.Controls.Add(this.buttonBrowse, 2, 0);
@@ -70,47 +71,51 @@
 			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
 			this.tableLayoutPanel1.RowCount = 1;
 			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-			this.tableLayoutPanel1.Size = new System.Drawing.Size(502, 46);
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(498, 35);
 			this.tableLayoutPanel1.TabIndex = 2;
 			// 
 			// labelEmuPath
 			// 
 			this.labelEmuPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.labelEmuPath.AutoSize = true;
-			this.labelEmuPath.Location = new System.Drawing.Point(3, 16);
+			this.labelEmuPath.Location = new System.Drawing.Point(3, 11);
 			this.labelEmuPath.Name = "labelEmuPath";
-			this.labelEmuPath.Size = new System.Drawing.Size(76, 13);
+			this.labelEmuPath.Size = new System.Drawing.Size(75, 13);
 			this.labelEmuPath.TabIndex = 0;
 			this.labelEmuPath.Text = "&Emulator Path";
 			// 
 			// tbEmuPath
 			// 
 			this.tbEmuPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.tbEmuPath.Location = new System.Drawing.Point(85, 13);
+			this.tbEmuPath.Location = new System.Drawing.Point(84, 7);
 			this.tbEmuPath.Name = "tbEmuPath";
-			this.tbEmuPath.Size = new System.Drawing.Size(335, 20);
-			this.tbEmuPath.TabIndex = 1;
+			this.tbEmuPath.Size = new System.Drawing.Size(331, 20);
+			this.tbEmuPath.TabIndex = 0;
 			// 
 			// buttonBrowse
 			// 
 			this.buttonBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.buttonBrowse.Location = new System.Drawing.Point(426, 11);
+			this.buttonBrowse.Location = new System.Drawing.Point(421, 6);
 			this.buttonBrowse.Name = "buttonBrowse";
-			this.buttonBrowse.Size = new System.Drawing.Size(73, 23);
-			this.buttonBrowse.TabIndex = 2;
+			this.buttonBrowse.Size = new System.Drawing.Size(74, 23);
+			this.buttonBrowse.TabIndex = 1;
 			this.buttonBrowse.Text = "&Browse...";
 			this.buttonBrowse.UseVisualStyleBackColor = true;
 			this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
 			// 
 			// ProgramOptionsDialog
 			// 
+			this.AcceptButton = this.buttonOK;
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(526, 102);
+			this.CancelButton = this.buttonCancel;
+			this.ClientSize = new System.Drawing.Size(522, 101);
 			this.Controls.Add(this.tableLayoutPanel1);
 			this.Controls.Add(this.buttonCancel);
 			this.Controls.Add(this.buttonOK);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
+			this.MinimizeBox = false;
 			this.Name = "ProgramOptionsDialog";
 			this.Text = "Program Options";
 			this.tableLayoutPanel1.ResumeLayout(false);
