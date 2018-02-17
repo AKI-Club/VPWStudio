bb2a7d3de1a47bd53227e394e28bbd409452ee37
freem <aj187@ajworld.net>
2018-02-17 17:22:07 -0600

    WIP No Mercy wrestler edit control

---
+++ VPWStudio/VPWStudio/Controls/WrestlerEdit_NoMercy.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace VPWStudio.Controls
+{
+	partial class WrestlerEdit_NoMercy
+	{
+		/// <summary> 
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary> 
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Component Designer generated code
+
+		/// <summary> 
+		/// Required method for Designer support - do not modify 
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.labelWrestlerID4 = new System.Windows.Forms.Label();
+			this.labelWrestlerID2 = new System.Windows.Forms.Label();
+			this.labelThemeMusic = new System.Windows.Forms.Label();
+			this.labelNameCall = new System.Windows.Forms.Label();
+			this.labelHeight = new System.Windows.Forms.Label();
+			this.labelWeight = new System.Windows.Forms.Label();
+			this.labelMovesetIndex = new System.Windows.Forms.Label();
+			this.labelParamsIndex = new System.Windows.Forms.Label();
+			this.labelAppearanceIndex = new System.Windows.Forms.Label();
+			this.labelProfileIndex = new System.Windows.Forms.Label();
+			this.tbWrestlerID4 = new System.Windows.Forms.TextBox();
+			this.tbWrestlerID2 = new System.Windows.Forms.TextBox();
+			this.cbThemeMusic = new System.Windows.Forms.ComboBox();
+			this.cbEntranceVideo = new System.Windows.Forms.ComboBox();
+			this.nudHeight = new System.Windows.Forms.NumericUpDown();
+			this.nudWeight = new System.Windows.Forms.NumericUpDown();
+			this.tbMovesetIndex = new System.Windows.Forms.TextBox();
+			this.tbParamsIndex = new System.Windows.Forms.TextBox();
+			this.tbAppearanceIndex = new System.Windows.Forms.TextBox();
+			this.tbProfileIndex = new System.Windows.Forms.TextBox();
+			this.labelUnknown = new System.Windows.Forms.Label();
+			this.tbUnknown = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudWeight)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+			this.tableLayoutPanel1.Controls.Add(this.tbUnknown, 1, 5);
+			this.tableLayoutPanel1.Controls.Add(this.labelWrestlerID4, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.labelWrestlerID2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.labelThemeMusic, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.labelNameCall, 0, 3);
+			this.tableLayoutPanel1.Controls.Add(this.labelHeight, 0, 4);
+			this.tableLayoutPanel1.Controls.Add(this.tbWrestlerID4, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.tbWrestlerID2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.cbThemeMusic, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.cbEntranceVideo, 1, 3);
+			this.tableLayoutPanel1.Controls.Add(this.nudHeight, 1, 4);
+			this.tableLayoutPanel1.Controls.Add(this.tbParamsIndex, 1, 8);
+			this.tableLayoutPanel1.Controls.Add(this.labelParamsIndex, 0, 8);
+			this.tableLayoutPanel1.Controls.Add(this.labelMovesetIndex, 0, 7);
+			this.tableLayoutPanel1.Controls.Add(this.tbMovesetIndex, 1, 7);
+			this.tableLayoutPanel1.Controls.Add(this.labelWeight, 0, 6);
+			this.tableLayoutPanel1.Controls.Add(this.nudWeight, 1, 6);
+			this.tableLayoutPanel1.Controls.Add(this.labelAppearanceIndex, 0, 9);
+			this.tableLayoutPanel1.Controls.Add(this.tbAppearanceIndex, 1, 9);
+			this.tableLayoutPanel1.Controls.Add(this.tbProfileIndex, 1, 10);
+			this.tableLayoutPanel1.Controls.Add(this.labelProfileIndex, 0, 10);
+			this.tableLayoutPanel1.Controls.Add(this.labelUnknown, 0, 5);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 11;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.09091F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(444, 294);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// labelWrestlerID4
+			// 
+			this.labelWrestlerID4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWrestlerID4.AutoSize = true;
+			this.labelWrestlerID4.Location = new System.Drawing.Point(3, 6);
+			this.labelWrestlerID4.Name = "labelWrestlerID4";
+			this.labelWrestlerID4.Size = new System.Drawing.Size(127, 13);
+			this.labelWrestlerID4.TabIndex = 0;
+			this.labelWrestlerID4.Text = "Wrestler ID4";
+			// 
+			// labelWrestlerID2
+			// 
+			this.labelWrestlerID2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWrestlerID2.AutoSize = true;
+			this.labelWrestlerID2.Location = new System.Drawing.Point(3, 32);
+			this.labelWrestlerID2.Name = "labelWrestlerID2";
+			this.labelWrestlerID2.Size = new System.Drawing.Size(127, 13);
+			this.labelWrestlerID2.TabIndex = 1;
+			this.labelWrestlerID2.Text = "Wrestler ID2";
+			// 
+			// labelThemeMusic
+			// 
+			this.labelThemeMusic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelThemeMusic.AutoSize = true;
+			this.labelThemeMusic.Location = new System.Drawing.Point(3, 58);
+			this.labelThemeMusic.Name = "labelThemeMusic";
+			this.labelThemeMusic.Size = new System.Drawing.Size(127, 13);
+			this.labelThemeMusic.TabIndex = 2;
+			this.labelThemeMusic.Text = "Theme Music";
+			// 
+			// labelNameCall
+			// 
+			this.labelNameCall.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelNameCall.AutoSize = true;
+			this.labelNameCall.Location = new System.Drawing.Point(3, 84);
+			this.labelNameCall.Name = "labelNameCall";
+			this.labelNameCall.Size = new System.Drawing.Size(127, 13);
+			this.labelNameCall.TabIndex = 3;
+			this.labelNameCall.Text = "Entrance Video";
+			// 
+			// labelHeight
+			// 
+			this.labelHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelHeight.AutoSize = true;
+			this.labelHeight.Location = new System.Drawing.Point(3, 110);
+			this.labelHeight.Name = "labelHeight";
+			this.labelHeight.Size = new System.Drawing.Size(127, 13);
+			this.labelHeight.TabIndex = 4;
+			this.labelHeight.Text = "Height (cm)";
+			// 
+			// labelWeight
+			// 
+			this.labelWeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWeight.AutoSize = true;
+			this.labelWeight.Location = new System.Drawing.Point(3, 162);
+			this.labelWeight.Name = "labelWeight";
+			this.labelWeight.Size = new System.Drawing.Size(127, 13);
+			this.labelWeight.TabIndex = 5;
+			this.labelWeight.Text = "Weight (kg)";
+			// 
+			// labelMovesetIndex
+			// 
+			this.labelMovesetIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelMovesetIndex.AutoSize = true;
+			this.labelMovesetIndex.Location = new System.Drawing.Point(3, 188);
+			this.labelMovesetIndex.Name = "labelMovesetIndex";
+			this.labelMovesetIndex.Size = new System.Drawing.Size(127, 13);
+			this.labelMovesetIndex.TabIndex = 8;
+			this.labelMovesetIndex.Text = "Moveset File Index";
+			// 
+			// labelParamsIndex
+			// 
+			this.labelParamsIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelParamsIndex.AutoSize = true;
+			this.labelParamsIndex.Location = new System.Drawing.Point(3, 214);
+			this.labelParamsIndex.Name = "labelParamsIndex";
+			this.labelParamsIndex.Size = new System.Drawing.Size(127, 13);
+			this.labelParamsIndex.TabIndex = 9;
+			this.labelParamsIndex.Text = "Params File Index";
+			// 
+			// labelAppearanceIndex
+			// 
+			this.labelAppearanceIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelAppearanceIndex.AutoSize = true;
+			this.labelAppearanceIndex.Location = new System.Drawing.Point(3, 240);
+			this.labelAppearanceIndex.Name = "labelAppearanceIndex";
+			this.labelAppearanceIndex.Size = new System.Drawing.Size(127, 13);
+			this.labelAppearanceIndex.TabIndex = 10;
+			this.labelAppearanceIndex.Text = "Appearance Index";
+			// 
+			// labelProfileIndex
+			// 
+			this.labelProfileIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelProfileIndex.AutoSize = true;
+			this.labelProfileIndex.Location = new System.Drawing.Point(3, 270);
+			this.labelProfileIndex.Name = "labelProfileIndex";
+			this.labelProfileIndex.Size = new System.Drawing.Size(127, 13);
+			this.labelProfileIndex.TabIndex = 11;
+			this.labelProfileIndex.Text = "Profile Index";
+			// 
+			// tbWrestlerID4
+			// 
+			this.tbWrestlerID4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestlerID4.Location = new System.Drawing.Point(136, 3);
+			this.tbWrestlerID4.Name = "tbWrestlerID4";
+			this.tbWrestlerID4.ReadOnly = true;
+			this.tbWrestlerID4.Size = new System.Drawing.Size(305, 20);
+			this.tbWrestlerID4.TabIndex = 12;
+			// 
+			// tbWrestlerID2
+			// 
+			this.tbWrestlerID2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestlerID2.Location = new System.Drawing.Point(136, 29);
+			this.tbWrestlerID2.Name = "tbWrestlerID2";
+			this.tbWrestlerID2.ReadOnly = true;
+			this.tbWrestlerID2.Size = new System.Drawing.Size(305, 20);
+			this.tbWrestlerID2.TabIndex = 13;
+			// 
+			// cbThemeMusic
+			// 
+			this.cbThemeMusic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbThemeMusic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbThemeMusic.FormattingEnabled = true;
+			this.cbThemeMusic.Items.AddRange(new object[] {
+            "00 None"});
+			this.cbThemeMusic.Location = new System.Drawing.Point(136, 55);
+			this.cbThemeMusic.Name = "cbThemeMusic";
+			this.cbThemeMusic.Size = new System.Drawing.Size(305, 21);
+			this.cbThemeMusic.TabIndex = 14;
+			// 
+			// cbEntranceVideo
+			// 
+			this.cbEntranceVideo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbEntranceVideo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbEntranceVideo.FormattingEnabled = true;
+			this.cbEntranceVideo.Items.AddRange(new object[] {
+            "00 None"});
+			this.cbEntranceVideo.Location = new System.Drawing.Point(136, 81);
+			this.cbEntranceVideo.Name = "cbEntranceVideo";
+			this.cbEntranceVideo.Size = new System.Drawing.Size(305, 21);
+			this.cbEntranceVideo.TabIndex = 15;
+			// 
+			// nudHeight
+			// 
+			this.nudHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.nudHeight.Location = new System.Drawing.Point(136, 107);
+			this.nudHeight.Name = "nudHeight";
+			this.nudHeight.Size = new System.Drawing.Size(305, 20);
+			this.nudHeight.TabIndex = 18;
+			// 
+			// nudWeight
+			// 
+			this.nudWeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.nudWeight.Location = new System.Drawing.Point(136, 159);
+			this.nudWeight.Name = "nudWeight";
+			this.nudWeight.Size = new System.Drawing.Size(305, 20);
+			this.nudWeight.TabIndex = 19;
+			// 
+			// tbMovesetIndex
+			// 
+			this.tbMovesetIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbMovesetIndex.Location = new System.Drawing.Point(136, 185);
+			this.tbMovesetIndex.Name = "tbMovesetIndex";
+			this.tbMovesetIndex.ReadOnly = true;
+			this.tbMovesetIndex.Size = new System.Drawing.Size(305, 20);
+			this.tbMovesetIndex.TabIndex = 20;
+			// 
+			// tbParamsIndex
+			// 
+			this.tbParamsIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbParamsIndex.Location = new System.Drawing.Point(136, 211);
+			this.tbParamsIndex.Name = "tbParamsIndex";
+			this.tbParamsIndex.ReadOnly = true;
+			this.tbParamsIndex.Size = new System.Drawing.Size(305, 20);
+			this.tbParamsIndex.TabIndex = 21;
+			// 
+			// tbAppearanceIndex
+			// 
+			this.tbAppearanceIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbAppearanceIndex.Location = new System.Drawing.Point(136, 237);
+			this.tbAppearanceIndex.Name = "tbAppearanceIndex";
+			this.tbAppearanceIndex.ReadOnly = true;
+			this.tbAppearanceIndex.Size = new System.Drawing.Size(305, 20);
+			this.tbAppearanceIndex.TabIndex = 22;
+			// 
+			// tbProfileIndex
+			// 
+			this.tbProfileIndex.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbProfileIndex.Location = new System.Drawing.Point(136, 267);
+			this.tbProfileIndex.Name = "tbProfileIndex";
+			this.tbProfileIndex.ReadOnly = true;
+			this.tbProfileIndex.Size = new System.Drawing.Size(305, 20);
+			this.tbProfileIndex.TabIndex = 23;
+			// 
+			// labelUnknown
+			// 
+			this.labelUnknown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelUnknown.AutoSize = true;
+			this.labelUnknown.Location = new System.Drawing.Point(3, 136);
+			this.labelUnknown.Name = "labelUnknown";
+			this.labelUnknown.Size = new System.Drawing.Size(127, 13);
+			this.labelUnknown.TabIndex = 24;
+			this.labelUnknown.Text = "Unknown";
+			// 
+			// tbUnknown
+			// 
+			this.tbUnknown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbUnknown.Location = new System.Drawing.Point(136, 133);
+			this.tbUnknown.Name = "tbUnknown";
+			this.tbUnknown.ReadOnly = true;
+			this.tbUnknown.Size = new System.Drawing.Size(305, 20);
+			this.tbUnknown.TabIndex = 25;
+			// 
+			// WrestlerEdit_NoMercy
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Name = "WrestlerEdit_NoMercy";
+			this.Size = new System.Drawing.Size(450, 300);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudWeight)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label labelWrestlerID4;
+		private System.Windows.Forms.Label labelWrestlerID2;
+		private System.Windows.Forms.Label labelThemeMusic;
+		private System.Windows.Forms.Label labelNameCall;
+		private System.Windows.Forms.Label labelHeight;
+		private System.Windows.Forms.Label labelWeight;
+		private System.Windows.Forms.Label labelMovesetIndex;
+		private System.Windows.Forms.Label labelParamsIndex;
+		private System.Windows.Forms.Label labelAppearanceIndex;
+		private System.Windows.Forms.Label labelProfileIndex;
+		private System.Windows.Forms.TextBox tbWrestlerID4;
+		private System.Windows.Forms.TextBox tbWrestlerID2;
+		private System.Windows.Forms.ComboBox cbThemeMusic;
+		private System.Windows.Forms.ComboBox cbEntranceVideo;
+		private System.Windows.Forms.NumericUpDown nudHeight;
+		private System.Windows.Forms.NumericUpDown nudWeight;
+		private System.Windows.Forms.TextBox tbMovesetIndex;
+		private System.Windows.Forms.TextBox tbParamsIndex;
+		private System.Windows.Forms.TextBox tbAppearanceIndex;
+		private System.Windows.Forms.TextBox tbProfileIndex;
+		private System.Windows.Forms.TextBox tbUnknown;
+		private System.Windows.Forms.Label labelUnknown;
+	}
+}
---
+++ VPWStudio/VPWStudio/Controls/WrestlerEdit_NoMercy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio.Controls
+{
+	public partial class WrestlerEdit_NoMercy : UserControl
+	{
+		public GameSpecific.NoMercy.WrestlerDefinition CurWrestlerDef;
+
+		public WrestlerEdit_NoMercy()
+		{
+			InitializeComponent();
+		}
+
+		public void LoadData(GameSpecific.NoMercy.WrestlerDefinition wdef)
+		{
+			this.CurWrestlerDef = wdef;
+
+			tbWrestlerID4.Text = String.Format("{0:X4}", this.CurWrestlerDef.WrestlerID4);
+			tbWrestlerID2.Text = String.Format("{0:X2}", this.CurWrestlerDef.WrestlerID2);
+			//cbThemeMusic.SelectedIndex = this.CurWrestlerDef.ThemeSong;
+			//cbEntranceVideo.SelectedIndex = this.CurWrestlerDef.EntranceVideo;
+			tbUnknown.Text = String.Format("0x{0:X2}", wdef.Unknown);
+			tbMovesetIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.MovesetFileIndex);
+			tbParamsIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.ParamsFileIndex);
+			tbAppearanceIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.AppearanceIndex);
+			tbProfileIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.ProfileIndex);
+		}
+	}
+}
