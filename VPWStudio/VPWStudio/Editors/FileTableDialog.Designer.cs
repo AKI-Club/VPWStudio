e596e74dab3a3abfc63561c8cf4d67b7bbe92369
freem <aj187@ajworld.net>
2018-02-17 03:49:26 -0600

    add FileTableDialog

---
+++ VPWStudio/VPWStudio/Editors/FileTableDialog.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace VPWStudio
+{
+	partial class FileTableDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tbInfoDump = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// tbInfoDump
+			// 
+			this.tbInfoDump.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbInfoDump.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tbInfoDump.Location = new System.Drawing.Point(12, 12);
+			this.tbInfoDump.Multiline = true;
+			this.tbInfoDump.Name = "tbInfoDump";
+			this.tbInfoDump.ReadOnly = true;
+			this.tbInfoDump.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.tbInfoDump.Size = new System.Drawing.Size(480, 333);
+			this.tbInfoDump.TabIndex = 0;
+			// 
+			// FileTableDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(504, 357);
+			this.Controls.Add(this.tbInfoDump);
+			this.Name = "FileTableDialog";
+			this.Text = "File Table";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbInfoDump;
+	}
+}
---
+++ VPWStudio/VPWStudio/Editors/FileTableDialog.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio
+{
+	public partial class FileTableDialog : Form
+	{
+		protected FileTable CurrentFileTable;
+
+		public FileTableDialog()
+		{
+			InitializeComponent();
+
+			if (Program.CurrentProject != null)
+			{
+				switch (Program.CurrentProject.Settings.BaseGame)
+				{
+					case VPWGames.VPW2:
+						LoadFileTable_VPW2();
+						UpdateInfo_VPW2();
+						break;
+					default:
+						MessageBox.Show(String.Format("not implemented for {0}", Program.CurrentProject.Settings.BaseGame));
+						break;
+				}
+			}
+		}
+
+		#region Game-Specific Loading
+		private void LoadFileTable_VPW2()
+		{
+			MemoryStream ms = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader br = new BinaryReader(ms);
+
+			this.CurrentFileTable = new FileTable();
+
+			bool hasLocation = false;
+			if (Program.CurLocationFile != null)
+			{
+				if (Program.CurLocationFile.FileTable != null)
+				{
+					br.BaseStream.Seek(Program.CurLocationFile.FileTable.Address, SeekOrigin.Begin);
+					this.CurrentFileTable.Load(br, Program.CurLocationFile.FileTable.Width);
+					hasLocation = true;
+				}
+			}
+			if (!hasLocation)
+			{
+				// fallback to hardedcoded offset
+				MessageBox.Show(
+					"File Table location not found; using hardcoded offset instead.",
+					SharedStrings.MainForm_Title,
+					MessageBoxButtons.OK,
+					MessageBoxIcon.Information
+				);
+				br.BaseStream.Seek(0x1310F40, SeekOrigin.Begin);
+				this.CurrentFileTable.Load(br, 52364);
+			}
+
+			br.Close();
+		}
+		#endregion
+
+		#region temp info dump
+		private void UpdateInfo_VPW2()
+		{
+			StringBuilder sb = new StringBuilder();
+
+			for (int i = 1; i < this.CurrentFileTable.Entries.Count; i++)
+			{
+				FileTableEntry fte = this.CurrentFileTable.Entries[i];
+				sb.AppendLine(
+					String.Format(
+						"[{0:X4}]{1} {2:X8} (ROM addr: {3:X8})",
+						i,
+						fte.IsEncoded ? "*" : " ",
+						fte.Location,
+						fte.Location + 0x152DF0
+					)
+				);
+			}
+
+			tbInfoDump.Text = sb.ToString();
+		}
+		#endregion
+	}
+}
