3698a89e8dc881602ff3e41e8ec38984f6e67e75
freem <aj187@ajworld.net>
2018-02-18 01:14:42 -0600

    probably the last commit to this wrestler edit control since I'm going to do the whole "separate dialogs per game type" method of editing

--- VPWStudio/VPWStudio/Controls/WrestlerEdit_VPW2.cs
+++ VPWStudio/VPWStudio/Controls/WrestlerEdit_VPW2.cs
@@ -12,6 +12,8 @@ namespace VPWStudio.Controls
 {
 	public partial class WrestlerEdit_VPW2 : UserControl
 	{
+		public GameSpecific.VPW2.WrestlerDefinition CurWrestlerDef;
+
 		public WrestlerEdit_VPW2()
 		{
 			InitializeComponent();
@@ -19,16 +21,18 @@ namespace VPWStudio.Controls
 
 		public void LoadData(GameSpecific.VPW2.WrestlerDefinition wdef)
 		{
-			tbWrestlerID4.Text = String.Format("{0:X4}", wdef.WrestlerID4);
-			tbWrestlerID2.Text = String.Format("{0:X2}", wdef.WrestlerID2);
-			cbThemeMusic.SelectedIndex = wdef.ThemeSong;
-			cbNameCall.SelectedIndex = wdef.NameCall;
-			cbVoice1.SelectedIndex = wdef.Voice1;
-			cbVoice2.SelectedIndex = wdef.Voice2;
-			tbMovesetIndex.Text = String.Format("{0:X4}",wdef.MovesetFileIndex);
-			tbParamsIndex.Text = String.Format("{0:X4}", wdef.ParamsFileIndex);
-			tbAppearanceIndex.Text = String.Format("{0:X4}", wdef.AppearanceIndex);
-			tbProfileIndex.Text = String.Format("{0:X4}", wdef.ProfileIndex);
+			this.CurWrestlerDef = wdef;
+
+			tbWrestlerID4.Text = String.Format("{0:X4}", this.CurWrestlerDef.WrestlerID4);
+			tbWrestlerID2.Text = String.Format("{0:X2}", this.CurWrestlerDef.WrestlerID2);
+			cbThemeMusic.SelectedIndex = this.CurWrestlerDef.ThemeSong;
+			cbNameCall.SelectedIndex = this.CurWrestlerDef.NameCall;
+			cbVoice1.SelectedIndex = this.CurWrestlerDef.Voice1;
+			cbVoice2.SelectedIndex = this.CurWrestlerDef.Voice2;
+			tbMovesetIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.MovesetFileIndex);
+			tbParamsIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.ParamsFileIndex);
+			tbAppearanceIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.AppearanceIndex);
+			tbProfileIndex.Text = String.Format("{0:X4}", this.CurWrestlerDef.ProfileIndex);
 		}
 	}
 }
