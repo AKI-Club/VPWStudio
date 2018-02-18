2fc90ea0770882875f845edd8b3c587d4bed8a65
freem <aj187@ajworld.net>
2018-02-18 04:25:05 -0600

    various FileTableDialog updates * reconfigured loading and info dump to not require per-game routines * added support for 7 more games (World Tour non-PAL, VPW64, WM2K all regions)

--- VPWStudio/VPWStudio/Editors/FileTableDialog.cs
+++ VPWStudio/VPWStudio/Editors/FileTableDialog.cs
@@ -20,26 +20,12 @@ namespace VPWStudio
 
 			if (Program.CurrentProject != null)
 			{
-				switch (Program.CurrentProject.Settings.BaseGame)
-				{
-					case VPWGames.Revenge:
-						LoadFileTable_Revenge();
-						UpdateInfoDump();
-						break;
-					case VPWGames.VPW2:
-						LoadFileTable_VPW2();
-						UpdateInfoDump();
-						break;
-					default:
-						MessageBox.Show(String.Format("not implemented for {0}", Program.CurrentProject.Settings.BaseGame));
-						break;
-				}
+				LoadFileTable();
+				UpdateInfoDump();
 			}
 		}
 
-		// todo: load could be genericized
-		#region Game-Specific Loading
-		private void LoadFileTable_Revenge()
+		private void LoadFileTable()
 		{
 			MemoryStream ms = new MemoryStream(Program.CurrentInputROM.Data);
 			BinaryReader br = new BinaryReader(ms);
@@ -47,6 +33,7 @@ namespace VPWStudio
 			this.CurrentFileTable = new FileTable();
 
 			bool hasLocation = false;
+			bool hasLength = false;
 			if (Program.CurLocationFile != null)
 			{
 				if (Program.CurLocationFile.FileTable != null)
@@ -54,64 +41,84 @@ namespace VPWStudio
 					br.BaseStream.Seek(Program.CurLocationFile.FileTable.Address, SeekOrigin.Begin);
 					this.CurrentFileTable.Load(br, Program.CurLocationFile.FileTable.Width);
 					hasLocation = true;
+					hasLength = true;
 				}
 			}
-			if (!hasLocation)
+			if (!hasLocation || !hasLength)
 			{
-				// fallback to hardedcoded offset
+				// fallback to hardedcoded offset and length.
 				MessageBox.Show(
 					"File Table location not found; using hardcoded offset and length instead.",
 					SharedStrings.MainForm_Title,
 					MessageBoxButtons.OK,
 					MessageBoxIcon.Information
 				);
+
+				int offset = 0;
+				int length = 0;
 				switch (Program.CurrentProject.Settings.GameType)
 				{
+					case SpecificGame.WorldTour_NTSC_U_10:
+						offset = 0x7C1A78;
+						length = 21996;
+						break;
+					case SpecificGame.WorldTour_NTSC_U_11:
+						offset = 0x7C1C70;
+						length = 21996;
+						break;
+					case SpecificGame.WorldTour_PAL:
+						offset = 0x7C1C00;
+						length = 21996;
+						break;
+					case SpecificGame.VPW64_NTSC_J:
+						offset = 0xC7B578;
+						length = 37432;
+						break;
 					case SpecificGame.Revenge_NTSC_U:
-						br.BaseStream.Seek(0xCE2752, SeekOrigin.Begin);
+						offset = 0xCE2752;
+						length = 30632;
 						break;
 					case SpecificGame.Revenge_PAL:
-						br.BaseStream.Seek(0xCDFCE2, SeekOrigin.Begin);
+						offset = 0xCDFCE2;
+						length = 30632;
+						break;
+					case SpecificGame.WM2K_NTSC_U:
+						offset = 0x11778BE;
+						length = 41248;
+						break;
+					case SpecificGame.WM2K_NTSC_J:
+						offset = 0x116F3C2;
+						length = 41480;
+						break;
+					case SpecificGame.WM2K_PAL:
+						offset = 0x11778BE;
+						length = 41248;
+						break;
+					case SpecificGame.VPW2_NTSC_J:
+						offset = 0x1310F40;
+						length = 52364;
+						break;
+					case SpecificGame.NoMercy_NTSC_U_10:
+						break;
+					case SpecificGame.NoMercy_NTSC_U_11:
+						break;
+					case SpecificGame.NoMercy_PAL_10:
+						break;
+					case SpecificGame.NoMercy_PAL_11:
 						break;
 				}
-				this.CurrentFileTable.Load(br, 30632);
-			}
-			br.Close();
-		}
-
-		private void LoadFileTable_VPW2()
-		{
-			MemoryStream ms = new MemoryStream(Program.CurrentInputROM.Data);
-			BinaryReader br = new BinaryReader(ms);
-
-			this.CurrentFileTable = new FileTable();
-
-			bool hasLocation = false;
-			if (Program.CurLocationFile != null)
-			{
-				if (Program.CurLocationFile.FileTable != null)
+				if (offset != 0 && length != 0)
 				{
-					br.BaseStream.Seek(Program.CurLocationFile.FileTable.Address, SeekOrigin.Begin);
-					this.CurrentFileTable.Load(br, Program.CurLocationFile.FileTable.Width);
-					hasLocation = true;
+					br.BaseStream.Seek(offset, SeekOrigin.Begin);
+					this.CurrentFileTable.Load(br, length);
+				}
+				else
+				{
+					MessageBox.Show(String.Format("Well I guess I didn't implement {0} yet...", Program.CurrentProject.Settings.GameType));
 				}
 			}
-			if (!hasLocation)
-			{
-				// fallback to hardedcoded offset
-				MessageBox.Show(
-					"File Table location not found; using hardcoded offset and length instead.",
-					SharedStrings.MainForm_Title,
-					MessageBoxButtons.OK,
-					MessageBoxIcon.Information
-				);
-				br.BaseStream.Seek(0x1310F40, SeekOrigin.Begin);
-				this.CurrentFileTable.Load(br, 52364);
-			}
-
 			br.Close();
 		}
-		#endregion
 
 		#region temp info dump
 		private void UpdateInfoDump()
@@ -132,15 +139,35 @@ namespace VPWStudio
 			{
 				switch (Program.CurrentProject.Settings.GameType)
 				{
+					case SpecificGame.WorldTour_NTSC_U_10:
+						offset = 0x39490;
+						break;
+					case SpecificGame.WorldTour_NTSC_U_11:
+						offset = 0x39500;
+						break;
+					// world tour pal
+					case SpecificGame.VPW64_NTSC_J:
+						offset = 0x4AD00;
+						break;
 					case SpecificGame.Revenge_NTSC_U:
 						offset = 0xDAC50;
 						break;
 					case SpecificGame.Revenge_PAL:
 						offset = 0xD81E0;
 						break;
+					case SpecificGame.WM2K_NTSC_U:
+						offset = 0x144AA0;
+						break;
+					case SpecificGame.WM2K_NTSC_J:
+						offset = 0x12C070;
+						break;
+					case SpecificGame.WM2K_PAL:
+						offset = 0x144AC0;
+						break;
 					case SpecificGame.VPW2_NTSC_J:
 						offset = 0x152DF0;
 						break;
+					// all wwf no mercy
 				}
 			}
 
