82a4d20def7aa0d5fd6ca77b3392c2de7effd1c7
freem <aj187@ajworld.net>
2018-02-20 17:53:33 -0600

    added some more shared strings

--- VPWStudio/VPWStudio/SharedStrings.cs
+++ VPWStudio/VPWStudio/SharedStrings.cs
@@ -6,9 +6,10 @@ namespace VPWStudio
 	/// <summary>
 	/// Strings shared throughout the program.
 	/// </summary>
+	/// todo: these should really be in the per-language Resources
 	public static class SharedStrings
 	{
-		// [ja] バープロスタジオ
+		// [ja] バープロ スタジオ
 		public static string MainForm_Title = "VPW Studio";
 
 		#region Save/Load Dialog Filter Strings
@@ -19,10 +20,21 @@ namespace VPWStudio
 		public static string FileFilter_GameSharkCodes = "GameShark Code File (*.gscodes)|*.gscodes|All Files (*.*)|*.*";
 		#endregion
 
+		#region Unsaved Project Strings
+		public static string UnsavedProject_OpenProject = "";
+		public static string UnsavedProject_CloseProject = "";
+		public static string UnsavedProject_ExitProgram = "";
+		#endregion
+
 		#region Error Message Content
 		public static string PlayRomError_NoProjectLoaded = "No project is loaded, so no ROM can be played.";
 		public static string PlayRomError_EmuPathNotSet = "The Emulator Path has not been set in the Program Options.";
 		public static string PlayRomError_EmuPathNotExist = "The emulator you are attempting to use has not been found. Please check the Emulator Path in the Program Options.";
+		#endregion
+
+		#region File Table dialog
+		public static string FileTableDialog_ExtractFile = "&Extract File...";
+		public static string FileTableDialog_ExtractFiles = "&Extract Files...";
 		#endregion
 	}
 }
