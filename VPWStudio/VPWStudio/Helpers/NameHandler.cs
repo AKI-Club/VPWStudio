e4e81d716a103181c0769bbaa9113e6e1054c1b3
freem <aj187@ajworld.net>
2018-02-21 14:00:01 -0600

    split out the name encoding/decoding code into a class

---
+++ VPWStudio/VPWStudio/Helpers/NameHandler.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPWStudio
+{
+	/// <summary>
+	/// Handles "encoded" names, used from World Tour to WrestleMania 2000.
+	/// </summary>
+	/// todo: VPW64 may need to handle strings as EUC-JP
+	public class NameHandler
+	{
+		/// <summary>
+		/// Parse mode for decoding names.
+		/// </summary>
+		private enum ParseMode
+		{
+			/// <summary>
+			/// output as-is (long name only)
+			/// </summary>
+			Normal,
+			/// <summary>
+			/// output between &lt;&gt; chars (short and long name)
+			/// </summary>
+			ShortLong, // <>
+			/// <summary>
+			/// output between {} chars (short name only)
+			/// </summary>
+			ShortOnly  // {}
+		}
+
+		/// <summary>
+		/// Encode a long and short name into a full name.
+		/// </summary>
+		/// <param name="_long">Long name to encode.</param>
+		/// <param name="_short">Short name to encode.</param>
+		/// <returns>Encoded name.</returns>
+		public static string EncodeName(string _long, string _short)
+		{
+			string result = String.Empty;
+
+			// this is a lot tricker than decoding.
+			// general idea: search for the longest substring of the short name within the long name.
+
+			// the easiest case is when the short name is found within the long name
+			if (_long.Contains(_short))
+			{
+				// well gee we have it easy, don't we
+				// <John> Cena (short name "John"), or John <Cena> (short name "Cena")
+
+				// figure out where the short name is within the long name
+				if (_long.StartsWith(_short))
+				{
+					// well that's even EASIER.
+					result = String.Format("<{0}>{1}", _short, _long.Substring(_short.Length));
+				}
+				else
+				{
+					// need to do some work, but not as bad as the other case below...
+					int shortPos = _long.IndexOf(_short);
+					result = string.Format("{0}<{1}>", _long.Substring(0, shortPos), _long.Substring(shortPos));
+				}
+			}
+			else
+			{
+				// a bit more work on our hands.
+
+				// sometimes, the short name is a hack of the long name
+				// * "<R>ick{.}< Steiner>"; long = "Rick Steiner", short = "R. Steiner"
+				// * "<S>tevie{.}< Ray>"; long = "Stevie Ray", short = "S. Ray"
+				// * "<M>aya <I>nca <B>oy"; long = "Maya Inca Boy", short = "MIB"
+
+				// sometimes, the short and long names differ so much, they can't be reconciled.
+				// Some examples:
+				// * "Diamond Dallas Page{DDP}" (human intervention version: "<D>iamond <D>allas <P>age" uses more bytes)
+				// * "Konnan{K-Dawg}" (human intervention version: "<K>onnan{-Dawg}" uses more bytes)
+				// * "Juventud Guerrera{Juvi}" (human intervention version: "<Juv>entud Guerrera{i}" uses LESS bytes)
+				result = String.Format("{0}{{{1}}}", _long, _short);
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Decode a wrestler name string into long and short names.
+		/// </summary>
+		/// <param name="_in">Input name string</param>
+		/// <returns>String array with long (index 0) and short (index 1) names.</returns>
+		public static string[] DecodeName(string _in)
+		{
+			string[] result = new string[2];
+			ParseMode CurParseMode = ParseMode.Normal;
+
+			bool ignore = false;
+			foreach (char c in _in.ToCharArray())
+			{
+				if (c.Equals('<'))
+				{
+					CurParseMode = ParseMode.ShortLong;
+					ignore = true;
+				}
+				if (c.Equals('{'))
+				{
+					CurParseMode = ParseMode.ShortOnly;
+					ignore = true;
+				}
+
+				if (c.Equals('>') || c.Equals('}'))
+				{
+					CurParseMode = ParseMode.Normal;
+					ignore = true;
+				}
+
+				if (!ignore)
+				{
+					switch (CurParseMode)
+					{
+						case ParseMode.ShortLong:
+							result[1] += c;
+							result[0] += c;
+							break;
+						case ParseMode.ShortOnly:
+							result[1] += c;
+							break;
+						case ParseMode.Normal:
+						default:
+							result[0] += c;
+							break;
+					}
+				}
+				ignore = false; // reset
+			}
+
+			return result;
+		}
+	}
+}
--- VPWStudio/VPWStudio/Tools/NameEncodeDecodeTool.cs
+++ VPWStudio/VPWStudio/Tools/NameEncodeDecodeTool.cs
@@ -3,19 +3,9 @@ using System.Windows.Forms;
 
 namespace VPWStudio
 {
-	// this is temporary, but the code behind it should be placed elsewhere.
-	// note 2: I think VPW64 is harder to deal with here.
+	// this dialog is temporary
 	public partial class NameEncodeDecodeTool : Form
 	{
-		private enum ParseMode
-		{
-			Normal,
-			ShortLong, // <>
-			ShortOnly  // {}
-		}
-
-		private ParseMode CurParseMode = ParseMode.Normal;
-
 		public NameEncodeDecodeTool()
 		{
 			InitializeComponent();
@@ -26,49 +16,7 @@ namespace VPWStudio
 		/// </summary>
 		private void buttonEncode_Click(object sender, EventArgs e)
 		{
-			// this one is tougher.
-			string full = String.Empty;
-			string curShort = tbShort.Text;
-			string curLong = tbLong.Text;
-
-			// any characters that appear in short and long names go between <>
-			// any characters only appearing in short names go between {}
-
-			if (curLong.Contains(curShort))
-			{
-				// well gee we have it easy, don't we
-				// <John> Cena (short name "John"), or John <Cena> (short name "Cena")
-
-				// figure out where the short name is within the long name
-				if (curLong.StartsWith(curShort))
-				{
-					// well that's even EASIER.
-					full = String.Format("<{0}>{1}", curShort, curLong.Substring(curShort.Length));
-				}
-				else
-				{
-					// need to do some work, but not as bad as the other case below...
-					int shortPos = curLong.IndexOf(curShort);
-					full = string.Format("{0}<{1}>", curLong.Substring(0, shortPos), curLong.Substring(shortPos));
-				}
-			}
-			else
-			{
-				MessageBox.Show("the instance where short name isn't found in long name needs to be worked on, sorry.");
-				// a bit more work on our hands.
-				// general idea: search for the longest substring of the short name within the long name.
-
-				// sometimes, the short name is a hack of the long name
-				// * "<R>ick{.}< Steiner>"; long = "Rick Steiner", short = "R. Steiner"
-
-				// sometimes, the short and long names differ so much, they can't be reconciled.
-				// Some examples:
-				// * "Diamond Dallas Page{DDP}" (human intervention version: "<D>iamond <D>allas <P>age" uses more bytes)
-				// * "Konnan{K-Dawg}" (human intervention version: "<K>onnan{-Dawg}" uses more bytes)
-				// * "Juventud Guerrera{Juvi}" (human intervention version: "<Juv>entud Guerrera{i}" uses LESS bytes)
-			}
-
-			tbFull.Text = full;
+			tbFull.Text = NameHandler.EncodeName(tbLong.Text, tbShort.Text);
 		}
 
 		/// <summary>
@@ -76,52 +24,9 @@ namespace VPWStudio
 		/// </summary>
 		private void buttonDecode_Click(object sender, EventArgs e)
 		{
-			string full = tbFull.Text;
-			string newShort = String.Empty;
-			string newLong = String.Empty;
-
-			bool ignore = false;
-			foreach (char c in full.ToCharArray())
-			{
-				if (c.Equals('<'))
-				{
-					this.CurParseMode = ParseMode.ShortLong;
-					ignore = true;
-				}
-				if (c.Equals('{'))
-				{
-					this.CurParseMode = ParseMode.ShortOnly;
-					ignore = true;
-				}
-
-				if (c.Equals('>') || c.Equals('}'))
-				{
-					this.CurParseMode = ParseMode.Normal;
-					ignore = true;
-				}
-
-				if (!ignore)
-				{
-					switch (this.CurParseMode)
-					{
-						case ParseMode.ShortLong:
-							newShort += c;
-							newLong += c;
-							break;
-						case ParseMode.ShortOnly:
-							newShort += c;
-							break;
-						case ParseMode.Normal:
-						default:
-							newLong += c;
-							break;
-					}
-				}
-				ignore = false; // reset
-			}
-
-			tbLong.Text = newLong;
-			tbShort.Text = newShort;
+			string[] result = NameHandler.DecodeName(tbFull.Text);
+			tbLong.Text = result[0];
+			tbShort.Text = result[1];
 		}
 	}
 }
