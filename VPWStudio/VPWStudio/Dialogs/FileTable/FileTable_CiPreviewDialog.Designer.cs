9724bfd5e29cbe0b92a6f2a215e4331d79af2033
freem <aj187@ajworld.net>
2018-02-22 12:59:00 -0600

    add FileTable_CiPreviewDialog

---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_CiPreviewDialog.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace VPWStudio
+{
+	partial class FileTable_CiPreviewDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.gbPalette = new System.Windows.Forms.GroupBox();
+			this.gbPreview = new System.Windows.Forms.GroupBox();
+			this.cbPalettes = new System.Windows.Forms.ComboBox();
+			this.pbPreview = new System.Windows.Forms.PictureBox();
+			this.gbPalette.SuspendLayout();
+			this.gbPreview.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// gbPalette
+			// 
+			this.gbPalette.Controls.Add(this.cbPalettes);
+			this.gbPalette.Location = new System.Drawing.Point(12, 12);
+			this.gbPalette.Name = "gbPalette";
+			this.gbPalette.Size = new System.Drawing.Size(368, 46);
+			this.gbPalette.TabIndex = 0;
+			this.gbPalette.TabStop = false;
+			this.gbPalette.Text = "Palette";
+			// 
+			// gbPreview
+			// 
+			this.gbPreview.Controls.Add(this.pbPreview);
+			this.gbPreview.Location = new System.Drawing.Point(12, 64);
+			this.gbPreview.Name = "gbPreview";
+			this.gbPreview.Size = new System.Drawing.Size(368, 217);
+			this.gbPreview.TabIndex = 1;
+			this.gbPreview.TabStop = false;
+			this.gbPreview.Text = "Preview";
+			// 
+			// cbPalettes
+			// 
+			this.cbPalettes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbPalettes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbPalettes.FormattingEnabled = true;
+			this.cbPalettes.Location = new System.Drawing.Point(6, 19);
+			this.cbPalettes.Name = "cbPalettes";
+			this.cbPalettes.Size = new System.Drawing.Size(356, 21);
+			this.cbPalettes.TabIndex = 0;
+			this.cbPalettes.SelectedValueChanged += new System.EventHandler(this.cbPalettes_SelectedValueChanged);
+			// 
+			// pbPreview
+			// 
+			this.pbPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pbPreview.Location = new System.Drawing.Point(3, 16);
+			this.pbPreview.Name = "pbPreview";
+			this.pbPreview.Size = new System.Drawing.Size(362, 198);
+			this.pbPreview.TabIndex = 0;
+			this.pbPreview.TabStop = false;
+			// 
+			// FileTable_CiPreviewDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(392, 293);
+			this.Controls.Add(this.gbPreview);
+			this.Controls.Add(this.gbPalette);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "FileTable_CiPreviewDialog";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "(CI* image preview)";
+			this.gbPalette.ResumeLayout(false);
+			this.gbPreview.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox gbPalette;
+		private System.Windows.Forms.ComboBox cbPalettes;
+		private System.Windows.Forms.GroupBox gbPreview;
+		private System.Windows.Forms.PictureBox pbPreview;
+	}
+}
---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_CiPreviewDialog.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio
+{
+	public partial class FileTable_CiPreviewDialog : Form
+	{
+		private Ci4Palette CurCI4Palette;
+		private Ci4Texture CurCI4Texture;
+
+		private Ci8Palette CurCI8Palette;
+		private Ci8Texture CurCI8Texture;
+
+		private Bitmap CurBitmap;
+
+		private List<int> paletteIDs;
+
+		private enum CiViewerModes
+		{
+			Ci4,
+			Ci8
+		}
+		private CiViewerModes CurViewMode;
+
+		public FileTable_CiPreviewDialog(int fileID)
+		{
+			InitializeComponent();
+			this.Text = String.Format("Preview [0x{0:X4}]", fileID);
+
+			MemoryStream romStream = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader romReader = new BinaryReader(romStream);
+
+			MemoryStream imgStream = new MemoryStream();
+			BinaryWriter imgWriter = new BinaryWriter(imgStream);
+
+			bool lzss = Program.CurrentProject.ProjectFileTable.Entries[fileID].IsEncoded;
+
+			// CurViewMode depends on item type.
+			if (Program.CurrentProject.ProjectFileTable.Entries[fileID].FileType == FileTypes.Ci4Texture)
+			{
+				CurViewMode = CiViewerModes.Ci4;
+				CurCI8Palette = null;
+				CurCI8Texture = null;
+
+				paletteIDs = Program.CurrentProject.ProjectFileTable.GetFilesOfType(FileTypes.Ci4Palette);
+				CurCI4Palette = new Ci4Palette();
+				CurCI4Texture = new Ci4Texture();
+
+				Program.CurrentProject.ProjectFileTable.ExtractFile(romReader, imgWriter, fileID, lzss);
+				imgStream.Seek(0, SeekOrigin.Begin);
+				BinaryReader fr = new BinaryReader(imgStream);
+				CurCI4Texture.ReadData(fr);
+				fr.Close();
+
+				CurBitmap = new Bitmap(CurCI4Texture.Width, CurCI4Texture.Height);
+			}
+			else if(Program.CurrentProject.ProjectFileTable.Entries[fileID].FileType == FileTypes.Ci8Texture)
+			{
+				CurViewMode = CiViewerModes.Ci8;
+				CurCI4Palette = null;
+				CurCI4Texture = null;
+
+				paletteIDs = Program.CurrentProject.ProjectFileTable.GetFilesOfType(FileTypes.Ci8Palette);
+				CurCI8Palette = new Ci8Palette();
+				CurCI8Texture = new Ci8Texture();
+
+				Program.CurrentProject.ProjectFileTable.ExtractFile(romReader, imgWriter, fileID, lzss);
+				imgStream.Seek(0, SeekOrigin.Begin);
+				BinaryReader fr = new BinaryReader(imgStream);
+				CurCI8Texture.ReadData(fr);
+				fr.Close();
+
+				CurBitmap = new Bitmap(CurCI8Texture.Width, CurCI8Texture.Height);
+			}
+
+			imgWriter.Close();
+			romReader.Close();
+
+			// load palettes
+			cbPalettes.Items.Clear();
+			cbPalettes.BeginUpdate();
+			foreach (int i in paletteIDs)
+			{
+				cbPalettes.Items.Add(String.Format("{0:X4}",i));
+			}
+			cbPalettes.EndUpdate();
+		}
+
+		private void cbPalettes_SelectedValueChanged(object sender, EventArgs e)
+		{
+			int curID = paletteIDs[cbPalettes.SelectedIndex];
+			bool lzss = Program.CurrentProject.ProjectFileTable.Entries[curID].IsEncoded;
+
+			MemoryStream romStream = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader romReader = new BinaryReader(romStream);
+
+			MemoryStream palStream = new MemoryStream();
+			BinaryWriter palWriter = new BinaryWriter(palStream);
+
+			Program.CurrentProject.ProjectFileTable.ExtractFile(romReader, palWriter, curID, lzss);
+			palStream.Seek(0, SeekOrigin.Begin);
+			BinaryReader fr = new BinaryReader(palStream);
+
+			switch (CurViewMode)
+			{
+				case CiViewerModes.Ci4:
+					{
+						CurCI4Palette.ReadData(fr);
+						CurBitmap = CurCI4Texture.GetBitmap(CurCI4Palette);
+					}
+					break;
+				case CiViewerModes.Ci8:
+					{
+						CurCI8Palette.ReadData(fr);
+						CurBitmap = CurCI8Texture.GetBitmap(CurCI8Palette);
+					}
+					break;
+			}
+
+			fr.Close();
+			palWriter.Close();
+			romReader.Close();
+
+			pbPreview.Image = CurBitmap;
+		}
+	}
+}
