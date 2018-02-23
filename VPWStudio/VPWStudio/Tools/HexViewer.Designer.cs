c2f4c61505535400168a8dbb23f0dc2df75dab85
freem <aj187@ajworld.net>
2018-02-23 01:02:01 -0600

    add HexViewer, a little quick and dirty thing that will be extremely helpful

---
+++ VPWStudio/VPWStudio/Tools/HexViewer.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace VPWStudio
+{
+	partial class HexViewer
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.byteViewer = new System.ComponentModel.Design.ByteViewer();
+			this.SuspendLayout();
+			// 
+			// byteViewer
+			// 
+			this.byteViewer.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+			this.byteViewer.ColumnCount = 1;
+			this.byteViewer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.byteViewer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.byteViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.byteViewer.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.byteViewer.Location = new System.Drawing.Point(0, 0);
+			this.byteViewer.Name = "byteViewer";
+			this.byteViewer.RowCount = 1;
+			this.byteViewer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.byteViewer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.byteViewer.Size = new System.Drawing.Size(634, 348);
+			this.byteViewer.TabIndex = 0;
+			// 
+			// HexViewer
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(632, 346);
+			this.Controls.Add(this.byteViewer);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(640, 373);
+			this.Name = "HexViewer";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Hex Viewer";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.ComponentModel.Design.ByteViewer byteViewer;
+	}
+}
---
+++ VPWStudio/VPWStudio/Tools/HexViewer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio
+{
+	/// <summary>
+	/// Quick and dirty hex dump viewer.
+	/// </summary>
+	public partial class HexViewer : Form
+	{
+		public HexViewer(int fileID)
+		{
+			InitializeComponent();
+			byteViewer.SetDisplayMode(DisplayMode.Hexdump);
+			LoadFile(fileID);
+		}
+
+		public void RequestLoad(int fileID) => LoadFile(fileID);
+
+		private void LoadFile(int fileID)
+		{
+			MemoryStream romStream = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader romReader = new BinaryReader(romStream);
+
+			MemoryStream extractStream = new MemoryStream();
+			BinaryWriter extractWriter = new BinaryWriter(extractStream);
+
+			bool lzss = Program.CurrentProject.ProjectFileTable.Entries[fileID].IsEncoded;
+			Program.CurrentProject.ProjectFileTable.ExtractFile(romReader, extractWriter, fileID, lzss);
+			romReader.Close();
+
+			int size = (int)extractStream.Position;
+			extractStream.Seek(0, SeekOrigin.Begin);
+			BinaryReader br = new BinaryReader(extractStream);
+			byte[] data = br.ReadBytes(size);
+			br.Close();
+			extractWriter.Close();
+
+			byteViewer.SetBytes(data);
+			this.Text = String.Format("Hex Viewer [{0:X4}]", fileID);
+		}
+	}
+}
