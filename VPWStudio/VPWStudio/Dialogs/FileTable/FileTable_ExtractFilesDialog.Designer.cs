1fdf69aad12f9dad4289e46da44a74ddb7eb9e10
freem <aj187@ajworld.net>
2018-02-23 00:03:37 -0600

    work in progress FileTable_ExtractFilesDialog

---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_ExtractFilesDialog.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace VPWStudio
+{
+	partial class FileTable_ExtractFilesDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.dgvFiles = new System.Windows.Forms.DataGridView();
+			this.EnableExport = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+			this.FileID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Comment = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.OutFilename = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// dgvFiles
+			// 
+			this.dgvFiles.AllowUserToAddRows = false;
+			this.dgvFiles.AllowUserToDeleteRows = false;
+			dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+			this.dgvFiles.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+			this.dgvFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dgvFiles.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+			this.dgvFiles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvFiles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.EnableExport,
+            this.FileID,
+            this.Comment,
+            this.OutFilename});
+			this.dgvFiles.Location = new System.Drawing.Point(12, 12);
+			this.dgvFiles.Name = "dgvFiles";
+			this.dgvFiles.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+			this.dgvFiles.Size = new System.Drawing.Size(468, 240);
+			this.dgvFiles.TabIndex = 0;
+			// 
+			// EnableExport
+			// 
+			this.EnableExport.HeaderText = "Export";
+			this.EnableExport.Name = "EnableExport";
+			this.EnableExport.Width = 43;
+			// 
+			// FileID
+			// 
+			this.FileID.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.FileID.FillWeight = 20F;
+			this.FileID.HeaderText = "File ID";
+			this.FileID.Name = "FileID";
+			this.FileID.ReadOnly = true;
+			// 
+			// Comment
+			// 
+			this.Comment.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.Comment.FillWeight = 35F;
+			this.Comment.HeaderText = "Comment";
+			this.Comment.Name = "Comment";
+			this.Comment.ReadOnly = true;
+			// 
+			// OutFilename
+			// 
+			this.OutFilename.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+			this.OutFilename.FillWeight = 45F;
+			this.OutFilename.HeaderText = "Export Filename";
+			this.OutFilename.Name = "OutFilename";
+			// 
+			// buttonOK
+			// 
+			this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonOK.Location = new System.Drawing.Point(324, 258);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(75, 23);
+			this.buttonOK.TabIndex = 1;
+			this.buttonOK.Text = "&OK";
+			this.buttonOK.UseVisualStyleBackColor = true;
+			this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonCancel.Location = new System.Drawing.Point(405, 258);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 2;
+			this.buttonCancel.Text = "&Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+			// 
+			// FileTable_ExtractFilesDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(492, 293);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonOK);
+			this.Controls.Add(this.dgvFiles);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.Name = "FileTable_ExtractFilesDialog";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Extract Files";
+			((System.ComponentModel.ISupportInitialize)(this.dgvFiles)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dgvFiles;
+		private System.Windows.Forms.Button buttonOK;
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.DataGridViewCheckBoxColumn EnableExport;
+		private System.Windows.Forms.DataGridViewTextBoxColumn FileID;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Comment;
+		private System.Windows.Forms.DataGridViewTextBoxColumn OutFilename;
+	}
+}
---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_ExtractFilesDialog.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio
+{
+	/// <summary>
+	/// Extract multiple files
+	/// </summary>
+	public partial class FileTable_ExtractFilesDialog : Form
+	{
+		/// <summary>
+		/// Initial list of File IDs to extract.
+		/// </summary>
+		private List<int> FileIDs;
+
+		/// <summary>
+		/// Final list of File IDs to extract.
+		/// </summary>
+		public SortedList<int, string> ExtractFiles;
+
+		public FileTable_ExtractFilesDialog(List<int> selectedIDs)
+		{
+			FileIDs = selectedIDs;
+			ExtractFiles = new SortedList<int, string>();
+			InitializeComponent();
+			PopulateList();
+		}
+
+		/// <summary>
+		/// Populate list with files to export.
+		/// </summary>
+		private void PopulateList()
+		{
+			dgvFiles.Rows.Clear();
+			dgvFiles.Rows.Add(FileIDs.Count);
+			for (int i = 0; i < FileIDs.Count; i++)
+			{
+				dgvFiles.Rows[i].Cells[0].Value = true;
+				dgvFiles.Rows[i].Cells[1].Value = String.Format("{0:X4}", FileIDs[i]);
+				dgvFiles.Rows[i].Cells[2].Value = Program.CurrentProject.ProjectFileTable.Entries[FileIDs[i]].Comment;
+				dgvFiles.Rows[i].Cells[3].Value = String.Empty;
+			}
+		}
+
+		/// <summary>
+		/// Cancel button.
+		/// </summary>
+		private void buttonCancel_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.Cancel;
+			this.Close();
+		}
+
+		/// <summary>
+		/// OK button.
+		/// </summary>
+		private void buttonOK_Click(object sender, EventArgs e)
+		{
+			// get number of checked files for export.
+			int checkedCount = 0;
+			for (int i = 0; i < FileIDs.Count; i++)
+			{
+				if ((bool)dgvFiles.Rows[i].Cells[0].Value == true)
+				{
+					checkedCount++;
+				}
+			}
+			if (checkedCount == 0)
+			{
+				MessageBox.Show("No files selected for export.");
+				return;
+			}
+
+			// wow we've got a lot to do here.
+			// * Make sure every entry that's chosen for export has a filename.
+			//   If it doesn't, we have one of two options:
+			//   1) Force the user to enter a name.
+			//   2) Default to "(fileID).bin" (which may not be the best option...)
+			for (int i = 0; i < dgvFiles.Rows.Count; i++)
+			{
+				if ((bool)dgvFiles.Rows[i].Cells[0].Value == false)
+				{
+					continue;
+				}
+
+				// add entry to ExtractFiles
+			}
+
+			this.DialogResult = DialogResult.OK;
+			this.Close();
+		}
+	}
+}
