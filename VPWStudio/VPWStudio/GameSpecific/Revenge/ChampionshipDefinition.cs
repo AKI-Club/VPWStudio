914b9b61cf03be0cab5999415163bfc812cc1e39
freem <aj187@ajworld.net>
2018-02-28 14:47:05 -0600

    revenge championship definition, WIP as explained in previous commit

---
+++ VPWStudio/VPWStudio/GameSpecific/Revenge/ChampionshipDefinition.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPWStudio.GameSpecific.Revenge
+{
+	public class ChampionshipDefinition
+	{
+		// [byte] championship number?
+		// [byte] ??? always 7
+
+		// [byte] Initial Champion 1 ID2
+		public byte ID2_Champion1;
+
+		// [byte] Initial Champion 2 ID2 (leave as 0 if not Tag Team)
+		public byte ID2_Champion2;
+
+		// [byte] unknown
+		// [byte] flags 1 (cruiserweight; others?)
+		// [byte] flags 2 (number of wrestlers; others?)
+		// [byte] unknown (this and the below may be a halfword)
+		// [byte] unknown (this and the above may be a halfword)
+		// [half] assumed halfword 1
+		// [half] assumed halfword 2
+		// [half] assumed halfword 3
+		// [word] Pointer to some ID2 list
+	}
+}
