0c5f63757347d6d7ef85daeb5165d308a4a8675d
freem <aj187@ajworld.net>
2018-03-01 15:03:39 -0600

    and related to the previous commit, this is where that code belongs, the overall "Costume Definitions" dialog for Revenge.

---
+++ VPWStudio/VPWStudio/Editors/Revenge/CostumeDefs_Revenge.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace VPWStudio.Editors.Revenge
+{
+	partial class CostumeDefs_Revenge
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tcMain = new System.Windows.Forms.TabControl();
+			this.tpCostumes = new System.Windows.Forms.TabPage();
+			this.lbCostumes = new System.Windows.Forms.ListBox();
+			this.tpHeads = new System.Windows.Forms.TabPage();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.lbHeadsMasks = new System.Windows.Forms.ListBox();
+			this.tcMain.SuspendLayout();
+			this.tpCostumes.SuspendLayout();
+			this.tpHeads.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tcMain
+			// 
+			this.tcMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tcMain.Controls.Add(this.tpCostumes);
+			this.tcMain.Controls.Add(this.tpHeads);
+			this.tcMain.Location = new System.Drawing.Point(12, 12);
+			this.tcMain.Name = "tcMain";
+			this.tcMain.SelectedIndex = 0;
+			this.tcMain.Size = new System.Drawing.Size(570, 351);
+			this.tcMain.TabIndex = 0;
+			// 
+			// tpCostumes
+			// 
+			this.tpCostumes.Controls.Add(this.lbCostumes);
+			this.tpCostumes.Location = new System.Drawing.Point(4, 22);
+			this.tpCostumes.Name = "tpCostumes";
+			this.tpCostumes.Padding = new System.Windows.Forms.Padding(3);
+			this.tpCostumes.Size = new System.Drawing.Size(562, 325);
+			this.tpCostumes.TabIndex = 0;
+			this.tpCostumes.Text = "Costumes";
+			this.tpCostumes.UseVisualStyleBackColor = true;
+			// 
+			// lbCostumes
+			// 
+			this.lbCostumes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.lbCostumes.FormattingEnabled = true;
+			this.lbCostumes.Location = new System.Drawing.Point(6, 6);
+			this.lbCostumes.Name = "lbCostumes";
+			this.lbCostumes.Size = new System.Drawing.Size(120, 316);
+			this.lbCostumes.TabIndex = 0;
+			this.lbCostumes.SelectedIndexChanged += new System.EventHandler(this.lbCostumes_SelectedIndexChanged);
+			// 
+			// tpHeads
+			// 
+			this.tpHeads.Controls.Add(this.groupBox4);
+			this.tpHeads.Controls.Add(this.groupBox3);
+			this.tpHeads.Controls.Add(this.groupBox2);
+			this.tpHeads.Controls.Add(this.groupBox1);
+			this.tpHeads.Controls.Add(this.lbHeadsMasks);
+			this.tpHeads.Location = new System.Drawing.Point(4, 22);
+			this.tpHeads.Name = "tpHeads";
+			this.tpHeads.Padding = new System.Windows.Forms.Padding(3);
+			this.tpHeads.Size = new System.Drawing.Size(562, 325);
+			this.tpHeads.TabIndex = 1;
+			this.tpHeads.Text = "Heads/Masks";
+			this.tpHeads.UseVisualStyleBackColor = true;
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Location = new System.Drawing.Point(350, 169);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(206, 150);
+			this.groupBox4.TabIndex = 5;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "groupBox4";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Location = new System.Drawing.Point(132, 169);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(206, 150);
+			this.groupBox3.TabIndex = 4;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "groupBox3";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Location = new System.Drawing.Point(350, 6);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(206, 150);
+			this.groupBox2.TabIndex = 3;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "groupBox2";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(132, 6);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(206, 150);
+			this.groupBox1.TabIndex = 2;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// lbHeadsMasks
+			// 
+			this.lbHeadsMasks.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.lbHeadsMasks.FormattingEnabled = true;
+			this.lbHeadsMasks.Location = new System.Drawing.Point(6, 5);
+			this.lbHeadsMasks.Name = "lbHeadsMasks";
+			this.lbHeadsMasks.Size = new System.Drawing.Size(120, 316);
+			this.lbHeadsMasks.TabIndex = 1;
+			this.lbHeadsMasks.SelectedIndexChanged += new System.EventHandler(this.lbHeadsMasks_SelectedIndexChanged);
+			// 
+			// CostumeDefs_Revenge
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(594, 375);
+			this.Controls.Add(this.tcMain);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Name = "CostumeDefs_Revenge";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Costumes and Heads/Masks";
+			this.tcMain.ResumeLayout(false);
+			this.tpCostumes.ResumeLayout(false);
+			this.tpHeads.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tcMain;
+		private System.Windows.Forms.TabPage tpCostumes;
+		private System.Windows.Forms.TabPage tpHeads;
+		private System.Windows.Forms.ListBox lbCostumes;
+		private System.Windows.Forms.ListBox lbHeadsMasks;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox1;
+	}
+}
---
+++ VPWStudio/VPWStudio/Editors/Revenge/CostumeDefs_Revenge.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VPWStudio.GameSpecific;
+
+namespace VPWStudio.Editors.Revenge
+{
+	public partial class CostumeDefs_Revenge : Form
+	{
+		/// <summary>
+		/// Costume definitions
+		/// </summary>
+		private List<CostumeDef_Early> CostumeDefs;
+
+		/// <summary>
+		/// Mask/Head definitions
+		/// </summary>
+		private List<MaskDef_Early> MaskDefs;
+
+		public CostumeDefs_Revenge()
+		{
+			InitializeComponent();
+
+			MemoryStream ms = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader br = new BinaryReader(ms);
+
+			LoadCostumeDefs(br);
+			LoadMaskDefs(br);
+
+			br.Close();
+
+			// populate lists
+		}
+
+		#region Data Load
+		public void LoadCostumeDefs(BinaryReader br)
+		{
+			//
+		}
+
+		public void LoadMaskDefs(BinaryReader br)
+		{
+			// 
+		}
+		#endregion
+
+		#region ListBox Population
+		#endregion
+
+		#region Costumes
+		private void lbCostumes_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+		#endregion
+
+		#region Masks/Heads
+		private void lbHeadsMasks_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+		}
+		#endregion
+	}
+}
