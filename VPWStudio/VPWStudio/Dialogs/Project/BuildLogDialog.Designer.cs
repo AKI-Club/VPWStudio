9387c6394130ebf56559a793fcbe4e4536aef0dd
freem <aj187@ajworld.net>
2018-03-07 20:07:55 -0600

    add BuildLogDialog

---
+++ VPWStudio/VPWStudio/Dialogs/Project/BuildLogDialog.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace VPWStudio
+{
+	partial class BuildLogDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tbLogOutput = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// tbLogOutput
+			// 
+			this.tbLogOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tbLogOutput.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tbLogOutput.Location = new System.Drawing.Point(0, 0);
+			this.tbLogOutput.MaxLength = 65535;
+			this.tbLogOutput.Multiline = true;
+			this.tbLogOutput.Name = "tbLogOutput";
+			this.tbLogOutput.ReadOnly = true;
+			this.tbLogOutput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.tbLogOutput.Size = new System.Drawing.Size(506, 103);
+			this.tbLogOutput.TabIndex = 0;
+			this.tbLogOutput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.tbLogOutput_KeyUp);
+			// 
+			// BuildLogDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(506, 103);
+			this.Controls.Add(this.tbLogOutput);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "BuildLogDialog";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Build Log";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BuildLogDialog_KeyDown);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox tbLogOutput;
+	}
+}
---
+++ VPWStudio/VPWStudio/Dialogs/Project/BuildLogDialog.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio
+{
+	public partial class BuildLogDialog : Form
+	{
+		public bool BuildFinished = false;
+
+		public BuildLogDialog()
+		{
+			InitializeComponent();
+			BuildFinished = false;
+		}
+
+		/// <summary>
+		/// Clear log output
+		/// </summary>
+		public void Clear()
+		{
+			tbLogOutput.Clear();
+		}
+
+		/// <summary>
+		/// Add text to log output (without newline)
+		/// </summary>
+		/// <param name="t">Text to add</param>
+		public void AddText(string t)
+		{
+			tbLogOutput.Text += t;
+		}
+
+		/// <summary>
+		/// Add text to log output (with newline)
+		/// </summary>
+		/// <param name="t">Text to add</param>
+		public void AddLine(string t)
+		{
+			tbLogOutput.Text += t + Environment.NewLine;
+		}
+
+		private void BuildLogDialog_KeyDown(object sender, KeyEventArgs e)
+		{
+			if(BuildFinished && e.KeyCode == Keys.Escape)
+			{
+				this.Close();
+			}
+		}
+
+		private void tbLogOutput_KeyUp(object sender, KeyEventArgs e)
+		{
+			if (BuildFinished && e.KeyCode == Keys.Escape)
+			{
+				this.Close();
+			}
+		}
+	}
+}
