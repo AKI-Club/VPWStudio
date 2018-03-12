9ccd4a1482c7b7677f5a8a2a195e32018e0d3786
freem <aj187@ajworld.net>
2018-03-11 21:34:30 -0500

    [Ci4Texture] add CI4 sub-palette support to GetBitmap()

--- VPWStudio/VPWStudio/DataStructures/N64TexPal/Ci4Texture.cs
+++ VPWStudio/VPWStudio/DataStructures/N64TexPal/Ci4Texture.cs
@@ -74,8 +74,9 @@ namespace VPWStudio
 		/// Convert this Ci4Image to a Bitmap.
 		/// </summary>
 		/// <param name="pal">CI4 Palette data to use.</param>
-		/// <returns></returns>
-		public Bitmap GetBitmap(Ci4Palette pal)
+		/// <param name="subPalette">Optional sub-palette number to use.</param>
+		/// <returns>Bitmap containing an image drawn with the requested Ci4Palette.</returns>
+		public Bitmap GetBitmap(Ci4Palette pal, int subPalette = 0)
 		{
 			if (this.Data == null)
 			{
@@ -88,6 +89,12 @@ namespace VPWStudio
 				return null;
 			}
 
+			// failsafe for subpalette usage
+			if (subPalette > 0 && pal.SubPalettes.Count <= 0)
+			{
+				subPalette = 0;
+			}
+
 			Bitmap bOut = new Bitmap(this.Width, this.Height);
 
 			for (int y = 0; y < this.Height; y++)
@@ -95,7 +102,15 @@ namespace VPWStudio
 				for (int x = 0; x < this.Width; x++)
 				{
 					byte palIdx = this.Data[(y * this.Width) + x];
-					Color c = N64Colors.Value5551ToColor(pal.Entries[palIdx]);
+					Color c;
+					if (subPalette != 0)
+					{
+						c = N64Colors.Value5551ToColor(pal.SubPalettes[subPalette-1].Entries[palIdx]);
+					}
+					else
+					{
+						c = N64Colors.Value5551ToColor(pal.Entries[palIdx]);
+					}
 					bOut.SetPixel(x, y, c);
 				}
 			}
