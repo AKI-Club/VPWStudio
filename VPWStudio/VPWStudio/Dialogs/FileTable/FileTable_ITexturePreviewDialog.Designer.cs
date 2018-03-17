0bc4f60cf55e9eeed95684e2439e555352e5c628
freem <aj187@ajworld.net>
2018-03-17 00:05:53 -0500

    add FileTable_ITexturePreviewDialog

---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_ITexturePreviewDialog.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace VPWStudio
+{
+	partial class FileTable_ITexturePreviewDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.cmsPreview = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.savePNGToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.backgroundColorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.tlpMainContainer = new System.Windows.Forms.TableLayoutPanel();
+			this.pbPreview = new System.Windows.Forms.PictureBox();
+			this.tlpImageControls = new System.Windows.Forms.TableLayoutPanel();
+			this.buttonRedraw = new System.Windows.Forms.Button();
+			this.labelWidth = new System.Windows.Forms.Label();
+			this.labelHeight = new System.Windows.Forms.Label();
+			this.nudWidth = new System.Windows.Forms.NumericUpDown();
+			this.nudHeight = new System.Windows.Forms.NumericUpDown();
+			this.cmsPreview.SuspendLayout();
+			this.tlpMainContainer.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+			this.tlpImageControls.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// cmsPreview
+			// 
+			this.cmsPreview.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.savePNGToolStripMenuItem,
+            this.backgroundColorToolStripMenuItem});
+			this.cmsPreview.Name = "cmsPreview";
+			this.cmsPreview.Size = new System.Drawing.Size(171, 48);
+			// 
+			// savePNGToolStripMenuItem
+			// 
+			this.savePNGToolStripMenuItem.Image = global::VPWStudio.Properties.Resources.MenuIcon16_Save;
+			this.savePNGToolStripMenuItem.Name = "savePNGToolStripMenuItem";
+			this.savePNGToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+			this.savePNGToolStripMenuItem.Text = "Save &PNG...";
+			this.savePNGToolStripMenuItem.Click += new System.EventHandler(this.savePNGToolStripMenuItem_Click);
+			// 
+			// backgroundColorToolStripMenuItem
+			// 
+			this.backgroundColorToolStripMenuItem.Name = "backgroundColorToolStripMenuItem";
+			this.backgroundColorToolStripMenuItem.Size = new System.Drawing.Size(170, 22);
+			this.backgroundColorToolStripMenuItem.Text = "&Background Color...";
+			this.backgroundColorToolStripMenuItem.Click += new System.EventHandler(this.backgroundColorToolStripMenuItem_Click);
+			// 
+			// tlpMainContainer
+			// 
+			this.tlpMainContainer.ColumnCount = 1;
+			this.tlpMainContainer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tlpMainContainer.Controls.Add(this.pbPreview, 0, 1);
+			this.tlpMainContainer.Controls.Add(this.tlpImageControls, 0, 0);
+			this.tlpMainContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tlpMainContainer.Location = new System.Drawing.Point(0, 0);
+			this.tlpMainContainer.Name = "tlpMainContainer";
+			this.tlpMainContainer.RowCount = 2;
+			this.tlpMainContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 16.51376F));
+			this.tlpMainContainer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 83.48624F));
+			this.tlpMainContainer.Size = new System.Drawing.Size(314, 218);
+			this.tlpMainContainer.TabIndex = 1;
+			// 
+			// pbPreview
+			// 
+			this.pbPreview.BackColor = System.Drawing.SystemColors.Control;
+			this.pbPreview.ContextMenuStrip = this.cmsPreview;
+			this.pbPreview.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pbPreview.Location = new System.Drawing.Point(0, 36);
+			this.pbPreview.Margin = new System.Windows.Forms.Padding(0);
+			this.pbPreview.MinimumSize = new System.Drawing.Size(1, 1);
+			this.pbPreview.Name = "pbPreview";
+			this.pbPreview.Size = new System.Drawing.Size(314, 182);
+			this.pbPreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+			this.pbPreview.TabIndex = 3;
+			this.pbPreview.TabStop = false;
+			// 
+			// tlpImageControls
+			// 
+			this.tlpImageControls.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tlpImageControls.ColumnCount = 5;
+			this.tlpImageControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tlpImageControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tlpImageControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tlpImageControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tlpImageControls.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tlpImageControls.Controls.Add(this.buttonRedraw, 4, 0);
+			this.tlpImageControls.Controls.Add(this.labelWidth, 0, 0);
+			this.tlpImageControls.Controls.Add(this.labelHeight, 2, 0);
+			this.tlpImageControls.Controls.Add(this.nudWidth, 1, 0);
+			this.tlpImageControls.Controls.Add(this.nudHeight, 3, 0);
+			this.tlpImageControls.Location = new System.Drawing.Point(3, 3);
+			this.tlpImageControls.Name = "tlpImageControls";
+			this.tlpImageControls.RowCount = 1;
+			this.tlpImageControls.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tlpImageControls.Size = new System.Drawing.Size(308, 30);
+			this.tlpImageControls.TabIndex = 4;
+			// 
+			// buttonRedraw
+			// 
+			this.buttonRedraw.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.buttonRedraw.Location = new System.Drawing.Point(247, 3);
+			this.buttonRedraw.Name = "buttonRedraw";
+			this.buttonRedraw.Size = new System.Drawing.Size(58, 24);
+			this.buttonRedraw.TabIndex = 2;
+			this.buttonRedraw.Text = "&Redraw";
+			this.buttonRedraw.UseVisualStyleBackColor = true;
+			this.buttonRedraw.Click += new System.EventHandler(this.buttonRedraw_Click);
+			// 
+			// labelWidth
+			// 
+			this.labelWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWidth.AutoSize = true;
+			this.labelWidth.Location = new System.Drawing.Point(3, 8);
+			this.labelWidth.Name = "labelWidth";
+			this.labelWidth.Size = new System.Drawing.Size(55, 13);
+			this.labelWidth.TabIndex = 0;
+			this.labelWidth.Text = "&Width";
+			// 
+			// labelHeight
+			// 
+			this.labelHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelHeight.AutoSize = true;
+			this.labelHeight.Location = new System.Drawing.Point(125, 8);
+			this.labelHeight.Name = "labelHeight";
+			this.labelHeight.Size = new System.Drawing.Size(55, 13);
+			this.labelHeight.TabIndex = 1;
+			this.labelHeight.Text = "&Height";
+			// 
+			// nudWidth
+			// 
+			this.nudWidth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.nudWidth.Location = new System.Drawing.Point(64, 5);
+			this.nudWidth.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+			this.nudWidth.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.nudWidth.Name = "nudWidth";
+			this.nudWidth.Size = new System.Drawing.Size(55, 20);
+			this.nudWidth.TabIndex = 0;
+			this.nudWidth.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.nudWidth.Enter += new System.EventHandler(this.nudWidth_Enter);
+			// 
+			// nudHeight
+			// 
+			this.nudHeight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.nudHeight.Location = new System.Drawing.Point(186, 5);
+			this.nudHeight.Maximum = new decimal(new int[] {
+            512,
+            0,
+            0,
+            0});
+			this.nudHeight.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.nudHeight.Name = "nudHeight";
+			this.nudHeight.Size = new System.Drawing.Size(55, 20);
+			this.nudHeight.TabIndex = 1;
+			this.nudHeight.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+			this.nudHeight.Enter += new System.EventHandler(this.nudHeight_Enter);
+			// 
+			// FileTable_ITexturePreviewDialog
+			// 
+			this.AcceptButton = this.buttonRedraw;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(314, 218);
+			this.Controls.Add(this.tlpMainContainer);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(320, 240);
+			this.Name = "FileTable_ITexturePreviewDialog";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Preview";
+			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.FileTable_ITexturePreviewDialog_KeyDown);
+			this.cmsPreview.ResumeLayout(false);
+			this.tlpMainContainer.ResumeLayout(false);
+			this.tlpMainContainer.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+			this.tlpImageControls.ResumeLayout(false);
+			this.tlpImageControls.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+		private System.Windows.Forms.ContextMenuStrip cmsPreview;
+		private System.Windows.Forms.ToolStripMenuItem savePNGToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem backgroundColorToolStripMenuItem;
+		private System.Windows.Forms.TableLayoutPanel tlpMainContainer;
+		private System.Windows.Forms.PictureBox pbPreview;
+		private System.Windows.Forms.TableLayoutPanel tlpImageControls;
+		private System.Windows.Forms.Button buttonRedraw;
+		private System.Windows.Forms.Label labelWidth;
+		private System.Windows.Forms.Label labelHeight;
+		private System.Windows.Forms.NumericUpDown nudWidth;
+		private System.Windows.Forms.NumericUpDown nudHeight;
+	}
+}
---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_ITexturePreviewDialog.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+
+namespace VPWStudio
+{
+	public partial class FileTable_ITexturePreviewDialog : Form
+	{
+		#region Members
+		/// <summary>
+		/// Current I4 texture file
+		/// </summary>
+		private I4Texture CurrentI4Tex;
+
+		/// <summary>
+		/// Current Bitmap
+		/// </summary>
+		private Bitmap CurrentBitmap;
+
+		/// <summary>
+		/// Default image size
+		/// </summary>
+		private Size DefaultImageSize;
+		#endregion
+
+		public FileTable_ITexturePreviewDialog(int fileID)
+		{
+			InitializeComponent();
+			this.Text = String.Format("Preview [0x{0:X4}]",fileID);
+
+			MemoryStream romStream = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader romReader = new BinaryReader(romStream);
+
+			MemoryStream outStream = new MemoryStream();
+			BinaryWriter outWriter = new BinaryWriter(outStream);
+
+			Program.CurrentProject.ProjectFileTable.ExtractFile(romReader, outWriter, fileID);
+			romReader.Close();
+
+			BinaryReader outReader = new BinaryReader(outStream);
+			outStream.Seek(0, SeekOrigin.Begin);
+
+			CurrentI4Tex = new I4Texture(outReader);
+			pbPreview.Width = 16;
+			pbPreview.Height = 16;
+			this.DefaultImageSize = new Size(16, 16);
+
+			pbPreview.Image = null;
+
+			outReader.Close();
+			outWriter.Close();
+		}
+
+		/// <summary>
+		/// allow escape to exit.
+		/// </summary>
+		private void FileTable_ITexturePreviewDialog_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Escape)
+			{
+				this.Close();
+			}
+		}
+
+		/// <summary>
+		/// Save as PNG.
+		/// </summary>
+		private void savePNGToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			SaveFileDialog sfd = new SaveFileDialog();
+			sfd.Title = "Save PNG";
+			sfd.Filter = "PNG Files (*.png)|*.png|All Files(*.*)|*.*";
+			if (sfd.ShowDialog() == DialogResult.OK)
+			{
+				this.CurrentBitmap.Save(sfd.FileName);
+			}
+		}
+
+		/// <summary>
+		/// Change background color.
+		/// </summary>
+		private void backgroundColorToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			ColorDialog cd = new ColorDialog();
+			if (cd.ShowDialog() == DialogResult.OK)
+			{
+				pbPreview.BackColor = cd.Color;
+				DrawImage();
+			}
+		}
+
+		private void DrawImage()
+		{
+			Bitmap zoomed = new Bitmap(this.DefaultImageSize.Width, this.DefaultImageSize.Height);
+
+			Graphics g = Graphics.FromImage(zoomed);
+			g.InterpolationMode = InterpolationMode.NearestNeighbor;
+			g.Clear(pbPreview.BackColor);
+
+			g.DrawImage(
+				CurrentBitmap,
+				new Rectangle(0, 0, this.DefaultImageSize.Width, this.DefaultImageSize.Height),
+				new Rectangle(0, 0, this.DefaultImageSize.Width, this.DefaultImageSize.Height),
+				GraphicsUnit.Pixel
+			);
+			g.Dispose();
+			pbPreview.Image = zoomed;
+		}
+
+		private void buttonRedraw_Click(object sender, EventArgs e)
+		{
+			DefaultImageSize = new Size((int)nudWidth.Value, (int)nudHeight.Value);
+			try
+			{
+				CurrentBitmap = CurrentI4Tex.ToBitmap(DefaultImageSize.Width, DefaultImageSize.Height);
+				DrawImage();
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(String.Format("Error attempting to draw texture:\n{0}", ex.Message));
+			}
+		}
+
+		private void nudWidth_Enter(object sender, EventArgs e)
+		{
+			nudWidth.Select(0, nudWidth.Value.ToString().Length);
+		}
+
+		private void nudHeight_Enter(object sender, EventArgs e)
+		{
+			nudHeight.Select(0, nudHeight.Value.ToString().Length);
+		}
+	}
+}
