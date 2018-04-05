749427052c137ff53c489d664a3bc656b9e115a9
freem <aj187@ajworld.net>
2018-04-05 18:15:30 -0500

    make BuildLogDialog resizable

--- VPWStudio/VPWStudio/Dialogs/Project/BuildLogDialog.Designer.cs
+++ VPWStudio/VPWStudio/Dialogs/Project/BuildLogDialog.Designer.cs
@@ -48,7 +48,6 @@
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(506, 103);
 			this.Controls.Add(this.tbLogOutput);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "BuildLogDialog";
