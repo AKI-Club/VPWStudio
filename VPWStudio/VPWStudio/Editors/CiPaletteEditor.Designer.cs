3a3fb9f3c4ed42dea1fb85bb84b443026475bde1
freem <aj187@ajworld.net>
2018-04-06 14:46:19 -0500

    more updates to the CI Palette Editor

--- VPWStudio/VPWStudio/Editors/CiPaletteEditor.Designer.cs
+++ VPWStudio/VPWStudio/Editors/CiPaletteEditor.Designer.cs
@@ -56,7 +56,7 @@
 			// 
 			// buttonOK
 			// 
-			this.buttonOK.Location = new System.Drawing.Point(314, 307);
+			this.buttonOK.Location = new System.Drawing.Point(348, 437);
 			this.buttonOK.Name = "buttonOK";
 			this.buttonOK.Size = new System.Drawing.Size(75, 23);
 			this.buttonOK.TabIndex = 0;
@@ -66,7 +66,7 @@
 			// 
 			// buttonCancel
 			// 
-			this.buttonCancel.Location = new System.Drawing.Point(395, 307);
+			this.buttonCancel.Location = new System.Drawing.Point(429, 437);
 			this.buttonCancel.Name = "buttonCancel";
 			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
 			this.buttonCancel.TabIndex = 1;
@@ -79,7 +79,7 @@
 			this.gbPalettePreview.Controls.Add(this.pbPalettePreview);
 			this.gbPalettePreview.Location = new System.Drawing.Point(12, 145);
 			this.gbPalettePreview.Name = "gbPalettePreview";
-			this.gbPalettePreview.Size = new System.Drawing.Size(458, 156);
+			this.gbPalettePreview.Size = new System.Drawing.Size(492, 286);
 			this.gbPalettePreview.TabIndex = 2;
 			this.gbPalettePreview.TabStop = false;
 			this.gbPalettePreview.Text = "Palette Preview";
@@ -88,7 +88,7 @@
 			// 
 			this.pbPalettePreview.Location = new System.Drawing.Point(6, 19);
 			this.pbPalettePreview.Name = "pbPalettePreview";
-			this.pbPalettePreview.Size = new System.Drawing.Size(448, 128);
+			this.pbPalettePreview.Size = new System.Drawing.Size(480, 256);
 			this.pbPalettePreview.TabIndex = 0;
 			this.pbPalettePreview.TabStop = false;
 			this.pbPalettePreview.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbPalettePreview_MouseClick);
@@ -113,48 +113,48 @@
 			this.tlpColorValues.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
 			this.tlpColorValues.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
 			this.tlpColorValues.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-			this.tlpColorValues.Size = new System.Drawing.Size(352, 100);
+			this.tlpColorValues.Size = new System.Drawing.Size(376, 100);
 			this.tlpColorValues.TabIndex = 3;
 			// 
 			// nudRed
 			// 
 			this.nudRed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.nudRed.Location = new System.Drawing.Point(143, 3);
+			this.nudRed.Location = new System.Drawing.Point(153, 3);
 			this.nudRed.Maximum = new decimal(new int[] {
             31,
             0,
             0,
             0});
 			this.nudRed.Name = "nudRed";
-			this.nudRed.Size = new System.Drawing.Size(206, 20);
+			this.nudRed.Size = new System.Drawing.Size(220, 20);
 			this.nudRed.TabIndex = 0;
 			this.nudRed.ValueChanged += new System.EventHandler(this.nudRed_ValueChanged);
 			// 
 			// nudGreen
 			// 
 			this.nudGreen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.nudGreen.Location = new System.Drawing.Point(143, 28);
+			this.nudGreen.Location = new System.Drawing.Point(153, 28);
 			this.nudGreen.Maximum = new decimal(new int[] {
             31,
             0,
             0,
             0});
 			this.nudGreen.Name = "nudGreen";
-			this.nudGreen.Size = new System.Drawing.Size(206, 20);
+			this.nudGreen.Size = new System.Drawing.Size(220, 20);
 			this.nudGreen.TabIndex = 1;
 			this.nudGreen.ValueChanged += new System.EventHandler(this.nudGreen_ValueChanged);
 			// 
 			// nudBlue
 			// 
 			this.nudBlue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.nudBlue.Location = new System.Drawing.Point(143, 53);
+			this.nudBlue.Location = new System.Drawing.Point(153, 53);
 			this.nudBlue.Maximum = new decimal(new int[] {
             31,
             0,
             0,
             0});
 			this.nudBlue.Name = "nudBlue";
-			this.nudBlue.Size = new System.Drawing.Size(206, 20);
+			this.nudBlue.Size = new System.Drawing.Size(220, 20);
 			this.nudBlue.TabIndex = 2;
 			this.nudBlue.ValueChanged += new System.EventHandler(this.nudBlue_ValueChanged);
 			// 
@@ -164,7 +164,7 @@
 			this.labelRed.AutoSize = true;
 			this.labelRed.Location = new System.Drawing.Point(3, 6);
 			this.labelRed.Name = "labelRed";
-			this.labelRed.Size = new System.Drawing.Size(134, 13);
+			this.labelRed.Size = new System.Drawing.Size(144, 13);
 			this.labelRed.TabIndex = 3;
 			this.labelRed.Text = "&Red";
 			// 
@@ -174,7 +174,7 @@
 			this.labelGreen.AutoSize = true;
 			this.labelGreen.Location = new System.Drawing.Point(3, 31);
 			this.labelGreen.Name = "labelGreen";
-			this.labelGreen.Size = new System.Drawing.Size(134, 13);
+			this.labelGreen.Size = new System.Drawing.Size(144, 13);
 			this.labelGreen.TabIndex = 4;
 			this.labelGreen.Text = "&Green";
 			// 
@@ -184,7 +184,7 @@
 			this.labelBlue.AutoSize = true;
 			this.labelBlue.Location = new System.Drawing.Point(3, 56);
 			this.labelBlue.Name = "labelBlue";
-			this.labelBlue.Size = new System.Drawing.Size(134, 13);
+			this.labelBlue.Size = new System.Drawing.Size(144, 13);
 			this.labelBlue.TabIndex = 5;
 			this.labelBlue.Text = "&Blue";
 			// 
@@ -194,7 +194,7 @@
 			this.labelTransparent.AutoSize = true;
 			this.labelTransparent.Location = new System.Drawing.Point(3, 81);
 			this.labelTransparent.Name = "labelTransparent";
-			this.labelTransparent.Size = new System.Drawing.Size(134, 13);
+			this.labelTransparent.Size = new System.Drawing.Size(144, 13);
 			this.labelTransparent.TabIndex = 6;
 			this.labelTransparent.Text = "&Transparent";
 			// 
@@ -202,9 +202,9 @@
 			// 
 			this.cbTransparent.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.cbTransparent.AutoSize = true;
-			this.cbTransparent.Location = new System.Drawing.Point(143, 79);
+			this.cbTransparent.Location = new System.Drawing.Point(153, 79);
 			this.cbTransparent.Name = "cbTransparent";
-			this.cbTransparent.Size = new System.Drawing.Size(206, 17);
+			this.cbTransparent.Size = new System.Drawing.Size(220, 17);
 			this.cbTransparent.TabIndex = 7;
 			this.cbTransparent.Text = "Transparent";
 			this.cbTransparent.UseVisualStyleBackColor = true;
@@ -217,7 +217,7 @@
 			this.cbColorEntries.Location = new System.Drawing.Point(86, 12);
 			this.cbColorEntries.MaxDropDownItems = 16;
 			this.cbColorEntries.Name = "cbColorEntries";
-			this.cbColorEntries.Size = new System.Drawing.Size(384, 21);
+			this.cbColorEntries.Size = new System.Drawing.Size(408, 21);
 			this.cbColorEntries.TabIndex = 4;
 			this.cbColorEntries.SelectionChangeCommitted += new System.EventHandler(this.cbColorEntries_SelectionChangeCommitted);
 			// 
@@ -233,14 +233,14 @@
 			// panelCurColor
 			// 
 			this.panelCurColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.panelCurColor.Location = new System.Drawing.Point(370, 39);
+			this.panelCurColor.Location = new System.Drawing.Point(394, 39);
 			this.panelCurColor.Name = "panelCurColor";
 			this.panelCurColor.Size = new System.Drawing.Size(100, 100);
 			this.panelCurColor.TabIndex = 6;
 			// 
 			// buttonImport
 			// 
-			this.buttonImport.Location = new System.Drawing.Point(12, 307);
+			this.buttonImport.Location = new System.Drawing.Point(12, 437);
 			this.buttonImport.Name = "buttonImport";
 			this.buttonImport.Size = new System.Drawing.Size(75, 23);
 			this.buttonImport.TabIndex = 7;
@@ -250,7 +250,7 @@
 			// 
 			// buttonExport
 			// 
-			this.buttonExport.Location = new System.Drawing.Point(93, 307);
+			this.buttonExport.Location = new System.Drawing.Point(93, 437);
 			this.buttonExport.Name = "buttonExport";
 			this.buttonExport.Size = new System.Drawing.Size(75, 23);
 			this.buttonExport.TabIndex = 8;
@@ -262,7 +262,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(482, 342);
+			this.ClientSize = new System.Drawing.Size(516, 472);
 			this.Controls.Add(this.buttonExport);
 			this.Controls.Add(this.buttonImport);
 			this.Controls.Add(this.panelCurColor);
