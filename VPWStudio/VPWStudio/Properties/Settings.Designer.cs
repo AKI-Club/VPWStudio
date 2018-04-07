0179e7e25335939975058c766aadc9e68faa66b5
freem <aj187@ajworld.net>
2018-04-07 02:20:44 -0500

    add EmulatorExtraCommands to configuration currently unused, but will be exposed to UI later

--- VPWStudio/VPWStudio/Properties/Settings.Designer.cs
+++ VPWStudio/VPWStudio/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace VPWStudio.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.6.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,18 @@ namespace VPWStudio.Properties {
             }
             set {
                 this["EmulatorPath"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string EmulatorExtraCommands {
+            get {
+                return ((string)(this["EmulatorExtraCommands"]));
+            }
+            set {
+                this["EmulatorExtraCommands"] = value;
             }
         }
     }
