0db62e9b43c6b1da2077e0e87a988dc8182f658f
freem <aj187@ajworld.net>
2018-04-10 14:48:57 -0500

    add icons for AkiArchive and AkiModel filetypes

--- VPWStudio/VPWStudio/Properties/Resources.Designer.cs
+++ VPWStudio/VPWStudio/Properties/Resources.Designer.cs
@@ -73,9 +73,29 @@ namespace VPWStudio.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap FileType_AkiArchive {
+            get {
+                object obj = ResourceManager.GetObject("FileType_AkiArchive", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap FileType_AkiLargeFont {
             get {
                 object obj = ResourceManager.GetObject("FileType_AkiLargeFont", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap FileType_AkiModel {
+            get {
+                object obj = ResourceManager.GetObject("FileType_AkiModel", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
