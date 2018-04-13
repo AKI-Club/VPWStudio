cead817eec3e82222f87603b67c35c36105bab07
freem <aj187@ajworld.net>
2018-04-12 19:19:15 -0500

    [DefaultGameData] Multiple changes - grouped like items - add SetupFT_FTLocation for Revenge NTSC/PAL, WM2K NTSC-U/J/PAL, No Mercy all versions (non-final versions; needs to be tweaked to match...) - add SetupFT_FTLocation for VPW2, using the intended address and offset

--- VPWStudio/VPWStudio/ProgStructures/DefaultGameData.cs
+++ VPWStudio/VPWStudio/ProgStructures/DefaultGameData.cs
@@ -58,40 +58,56 @@ namespace VPWStudio
 			{
 				SpecificGame.WorldTour_NTSC_U_10,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
-					{ "StableDefs", new DefaultLocationDataEntry(0x37EC8, 96) },
-					{ "FirstFile", new DefaultLocationDataEntry(0x39490, 0) },
-					{ "FileTable", new DefaultLocationDataEntry(0x7C1A78, 21996) },
+					// code changes
+
+					// data defs
 					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x2F100, 176) },
 					//{ "CostumeDefs", new DefaultLocationDataEntry(0x3368C, ?) },
+					{ "StableDefs", new DefaultLocationDataEntry(0x37EC8, 96) },
+
+					// filetable-related
+					{ "FirstFile", new DefaultLocationDataEntry(0x39490, 0) },
+					{ "FileTable", new DefaultLocationDataEntry(0x7C1A78, 21996) },
 				})
 			},
 			{
 				SpecificGame.WorldTour_NTSC_U_11,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
-					{ "StableDefs", new DefaultLocationDataEntry(0x37F38, 96) },
-					{ "FirstFile", new DefaultLocationDataEntry(0x39500, 0) },
-					{ "FileTable", new DefaultLocationDataEntry(0x7C1C70, 21996) },
+					// code changes
+
+					// data defs
 					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x2F170, 176) },
 					//{ "CostumeDefs", new DefaultLocationDataEntry(0x336FC, ?) },
+					{ "StableDefs", new DefaultLocationDataEntry(0x37F38, 96) },
+
+					// filetable-related
+					{ "FirstFile", new DefaultLocationDataEntry(0x39500, 0) },
+					{ "FileTable", new DefaultLocationDataEntry(0x7C1C70, 21996) },
 				})
 			},
 			{
 				SpecificGame.WorldTour_PAL,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+
+					// data defs
+					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x2F150, 176) },
 					{ "StableDefs", new DefaultLocationDataEntry(0x37EC8, 96) },
+					//{ "CostumeDefs", new DefaultLocationDataEntry(0x336DC, ?) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x39490, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x7C1C00, 21996) },
-					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x2F150, 176) },
-					//{ "CostumeDefs", new DefaultLocationDataEntry(0x336DC, ?) },
 				})
 			},
 			{
 				SpecificGame.VPW64_NTSC_J,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
-					{ "StableDefs", new DefaultLocationDataEntry(0x4989C, 176) },
-					{ "FirstFile", new DefaultLocationDataEntry(0x4AD00, 0) },
-					{ "FileTable", new DefaultLocationDataEntry(0xC7B578, 37432) },
+					// code changes
+
+					// data defs
 					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x2FCE4, 176) },
+					{ "StableDefs", new DefaultLocationDataEntry(0x4989C, 176) },
 
 					// costume definitions
 					{ "VPW64Costumes_Small", new DefaultLocationDataEntry(0x3EE40, 504) },
@@ -102,32 +118,54 @@ namespace VPWStudio
 					{ "VPW64Costumes_Judoka", new DefaultLocationDataEntry(0x31CB4, 8) },
 					{ "VPW64Costumes_Female", new DefaultLocationDataEntry(0x31D60, 8) },
 					{ "VPW64Costumes_Unused", new DefaultLocationDataEntry(0x31EAC, 16) },
+
+					// filetable-related
+					{ "FirstFile", new DefaultLocationDataEntry(0x4AD00, 0) },
+					{ "FileTable", new DefaultLocationDataEntry(0xC7B578, 37432) },
 				})
 			},
 			{
 				SpecificGame.Revenge_NTSC_U,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
-					{ "StableDefs", new DefaultLocationDataEntry(0x3B0F8, 104) },
-					{ "FirstFile", new DefaultLocationDataEntry(0xDAC50, 0) },
-					{ "FileTable", new DefaultLocationDataEntry(0xCE2752, 30632) },
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x40C8, 8) },
+
+					// data defs
 					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x323F0, 208) },
 					{ "CostumeDefs", new DefaultLocationDataEntry(0x36AA4, 592) },
+					{ "StableDefs", new DefaultLocationDataEntry(0x3B0F8, 104) },
+
+					// filetable-related
+					{ "FirstFile", new DefaultLocationDataEntry(0xDAC50, 0) },
+					{ "FileTable", new DefaultLocationDataEntry(0xCE2752, 30632) },
 				})
 			},
 			{
 				SpecificGame.Revenge_PAL,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x3FE8, 8) },
+
+					// data defs
 					{ "StableDefs", new DefaultLocationDataEntry(0x38548, 104) },
-					{ "FirstFile", new DefaultLocationDataEntry(0xD81E0, 0) },
-					{ "FileTable", new DefaultLocationDataEntry(0xCDFCE2, 30632) },
 					{ "BodyTypeDefs", new DefaultLocationDataEntry(0x2FB40, 208) },
 					{ "CostumeDefs", new DefaultLocationDataEntry(0x341F4, 592) },
+
+					// filetable-related
+					{ "FirstFile", new DefaultLocationDataEntry(0xD81E0, 0) },
+					{ "FileTable", new DefaultLocationDataEntry(0xCDFCE2, 30632) },
 				})
 			},
 			{
 				SpecificGame.WM2K_NTSC_U,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x48E8, 8) },
+
+					// data defs
 					{ "StableDefs", new DefaultLocationDataEntry(0x41EE0, 132) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x144AA0, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x11778BE, 41248) },
 				})
@@ -135,7 +173,13 @@ namespace VPWStudio
 			{
 				SpecificGame.WM2K_NTSC_J,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x4788, 8) },
+
+					// data defs
 					{ "StableDefs", new DefaultLocationDataEntry(0x3F980, 132) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x12C070, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x116F3C2, 41480) },
 				})
@@ -143,7 +187,13 @@ namespace VPWStudio
 			{
 				SpecificGame.WM2K_PAL,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x48E8, 8) },
+
+					// data defs
 					// todo: StableDefs
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x144AC0, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x11778BE, 41248) },
 				})
@@ -151,6 +201,12 @@ namespace VPWStudio
 			{
 				SpecificGame.VPW2_NTSC_J,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x48DA, 4) }, // ok for new version
+
+					// data defs
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x152DF0, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x1310F40, 52364) },
 				})
@@ -158,7 +214,13 @@ namespace VPWStudio
 			{
 				SpecificGame.NoMercy_NTSC_U_10,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x46DC, 8) },
+
+					// data defs
 					{ "WrestlerDefs", new DefaultLocationDataEntry(0x46658, 0) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x1BD1B0, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x16C3238, 77848) },
 				})
@@ -166,7 +228,13 @@ namespace VPWStudio
 			{
 				SpecificGame.NoMercy_NTSC_U_11,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x46DC, 8) },
+
+					// data defs
 					{ "WrestlerDefs", new DefaultLocationDataEntry(0x465B8, 0) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x1BD150, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x16C31D8, 77848) },
 				})
@@ -174,15 +242,28 @@ namespace VPWStudio
 			{
 				SpecificGame.NoMercy_PAL_10,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x46DC, 8) },
+
+					// data defs
 					{ "WrestlerDefs", new DefaultLocationDataEntry(0x46658, 0) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x1BD220, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x16C32A8, 77848) },
+					
 				})
 			},
 			{
 				SpecificGame.NoMercy_PAL_11,
 				new DefaultLocationData(new Dictionary<string, DefaultLocationDataEntry>(){
+					// code changes
+					{ "SetupFT_FTLocation", new DefaultLocationDataEntry(0x46DC, 8) },
+
+					// data defs
 					{ "WrestlerDefs", new DefaultLocationDataEntry(0x464B8, 0) },
+
+					// filetable-related
 					{ "FirstFile", new DefaultLocationDataEntry(0x1BD0C0, 0) },
 					{ "FileTable", new DefaultLocationDataEntry(0x16C3148, 77848) },
 				})
