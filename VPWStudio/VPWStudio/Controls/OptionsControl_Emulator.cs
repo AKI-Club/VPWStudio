e02b7c17aaf360d9ed884b29be2e182883698331
freem <aj187@ajworld.net>
2018-04-14 21:29:02 -0500

    add OptionsControl_Emulator

---
+++ VPWStudio/VPWStudio/Controls/OptionsControl_Emulator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace VPWStudio.Controls
+{
+	public partial class OptionsControl_Emulator : UserControl
+	{
+		public OptionsControl_Emulator()
+		{
+			InitializeComponent();
+		}
+
+		private void buttonBrowse_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Title = "Select Emulator Executable";
+			ofd.Filter = "Executable Files (*.exe)|*.exe|All Files (*.*)|*.*";
+			if (ofd.ShowDialog() == DialogResult.OK)
+			{
+				tbEmuPath.Text = ofd.FileName;
+			}
+		}
+	}
+}
