32d25d9756c9caf5a6e5f2cb96e65e2646cff1e6
freem <aj187@ajworld.net>
2018-04-14 20:27:30 -0500

    [AkiModel] "this." removal and splitting out $CONSTRUCTOR(BinaryReader) to ReadData(BinaryReader)

--- VPWStudio/VPWStudio/DataStructures/AKI/AkiModel.cs
+++ VPWStudio/VPWStudio/DataStructures/AKI/AkiModel.cs
@@ -20,6 +20,7 @@ namespace VPWStudio
 	/// </summary>
 	public class AkiVertex
 	{
+		#region Members
 		/// <summary>
 		/// X position
 		/// </summary>
@@ -44,18 +45,20 @@ namespace VPWStudio
 		/// Vertex color
 		/// </summary>
 		public Color VertexColor;
+		#endregion
 
+		#region Constructors
 		/// <summary>
 		/// Default Constructor
 		/// </summary>
 		public AkiVertex()
 		{
-			this.X = 0;
-			this.Y = 0;
-			this.Z = 0;
-			this.U = 0;
-			this.V = 0;
-			this.VertexColor = Color.White;
+			X = 0;
+			Y = 0;
+			Z = 0;
+			U = 0;
+			V = 0;
+			VertexColor = Color.White;
 		}
 
 		/// <summary>
@@ -66,12 +69,12 @@ namespace VPWStudio
 		/// <param name="z">Z position</param>
 		public AkiVertex(int x, int y, int z)
 		{
-			this.X = (SByte)x;
-			this.Y = (SByte)y;
-			this.Z = (SByte)z;
-			this.U = (SByte)0;
-			this.V = (SByte)0;
-			this.VertexColor = Color.White;
+			X = (SByte)x;
+			Y = (SByte)y;
+			Z = (SByte)z;
+			U = 0;
+			V = 0;
+			VertexColor = Color.White;
 		}
 
 		/// <summary>
@@ -84,12 +87,12 @@ namespace VPWStudio
 		/// <param name="v">Texture Map Vertical offset</param>
 		public AkiVertex(int x, int y, int z, int u, int v)
 		{
-			this.X = (SByte)x;
-			this.Y = (SByte)y;
-			this.Z = (SByte)z;
-			this.U = (SByte)u;
-			this.V = (SByte)v;
-			this.VertexColor = Color.White;
+			X = (SByte)x;
+			Y = (SByte)y;
+			Z = (SByte)z;
+			U = (SByte)u;
+			V = (SByte)v;
+			VertexColor = Color.White;
 		}
 
 		/// <summary>
@@ -103,12 +106,12 @@ namespace VPWStudio
 		/// <param name="c">Vertex color</param>
 		public AkiVertex(int x, int y, int z, int u, int v, Color c)
 		{
-			this.X = (SByte)x;
-			this.Y = (SByte)y;
-			this.Z = (SByte)z;
-			this.U = (SByte)u;
-			this.V = (SByte)v;
-			this.VertexColor = c;
+			X = (SByte)x;
+			Y = (SByte)y;
+			Z = (SByte)z;
+			U = (SByte)u;
+			V = (SByte)v;
+			VertexColor = c;
 		}
 
 		/// <summary>
@@ -117,59 +120,68 @@ namespace VPWStudio
 		/// <param name="br">BinaryReader instance to use.</param>
 		public AkiVertex(BinaryReader br)
 		{
-			this.X = (SByte)br.ReadByte();
-			this.Y = (SByte)br.ReadByte();
-			this.Z = (SByte)br.ReadByte();
+			ReadData(br);
+		}
+		#endregion
+
+		#region Binary Read/Write
+		public void ReadData(BinaryReader br)
+		{
+			X = (SByte)br.ReadByte();
+			Y = (SByte)br.ReadByte();
+			Z = (SByte)br.ReadByte();
 			// todo: convert UV values (63,0,31) -> (-1.0, 0.0, 1.0)
-			this.U = (SByte)br.ReadByte();
-			this.V = (SByte)br.ReadByte();
+			U = (SByte)br.ReadByte();
+			V = (SByte)br.ReadByte();
 			int red = br.ReadByte();
 			int green = br.ReadByte();
 			int blue = br.ReadByte();
-			this.VertexColor = Color.FromArgb(1, red, green, blue);
+			VertexColor = Color.FromArgb(1, red, green, blue);
 		}
 
 		/// <summary>
 		/// Write data to a BinaryWriter.
 		/// </summary>
 		/// <param name="bw">BinaryWriter instance to use.</param>
-		public void WriteBytes(BinaryWriter bw)
+		public void WriteData(BinaryWriter bw)
 		{
-			bw.Write((byte)this.X);
-			bw.Write((byte)this.Y);
-			bw.Write((byte)this.Z);
+			bw.Write((byte)X);
+			bw.Write((byte)Y);
+			bw.Write((byte)Z);
 			// todo: convert UV values (-1.0, 0.0, 1.0) -> (63,0,31)
-			bw.Write((SByte)this.U);
-			bw.Write((SByte)this.V);
-			bw.Write((byte)this.VertexColor.R);
-			bw.Write((byte)this.VertexColor.G);
-			bw.Write((byte)this.VertexColor.B);
+			bw.Write((SByte)U);
+			bw.Write((SByte)V);
+			bw.Write(VertexColor.R);
+			bw.Write(VertexColor.G);
+			bw.Write(VertexColor.B);
 		}
+		#endregion
 
+		#region Helpers
 		public float[] UVToFloat()
 		{
 			float[] values = new float[2];
 
-			if (this.U > 31)
+			if (U > 31)
 			{
 				// negative
-				values[0] = ((this.U - 32) / 31) * -1;
+				values[0] = ((U - 32) / 31) * -1;
 			}
 			else
 			{
 				// positive
-				values[0] = (this.U / 31);
+				values[0] = (U / 31);
 			}
 
-			if (this.V > 31)
+			if (V > 31)
 			{
 				// negative
-				values[1] = ((this.V - 32) / 31) * -1;
+				values[1] = ((V - 32) / 31) * -1;
 			}
 			else
 			{
 				// positive
-				values[1] = (this.V / 31);
+				values[1] = (V / 31);
 			}
 
 			return values;
@@ -179,6 +191,7 @@ namespace VPWStudio
 		{
 			// todo.
 		}
+		#endregion
 	}
 
 	/// <summary>
@@ -186,18 +199,21 @@ namespace VPWStudio
 	/// </summary>
 	public class AkiFace
 	{
+		#region Members
 		public int Vertex1;
 		public int Vertex2;
 		public int Vertex3;
+		#endregion
 
+		#region Constructors
 		/// <summary>
 		/// Default Constructor
 		/// </summary>
 		public AkiFace()
 		{
-			this.Vertex1 = 0;
-			this.Vertex2 = 0;
-			this.Vertex3 = 0;
+			Vertex1 = 0;
+			Vertex2 = 0;
+			Vertex3 = 0;
 		}
 
 		/// <summary>
@@ -208,9 +224,9 @@ namespace VPWStudio
 		/// <param name="v3">Vertex Index 3</param>
 		public AkiFace(int v1, int v2, int v3)
 		{
-			this.Vertex1 = v1;
-			this.Vertex2 = v2;
-			this.Vertex3 = v3;
+			Vertex1 = v1;
+			Vertex2 = v2;
+			Vertex3 = v3;
 		}
 
 		/// <summary>
@@ -219,21 +235,29 @@ namespace VPWStudio
 		/// <param name="br">BinaryReader instance to use.</param>
 		public AkiFace(BinaryReader br)
 		{
-			this.Vertex1 = br.ReadByte();
-			this.Vertex2 = br.ReadByte();
-			this.Vertex3 = br.ReadByte();
+			ReadData(br);
+		}
+		#endregion
+
+		#region Binary Read/Write
+		public void ReadData(BinaryReader br)
+		{
+			Vertex1 = br.ReadByte();
+			Vertex2 = br.ReadByte();
+			Vertex3 = br.ReadByte();
 		}
 
 		/// <summary>
 		/// Write data to a BinaryWriter.
 		/// </summary>
 		/// <param name="bw">BinaryWriter instance to use.</param>
-		public void WriteBytes(BinaryWriter bw)
+		public void WriteData(BinaryWriter bw)
 		{
-			bw.Write((byte)this.Vertex1);
-			bw.Write((byte)this.Vertex2);
-			bw.Write((byte)this.Vertex3);
+			bw.Write((byte)Vertex1);
+			bw.Write((byte)Vertex2);
+			bw.Write((byte)Vertex3);
 		}
+		#endregion
 	}
 
 	/// <summary>
@@ -241,6 +265,7 @@ namespace VPWStudio
 	/// </summary>
 	public class AkiModel
 	{
+		#region Members
 		/// <summary>
 		/// Scale value
 		/// </summary>
@@ -294,7 +319,9 @@ namespace VPWStudio
 		public List<AkiFace> Faces;
 
 		public byte ModelType;
+		#endregion
 
+		#region Constructors
 		/// <summary>
 		/// Default Constructor
 		/// </summary>
@@ -320,27 +347,34 @@ namespace VPWStudio
 		/// <param name="br">BinaryReader instance to use.</param>
 		public AkiModel(BinaryReader br)
 		{
-			this.Scale = br.ReadByte();
-			byte numVerts = br.ReadByte();
-			this.NumVertices = numVerts & 0x7F;
-			this.ModelType = (byte)(numVerts & 0x80);
-			this.NumFaces = br.ReadByte() & 0x7F;
-			this.UnknownValue = br.ReadByte();
-			this.OffsetX = (SByte)br.ReadByte();
-			this.OffsetY = (SByte)br.ReadByte();
-			this.OffsetZ = (SByte)br.ReadByte();
-			this.OffsetTexture = (SByte)br.ReadByte();
+			ReadData(br);
+		}
+		#endregion
 
-			this.Vertices = new List<AkiVertex>();
+		#region Binary Read/Write
+		public void ReadData(BinaryReader br)
+		{
+			Scale = br.ReadByte();
+			byte numVerts = br.ReadByte();
+			NumVertices = numVerts & 0x7F;
+			ModelType = (byte)(numVerts & 0x80);
+			NumFaces = br.ReadByte() & 0x7F;
+			UnknownValue = br.ReadByte();
+			OffsetX = (SByte)br.ReadByte();
+			OffsetY = (SByte)br.ReadByte();
+			OffsetZ = (SByte)br.ReadByte();
+			OffsetTexture = (SByte)br.ReadByte();
+
+			Vertices = new List<AkiVertex>();
 			for (int v = 0; v < this.NumVertices; v++)
 			{
-				this.Vertices.Add(new AkiVertex(br));
+				Vertices.Add(new AkiVertex(br));
 			}
 
-			this.Faces = new List<AkiFace>();
-			for (int f = 0; f < this.NumFaces; f++)
+			Faces = new List<AkiFace>();
+			for (int f = 0; f < NumFaces; f++)
 			{
-				this.Faces.Add(new AkiFace(br));
+				Faces.Add(new AkiFace(br));
 			}
 		}
 
@@ -348,25 +382,26 @@ namespace VPWStudio
 		/// Write data to a BinaryWriter.
 		/// </summary>
 		/// <param name="bw">BinaryWriter instance to use.</param>
-		public void WriteBytes(BinaryWriter bw)
+		public void WriteData(BinaryWriter bw)
 		{
-			bw.Write((byte)this.Scale);
-			bw.Write((byte)this.Vertices.Count);
-			bw.Write((byte)this.Faces.Count);
+			bw.Write((byte)Scale);
+			bw.Write((byte)Vertices.Count);
+			bw.Write((byte)Faces.Count);
 			bw.Write((byte)UnknownValue);
-			bw.Write((byte)this.OffsetX);
-			bw.Write((byte)this.OffsetY);
-			bw.Write((byte)this.OffsetZ);
-			bw.Write((byte)this.OffsetTexture);
-			foreach (AkiVertex v in this.Vertices)
+			bw.Write((byte)OffsetX);
+			bw.Write((byte)OffsetY);
+			bw.Write((byte)OffsetZ);
+			bw.Write((byte)OffsetTexture);
+			foreach (AkiVertex v in Vertices)
 			{
-				v.WriteBytes(bw);
+				v.WriteData(bw);
 			}
-			foreach (AkiFace f in this.Faces)
+			foreach (AkiFace f in Faces)
 			{
-				f.WriteBytes(bw);
+				f.WriteData(bw);
 			}
 		}
+		#endregion
 
 		// wip shite
 		public void WriteWavefrontObj(StreamWriter sw)
@@ -375,23 +410,23 @@ namespace VPWStudio
 			// todo: obj format doesn't officially support vertex colors
 
 			sw.WriteLine();
-			sw.WriteLine(String.Format("# Scale Value: {0}", this.Scale));
+			sw.WriteLine(String.Format("# Scale Value: {0}", Scale));
 			sw.WriteLine();
 
-			sw.WriteLine(String.Format("# Vertices: {0}", this.Vertices.Count));
-			foreach (AkiVertex v in this.Vertices)
+			sw.WriteLine(String.Format("# Vertices: {0}", Vertices.Count));
+			foreach (AkiVertex v in Vertices)
 			{
 				sw.WriteLine(String.Format("v {0} {1} {2}",
-					(float)((v.X + this.OffsetX) * (this.Scale+1)),
-					(float)((v.Y + this.OffsetY) * (this.Scale+1)),
-					(float)((v.Z + this.OffsetZ) * (this.Scale+1))
+					(float)((v.X + OffsetX) * (Scale+1)),
+					(float)((v.Y + OffsetY) * (Scale+1)),
+					(float)((v.Z + OffsetZ) * (Scale+1))
 					)
 				);
 			}
 			sw.WriteLine();
 
 			sw.WriteLine("# Texture/UV");
-			foreach (AkiVertex v in this.Vertices)
+			foreach (AkiVertex v in Vertices)
 			{
 				float[] fUV = v.UVToFloat();
 
@@ -403,8 +438,8 @@ namespace VPWStudio
 			}
 			sw.WriteLine();
 
-			sw.WriteLine(String.Format("# Faces: {0}", this.Faces.Count));
-			foreach (AkiFace f in this.Faces)
+			sw.WriteLine(String.Format("# Faces: {0}", Faces.Count));
+			foreach (AkiFace f in Faces)
 			{
 				//sw.WriteLine(String.Format("f {0} {1} {2}", f.Vertex1+1, f.Vertex2+1, f.Vertex3+1));
 				sw.WriteLine(String.Format("f {0}/{0} {1}/{1} {2}/{2}", f.Vertex1 + 1, f.Vertex2 + 1, f.Vertex3 + 1));
