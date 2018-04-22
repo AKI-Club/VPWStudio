c020ffdafcd8eeac3b313d8a031956518bc00b36
freem <aj187@ajworld.net>
2018-04-22 09:34:36 -0500

    ModelTool2: so early, I shouldn't even be committing it, but here we are

---
+++ VPWStudio/VPWStudio/Tools/ModelTool2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// todo: what am I doing
+using SharpDX;
+
+namespace VPWStudio
+{
+	/// <summary>
+	/// ModelTool2, an attempt to test SharpDX
+	/// </summary>
+	public partial class ModelTool2 : Form
+	{
+		private AkiModel CurModel = new AkiModel();
+
+		public ModelTool2()
+		{
+			InitializeComponent();
+		}
+
+		private void LoadModel(int fileID)
+		{
+		}
+	}
+}
