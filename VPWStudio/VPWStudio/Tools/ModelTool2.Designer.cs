6d32c098feaca0794d94f7b3de59a219b28bb3ce
freem <aj187@ajworld.net>
2018-04-22 09:34:36 -0500

    ModelTool2: so early, I shouldn't even be committing it, but here we are

---
+++ VPWStudio/VPWStudio/Tools/ModelTool2.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace VPWStudio
+{
+	partial class ModelTool2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.SuspendLayout();
+			// 
+			// ModelTool2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(632, 453);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "ModelTool2";
+			this.Text = "ModelTool2";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
