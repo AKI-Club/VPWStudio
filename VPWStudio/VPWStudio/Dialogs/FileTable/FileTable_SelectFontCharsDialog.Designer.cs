6ace80a09e46ed5db4b1011ff144f8e0ee6f746f
freem <aj187@ajworld.net>
2018-05-19 14:30:45 -0500

    add FileTable_SelectFontCharsDialog

---
+++ VPWStudio/VPWStudio/Dialogs/FileTable/FileTable_SelectFontCharsDialog.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace VPWStudio
+{
+	partial class FileTable_SelectFontCharsDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonOK = new System.Windows.Forms.Button();
+			this.buttonCancel = new System.Windows.Forms.Button();
+			this.cbFontCharsFile = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// buttonOK
+			// 
+			this.buttonOK.Location = new System.Drawing.Point(12, 39);
+			this.buttonOK.Name = "buttonOK";
+			this.buttonOK.Size = new System.Drawing.Size(75, 23);
+			this.buttonOK.TabIndex = 0;
+			this.buttonOK.Text = "&OK";
+			this.buttonOK.UseVisualStyleBackColor = true;
+			this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+			// 
+			// buttonCancel
+			// 
+			this.buttonCancel.Location = new System.Drawing.Point(199, 39);
+			this.buttonCancel.Name = "buttonCancel";
+			this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+			this.buttonCancel.TabIndex = 1;
+			this.buttonCancel.Text = "&Cancel";
+			this.buttonCancel.UseVisualStyleBackColor = true;
+			this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+			// 
+			// cbFontCharsFile
+			// 
+			this.cbFontCharsFile.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbFontCharsFile.FormattingEnabled = true;
+			this.cbFontCharsFile.Location = new System.Drawing.Point(12, 12);
+			this.cbFontCharsFile.Name = "cbFontCharsFile";
+			this.cbFontCharsFile.Size = new System.Drawing.Size(262, 21);
+			this.cbFontCharsFile.TabIndex = 2;
+			// 
+			// FileTable_SelectFontCharsDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(286, 74);
+			this.Controls.Add(this.cbFontCharsFile);
+			this.Controls.Add(this.buttonCancel);
+			this.Controls.Add(this.buttonOK);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "FileTable_SelectFontCharsDialog";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Select FontChars File";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonOK;
+		private System.Windows.Forms.Button buttonCancel;
+		private System.Windows.Forms.ComboBox cbFontCharsFile;
+	}
+}
