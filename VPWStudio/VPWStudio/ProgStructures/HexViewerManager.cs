4a0a10368cc7213c13d6323e4f8efb911b06a34e
freem <aj187@ajworld.net>
2018-07-13 01:18:33 -0500

    at some point i thought it'd be a good idea to have a manager for the hexviewer the general idea is to allow multiple windows and prevent opening the same data twice

---
+++ VPWStudio/VPWStudio/ProgStructures/HexViewerManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace VPWStudio
+{
+	/// <summary>
+	/// Manager class for the HexViewer.
+	/// </summary>
+	public class HexViewerManager
+	{
+		// the goal of this class is to handle multiple hexviewers while not summoning more views than needed.
+		// the implied key for each entry is a hash of the data.
+		public Dictionary<string, HexViewer> ActiveHexViewers;
+
+		public HexViewerManager()
+		{
+			ActiveHexViewers = new Dictionary<string, HexViewer>();
+		}
+	}
+}
