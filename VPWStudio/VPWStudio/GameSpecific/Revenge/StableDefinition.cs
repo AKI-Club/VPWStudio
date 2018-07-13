fefd8864650fe023831982d097139cfd3cacd16c
freem <aj187@ajworld.net>
2018-07-13 01:07:57 -0500

    remove unnecessary xml using statements

--- VPWStudio/VPWStudio/GameSpecific/Revenge/StableDefinition.cs
+++ VPWStudio/VPWStudio/GameSpecific/Revenge/StableDefinition.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Xml;
-using System.Xml.Schema;
-using System.Xml.Serialization;
 
 namespace VPWStudio.GameSpecific.Revenge
 {
