fd19f0ba5d16f1bac8a566f08c9f88421ddf51dc
freem <aj187@ajworld.net>
2018-07-13 01:11:08 -0500

    adding WIP championship and stable def classes for world tour/vpw64

---
+++ VPWStudio/VPWStudio/GameSpecific/ChampionshipDef_Early.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+
+namespace VPWStudio.GameSpecific
+{
+	/// <summary>
+	/// Championship definition for WCW vs. nWo World Tour and Virtual Pro-Wrestling 64.
+	/// </summary>
+	/// I forget if Revenge uses something similar offhand and I'm too lazy to check right now.
+	public class ChampionshipDef_Early
+	{
+		// a single championship data is 16 bytes
+		#region Class Members
+		public byte Unknown0;
+		public byte Unknown1;
+		public byte Unknown2;
+		public byte Unknown3;
+		public byte Unknown4; // weight class
+		public byte Unknown5; // match type
+		public byte Unknown6; // (related to icon)
+		public byte Unknown7; // (related to icon)
+
+		public UInt16 Unknown8;
+		public UInt16 Unknown10;
+
+		/// <summary>
+		/// Pointer to name for this Championship.
+		/// </summary>
+		public UInt32 NamePointer;
+		#endregion
+
+		#region Program-specific
+		/// <summary>
+		/// Actual championship name.
+		/// </summary>
+		public string ChampionshipName;
+		#endregion
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public ChampionshipDef_Early()
+		{
+			Unknown0 = 0;
+			Unknown1 = 0;
+			Unknown2 = 0;
+			Unknown3 = 0;
+			Unknown4 = 0;
+			Unknown5 = 0;
+			Unknown6 = 0;
+			Unknown7 = 0;
+
+			Unknown8 = 0;
+			Unknown10 = 0;
+			NamePointer = 0;
+			ChampionshipName = String.Empty;
+		}
+
+		#region Binary Read/Write
+		public void ReadData(BinaryReader br)
+		{
+			Unknown0 = br.ReadByte();
+			Unknown1 = br.ReadByte();
+			Unknown2 = br.ReadByte();
+			Unknown3 = br.ReadByte();
+			Unknown4 = br.ReadByte();
+			Unknown5 = br.ReadByte();
+			Unknown6 = br.ReadByte();
+			Unknown7 = br.ReadByte();
+
+			byte[] unk8 = br.ReadBytes(2);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(unk8);
+			}
+			Unknown8 = BitConverter.ToUInt16(unk8, 0);
+
+			byte[] unk10 = br.ReadBytes(2);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(unk10);
+			}
+			Unknown10 = BitConverter.ToUInt16(unk10, 0);
+
+			// then handle the name pointer
+			byte[] namePtr = br.ReadBytes(4);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(namePtr);
+			}
+			NamePointer = BitConverter.ToUInt32(namePtr, 0);
+		}
+
+		public void WriteData(BinaryWriter bw)
+		{
+			bw.Write(Unknown0);
+			bw.Write(Unknown1);
+			bw.Write(Unknown2);
+			bw.Write(Unknown3);
+			bw.Write(Unknown4);
+			bw.Write(Unknown5);
+			bw.Write(Unknown6);
+			bw.Write(Unknown7);
+
+			byte[] unk8 = BitConverter.GetBytes(Unknown8);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(unk8);
+			}
+			bw.Write(unk8);
+
+			byte[] unk10 = BitConverter.GetBytes(Unknown10);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(unk10);
+			}
+			bw.Write(unk10);
+
+			byte[] namePtr = BitConverter.GetBytes(NamePointer);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(namePtr);
+			}
+			bw.Write(namePtr);
+		}
+		#endregion
+	}
+}
---
+++ VPWStudio/VPWStudio/GameSpecific/StableDef_Early.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.IO;
+
+namespace VPWStudio.GameSpecific
+{
+	/// <summary>
+	/// Shared Stable definition for World Tour and VPW64.
+	/// </summary>
+	public class StableDef_Early
+	{
+		#region Class Members
+		/// <summary>
+		/// Pointer to Wrestler Definitions for this Stable.
+		/// </summary>
+		public UInt32 WrestlerPointerStart;
+
+		/// <summary>
+		/// Number of wrestlers in this Stable.
+		/// </summary>
+		public UInt32 NumWrestlers;
+
+		// pointer to championships
+
+		/// <summary>
+		/// Pointer to Championship Definitions for this Stable.
+		/// </summary>
+		public UInt32 ChampionshipPointerStart;
+
+		/// <summary>
+		/// Number of championships associated with this Stable.
+		/// </summary>
+		public UInt32 NumChampionships;
+		#endregion
+
+		#region Program-Specific
+		/// <summary>
+		/// Pointers to wrestler definitions used in this Stable.
+		/// </summary>
+		public UInt32[] WrestlerPointers;
+
+		/// <summary>
+		/// Championship data tied to this Stable.
+		/// </summary>
+		public ChampionshipDef_Early[] ChampionshipData;
+		#endregion
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public StableDef_Early()
+		{
+			WrestlerPointerStart = 0;
+			NumWrestlers = 0;
+			ChampionshipPointerStart = 0;
+			NumChampionships = 0;
+			WrestlerPointers = null;
+			ChampionshipData = null;
+		}
+
+		#region Binary Read/Write
+		/// <summary>
+		/// Read StableDef_Early data using a BinaryReader.
+		/// </summary>
+		/// <param name="br">BinaryReader instance to use.</param>
+		public void ReadData(BinaryReader br)
+		{
+			byte[] wresPtr = br.ReadBytes(4);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(wresPtr);
+			}
+			WrestlerPointerStart = BitConverter.ToUInt32(wresPtr,0 );
+
+			byte[] numWres = br.ReadBytes(4);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(numWres);
+			}
+			NumWrestlers = BitConverter.ToUInt32(numWres, 0);
+
+			byte[] champPtr = br.ReadBytes(4);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(champPtr);
+			}
+			ChampionshipPointerStart = BitConverter.ToUInt32(champPtr, 0);
+
+			byte[] numTitles = br.ReadBytes(4);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(numTitles);
+			}
+			NumChampionships = BitConverter.ToUInt32(numTitles, 0);
+
+			// save location
+			long curLoc = br.BaseStream.Position;
+
+			WrestlerPointers = new UInt32[NumWrestlers];
+			br.BaseStream.Seek(Z64Rom.PointerToRom(WrestlerPointerStart), SeekOrigin.Begin);
+			// read wrestler pointers
+			for (int i = 0; i < NumWrestlers; i++)
+			{
+				byte[] ptr = br.ReadBytes(4);
+				if (BitConverter.IsLittleEndian)
+				{
+					Array.Reverse(ptr);
+				}
+				WrestlerPointers[i] = BitConverter.ToUInt32(ptr, 0);
+			}
+
+			ChampionshipData = new ChampionshipDef_Early[NumChampionships];
+			// jump to championship pointers
+			br.BaseStream.Seek(Z64Rom.PointerToRom(ChampionshipPointerStart), SeekOrigin.Begin);
+			// read championship data
+			for (int i = 0; i < NumChampionships; i++)
+			{
+				ChampionshipData[i] = new ChampionshipDef_Early();
+				ChampionshipData[i].ReadData(br);
+			}
+
+			// reload location
+			br.BaseStream.Seek(curLoc, SeekOrigin.Begin);
+		}
+
+		/// <summary>
+		/// Write StableDef_Early data using a BinaryWriter.
+		/// </summary>
+		/// <param name="bw">BinaryWriter instance to use.</param>
+		public void WriteData(BinaryWriter bw)
+		{
+			byte[] wp = BitConverter.GetBytes(WrestlerPointerStart);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(wp);
+			}
+			bw.Write(wp);
+
+			byte[] wn = BitConverter.GetBytes(NumWrestlers);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(wn);
+			}
+			bw.Write(wn);
+
+			byte[] cp = BitConverter.GetBytes(ChampionshipPointerStart);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(cp);
+			}
+			bw.Write(cp);
+
+			byte[] cn = BitConverter.GetBytes(NumChampionships);
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(cn);
+			}
+			bw.Write(cn);
+
+			long curPos = bw.BaseStream.Position;
+
+			// update wrestler pointers
+			bw.BaseStream.Seek(Z64Rom.PointerToRom(WrestlerPointerStart), SeekOrigin.Begin);
+			for (int i = 0; i < NumWrestlers; i++)
+			{
+				byte[] wresPtr = BitConverter.GetBytes(WrestlerPointers[i]);
+				if (BitConverter.IsLittleEndian)
+				{
+					Array.Reverse(wresPtr);
+				}
+				bw.Write(wresPtr);
+			}
+
+			// update championship data
+			bw.BaseStream.Seek(Z64Rom.PointerToRom(ChampionshipPointerStart), SeekOrigin.Begin);
+			for (int i = 0; i < NumChampionships; i++)
+			{
+				ChampionshipData[i].WriteData(bw);
+			}
+
+			bw.BaseStream.Seek(curPos, SeekOrigin.Begin);
+		}
+		#endregion
+	}
+}
