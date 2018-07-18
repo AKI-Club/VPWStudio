f6d65104e0fb4c79e617426aa287292dff74d3d9
freem <aj187@ajworld.net>
2018-07-18 02:22:41 -0500

    revenge stable editor didn't have the "Swap Wrestler" button. added it, even though it does nothing

--- VPWStudio/VPWStudio/Editors/Revenge/StableDefs_Revenge.Designer.cs
+++ VPWStudio/VPWStudio/Editors/Revenge/StableDefs_Revenge.Designer.cs
@@ -30,12 +30,12 @@
 		{
 			this.lbStables = new System.Windows.Forms.ListBox();
 			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tbHeaderGraphic = new System.Windows.Forms.TextBox();
+			this.labelHeader = new System.Windows.Forms.Label();
 			this.labelWresDefPointer = new System.Windows.Forms.Label();
 			this.labelNumWrestlers = new System.Windows.Forms.Label();
-			this.labelHeader = new System.Windows.Forms.Label();
 			this.tbWrestlerDefPointer = new System.Windows.Forms.TextBox();
 			this.tbNumWrestlers = new System.Windows.Forms.TextBox();
-			this.tbHeaderGraphic = new System.Windows.Forms.TextBox();
 			this.pbHeaderGraphic = new System.Windows.Forms.PictureBox();
 			this.gbStables = new System.Windows.Forms.GroupBox();
 			this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
@@ -44,6 +44,7 @@
 			this.buttonMoveUp = new System.Windows.Forms.Button();
 			this.buttonMoveDown = new System.Windows.Forms.Button();
 			this.buttonSwitchGroup = new System.Windows.Forms.Button();
+			this.buttonSwapWres = new System.Windows.Forms.Button();
 			this.tableLayoutPanel1.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)(this.pbHeaderGraphic)).BeginInit();
 			this.gbStables.SuspendLayout();
@@ -79,13 +80,32 @@
 			this.tableLayoutPanel1.Size = new System.Drawing.Size(201, 80);
 			this.tableLayoutPanel1.TabIndex = 1;
 			// 
+			// tbHeaderGraphic
+			// 
+			this.tbHeaderGraphic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbHeaderGraphic.Location = new System.Drawing.Point(113, 56);
+			this.tbHeaderGraphic.Name = "tbHeaderGraphic";
+			this.tbHeaderGraphic.ReadOnly = true;
+			this.tbHeaderGraphic.Size = new System.Drawing.Size(85, 20);
+			this.tbHeaderGraphic.TabIndex = 5;
+			// 
+			// labelHeader
+			// 
+			this.labelHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelHeader.AutoSize = true;
+			this.labelHeader.Location = new System.Drawing.Point(3, 59);
+			this.labelHeader.Name = "labelHeader";
+			this.labelHeader.Size = new System.Drawing.Size(104, 13);
+			this.labelHeader.TabIndex = 2;
+			this.labelHeader.Text = "Header Graphic";
+			// 
 			// labelWresDefPointer
 			// 
 			this.labelWresDefPointer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
 			this.labelWresDefPointer.AutoSize = true;
 			this.labelWresDefPointer.Location = new System.Drawing.Point(3, 6);
 			this.labelWresDefPointer.Name = "labelWresDefPointer";
-			this.labelWresDefPointer.Size = new System.Drawing.Size(103, 13);
+			this.labelWresDefPointer.Size = new System.Drawing.Size(104, 13);
 			this.labelWresDefPointer.TabIndex = 0;
 			this.labelWresDefPointer.Text = "Wrestler Definitions";
 			// 
@@ -95,46 +115,27 @@
 			this.labelNumWrestlers.AutoSize = true;
 			this.labelNumWrestlers.Location = new System.Drawing.Point(3, 32);
 			this.labelNumWrestlers.Name = "labelNumWrestlers";
-			this.labelNumWrestlers.Size = new System.Drawing.Size(103, 13);
+			this.labelNumWrestlers.Size = new System.Drawing.Size(104, 13);
 			this.labelNumWrestlers.TabIndex = 1;
 			this.labelNumWrestlers.Text = "Wrestler Count";
-			// 
-			// labelHeader
-			// 
-			this.labelHeader.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.labelHeader.AutoSize = true;
-			this.labelHeader.Location = new System.Drawing.Point(3, 59);
-			this.labelHeader.Name = "labelHeader";
-			this.labelHeader.Size = new System.Drawing.Size(103, 13);
-			this.labelHeader.TabIndex = 2;
-			this.labelHeader.Text = "Header Graphic";
 			// 
 			// tbWrestlerDefPointer
 			// 
 			this.tbWrestlerDefPointer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.tbWrestlerDefPointer.Location = new System.Drawing.Point(112, 3);
+			this.tbWrestlerDefPointer.Location = new System.Drawing.Point(113, 3);
 			this.tbWrestlerDefPointer.Name = "tbWrestlerDefPointer";
 			this.tbWrestlerDefPointer.ReadOnly = true;
-			this.tbWrestlerDefPointer.Size = new System.Drawing.Size(86, 20);
+			this.tbWrestlerDefPointer.Size = new System.Drawing.Size(85, 20);
 			this.tbWrestlerDefPointer.TabIndex = 3;
 			// 
 			// tbNumWrestlers
 			// 
 			this.tbNumWrestlers.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.tbNumWrestlers.Location = new System.Drawing.Point(112, 29);
+			this.tbNumWrestlers.Location = new System.Drawing.Point(113, 29);
 			this.tbNumWrestlers.Name = "tbNumWrestlers";
 			this.tbNumWrestlers.ReadOnly = true;
-			this.tbNumWrestlers.Size = new System.Drawing.Size(86, 20);
+			this.tbNumWrestlers.Size = new System.Drawing.Size(85, 20);
 			this.tbNumWrestlers.TabIndex = 4;
-			// 
-			// tbHeaderGraphic
-			// 
-			this.tbHeaderGraphic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-			this.tbHeaderGraphic.Location = new System.Drawing.Point(112, 56);
-			this.tbHeaderGraphic.Name = "tbHeaderGraphic";
-			this.tbHeaderGraphic.ReadOnly = true;
-			this.tbHeaderGraphic.Size = new System.Drawing.Size(86, 20);
-			this.tbHeaderGraphic.TabIndex = 5;
 			// 
 			// pbHeaderGraphic
 			// 
@@ -151,7 +152,7 @@
 			this.gbStables.Controls.Add(this.lbStables);
 			this.gbStables.Location = new System.Drawing.Point(12, 12);
 			this.gbStables.Name = "gbStables";
-			this.gbStables.Size = new System.Drawing.Size(120, 251);
+			this.gbStables.Size = new System.Drawing.Size(120, 253);
 			this.gbStables.TabIndex = 3;
 			this.gbStables.TabStop = false;
 			this.gbStables.Text = "Stables";
@@ -188,7 +189,7 @@
 			// 
 			// buttonMoveUp
 			// 
-			this.buttonMoveUp.Location = new System.Drawing.Point(250, 182);
+			this.buttonMoveUp.Location = new System.Drawing.Point(250, 155);
 			this.buttonMoveUp.Name = "buttonMoveUp";
 			this.buttonMoveUp.Size = new System.Drawing.Size(89, 23);
 			this.buttonMoveUp.TabIndex = 10;
@@ -198,7 +199,7 @@
 			// 
 			// buttonMoveDown
 			// 
-			this.buttonMoveDown.Location = new System.Drawing.Point(250, 211);
+			this.buttonMoveDown.Location = new System.Drawing.Point(250, 184);
 			this.buttonMoveDown.Name = "buttonMoveDown";
 			this.buttonMoveDown.Size = new System.Drawing.Size(89, 23);
 			this.buttonMoveDown.TabIndex = 11;
@@ -208,7 +209,7 @@
 			// 
 			// buttonSwitchGroup
 			// 
-			this.buttonSwitchGroup.Location = new System.Drawing.Point(250, 240);
+			this.buttonSwitchGroup.Location = new System.Drawing.Point(250, 213);
 			this.buttonSwitchGroup.Name = "buttonSwitchGroup";
 			this.buttonSwitchGroup.Size = new System.Drawing.Size(89, 23);
 			this.buttonSwitchGroup.TabIndex = 12;
@@ -216,11 +217,22 @@
 			this.buttonSwitchGroup.UseVisualStyleBackColor = true;
 			this.buttonSwitchGroup.Click += new System.EventHandler(this.buttonSwitchGroup_Click);
 			// 
+			// buttonSwapWres
+			// 
+			this.buttonSwapWres.Location = new System.Drawing.Point(250, 242);
+			this.buttonSwapWres.Name = "buttonSwapWres";
+			this.buttonSwapWres.Size = new System.Drawing.Size(89, 23);
+			this.buttonSwapWres.TabIndex = 17;
+			this.buttonSwapWres.Text = "Swa&p Wrestler";
+			this.buttonSwapWres.UseVisualStyleBackColor = true;
+			this.buttonSwapWres.Click += new System.EventHandler(this.buttonSwapWres_Click);
+			// 
 			// StableDefs_Revenge
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(351, 275);
+			this.ClientSize = new System.Drawing.Size(351, 277);
+			this.Controls.Add(this.buttonSwapWres);
 			this.Controls.Add(this.buttonSwitchGroup);
 			this.Controls.Add(this.buttonMoveDown);
 			this.Controls.Add(this.buttonMoveUp);
@@ -261,5 +273,6 @@
 		private System.Windows.Forms.Button buttonMoveUp;
 		private System.Windows.Forms.Button buttonMoveDown;
 		private System.Windows.Forms.Button buttonSwitchGroup;
+		private System.Windows.Forms.Button buttonSwapWres;
 	}
 }
