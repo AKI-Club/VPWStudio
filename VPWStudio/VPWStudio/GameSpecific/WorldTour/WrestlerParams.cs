62c0b2f8f9c381eb7f69dccf9c7915741af3eb13
freem <aj187@ajworld.net>
2018-07-25 10:56:46 -0500

    add stub classes for VPW64 and WorldTour WrestlerParams

---
+++ VPWStudio/VPWStudio/GameSpecific/WorldTour/WrestlerParams.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPWStudio.GameSpecific.WorldTour
+{
+	/// <summary>
+	/// WCW vs. nWo World Tour Wrestler Parameters
+	/// </summary>
+	public class WrestlerParams
+	{
+		// 32 bytes
+	}
+}
