2f009aaf18add685d0d5b33149c9f53e1a5119a7
freem <aj187@ajworld.net>
2018-07-28 19:47:58 -0500

    throw the attack/defense params into vpw64 and world tour (not bothering with the rest since the values are still unknown)

--- VPWStudio/VPWStudio/GameSpecific/WorldTour/WrestlerParams.cs
+++ VPWStudio/VPWStudio/GameSpecific/WorldTour/WrestlerParams.cs
@@ -12,5 +12,22 @@ namespace VPWStudio.GameSpecific.WorldTour
 	public class WrestlerParams
 	{
 		// 32 bytes
+
+		// 0x00-0x04 currently unknown
+		public byte Offset00;
+		public byte Offset01;
+		public byte Offset02;
+		public byte Offset03;
+		public byte Offset04;
+
+		/// <summary>
+		/// Attack parameters. (Offsets 0x05-0x09 inclusive)
+		/// </summary>
+		public byte[] OffenseValues;
+
+		/// <summary>
+		/// Defense parameters. (Offsets 0x0A-0x0E inclusive)
+		/// </summary>
+		public byte[] DefenseValues;
 	}
 }
