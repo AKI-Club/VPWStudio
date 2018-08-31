4f867891eed1d9a14c101773b35c2e7fee0785a0
freem <aj187@ajworld.net>
2018-08-30 22:36:37 -0500

    update no mercy default costume tool

--- VPWStudio/VPWStudio/Editors/NoMercy/DefaultCostume_NoMercy.cs
+++ VPWStudio/VPWStudio/Editors/NoMercy/DefaultCostume_NoMercy.cs
@@ -37,49 +37,54 @@ namespace VPWStudio.Editors.NoMercy
 
 		private void PopulateData()
 		{
-			tbBodyType.Text = String.Format("0x{0:X2}", CostumeData.BodyType);
+			cbBodyType.SelectedIndex = CostumeData.BodyType;
 			cbSkinColor.SelectedIndex = CostumeData.SkinColor;
-			tbRingAttire.Text = String.Format("0x{0:X2}", CostumeData.RingAttire);
-			tbRingAttireColor1.Text = String.Format("0x{0:X2}", CostumeData.RingAttireColor1);
-			tbRingAttireColor2.Text = String.Format("0x{0:X2}", CostumeData.RingAttireColor2);
+			cbRingAttire.SelectedIndex = CostumeData.RingAttire;
+			cccRingAttireColor1.SetColorNum(CostumeData.RingAttireColor1);
+			cccRingAttireColor2.SetColorNum(CostumeData.RingAttireColor2);
 			tbUpperAttire.Text = String.Format("0x{0:X2}", CostumeData.UpperAttire);
-			tbUpperAttireColor1.Text = String.Format("0x{0:X2}", CostumeData.UpperAttireColor1);
-			tbUpperAttireColor2.Text = String.Format("0x{0:X2}", CostumeData.UpperAttireColor2);
-			tbEntranceAttire.Text = String.Format("0x{0:X2}", CostumeData.EntranceAttire);
-			tbEntranceAttireColor1.Text = String.Format("0x{0:X2}", CostumeData.EntranceAttireColor1);
-			tbEntranceAttireColor2.Text = String.Format("0x{0:X2}", CostumeData.EntranceAttireColor2);
-			tbEntranceWeapon.Text = String.Format("0x{0:X2}", CostumeData.EntranceWeapon);
-			tbGloves.Text = String.Format("0x{0:X2}", CostumeData.Gloves);
-			tbGlovesColor.Text = String.Format("0x{0:X2}", CostumeData.GlovesColor);
+			cccUpperAttireColor1.SetColorNum(CostumeData.UpperAttireColor1);
+			cccUpperAttireColor2.SetColorNum(CostumeData.UpperAttireColor2);
+			cbEntranceAttire.SelectedIndex = CostumeData.EntranceAttire;
+			cccEntranceAttireColor1.SetColorNum(CostumeData.EntranceAttireColor1);
+			cccEntranceAttireColor2.SetColorNum(CostumeData.EntranceAttireColor2);
+			cbEntranceWeapon.SelectedIndex = CostumeData.EntranceWeapon;
+			cbGloves.SelectedIndex = CostumeData.Gloves;
+			cccGlovesColor.SetColorNum(CostumeData.GlovesColor);
 			tbTattoo.Text = String.Format("0x{0:X2}", CostumeData.Tattoo);
-			tbWristband.Text = String.Format("0x{0:X2}", CostumeData.Wristband);
-			tbWristbandColor.Text = String.Format("0x{0:X2}", CostumeData.WristbandColor);
-			tbLeftElbowPad.Text = String.Format("0x{0:X2}", CostumeData.LeftElbowPad);
-			tbLeftElbowPadColor.Text = String.Format("0x{0:X2}", CostumeData.LeftElbowPadColor);
-			tbRightElbowPad.Text = String.Format("0x{0:X2}", CostumeData.RightElbowPad);
-			tbRightElbowPadColor.Text = String.Format("0x{0:X2}", CostumeData.RightElbowPadColor);
-			tbLeftKneepad.Text = String.Format("0x{0:X2}", CostumeData.LeftKneePad);
-			tbLeftKneepadColor.Text = String.Format("0x{0:X2}", CostumeData.LeftKneePadColor);
-			tbRightKneepad.Text = String.Format("0x{0:X2}", CostumeData.RightKneePad);
-			tbRightKneepadColor.Text = String.Format("0x{0:X2}", CostumeData.RightKneePadColor);
-			tbBoots.Text = String.Format("0x{0:X2}", CostumeData.Boots);
-			tbBootsColor1.Text = String.Format("0x{0:X2}", CostumeData.BootsColor1);
-			tbBootsColor2.Text = String.Format("0x{0:X2}", CostumeData.BootsColor2);
-			tbHeadShape.Text = String.Format("0x{0:X2}", CostumeData.HeadShape);
-			tbFaceNumber.Text = String.Format("0x{0:X2}", CostumeData.FaceNumber);
-			tbHairType.Text = String.Format("0x{0:X2}", CostumeData.HairType);
-			tbHairColor.Text = String.Format("0x{0:X2}", CostumeData.HairColor);
-			tbFrontHair.Text = String.Format("0x{0:X2}", CostumeData.FrontHair);
-			tbFacialHair.Text = String.Format("0x{0:X2}", CostumeData.FacialHair);
-			tbMasksEtc.Text = String.Format("0x{0:X2}", CostumeData.MasksEtc);
-			tbHatsCaps.Text = String.Format("0x{0:X2}", CostumeData.HatsCaps);
-			tbHatsCapsColor.Text = String.Format("0x{0:X2}", CostumeData.HatsCapsColor);
-			tbPortrait.Text = String.Format("0x{0:X2}", CostumeData.Portrait);
+			cbWristband.SelectedIndex = CostumeData.Wristband;
+			cccWristbandColor.SetColorNum(CostumeData.WristbandColor);
+			cbLeftElbowPad.SelectedIndex = CostumeData.LeftElbowPad;
+			cccLeftElbowPadColor.SetColorNum(CostumeData.LeftElbowPadColor);
+			cbRightElbowPad.SelectedIndex = CostumeData.RightElbowPad;
+			cccRightElbowPadColor.SetColorNum(CostumeData.RightElbowPadColor);
+			cbLeftKneepad.SelectedIndex = CostumeData.LeftKneePad;
+			cccLeftKneePadColor.SetColorNum(CostumeData.LeftKneePadColor);
+			cbRightKneepad.SelectedIndex = CostumeData.RightKneePad;
+			cccRightKneePadColor.SetColorNum(CostumeData.RightKneePadColor);
+			cbBoots.SelectedIndex = CostumeData.Boots;
+			cccBootsColor1.SetColorNum(CostumeData.BootsColor1);
+			cccBootsColor2.SetColorNum(CostumeData.BootsColor2);
+			cbHeadShape.SelectedIndex = CostumeData.HeadShape;
+			cbFaces.SelectedIndex = CostumeData.FaceNumber;
+			cbHairType.SelectedIndex = CostumeData.HairType;
+			cbHairColor.SelectedIndex = CostumeData.HairColor;
+			cbFrontHair.SelectedIndex = CostumeData.FrontHair;
+			cbFacialHair.SelectedIndex = CostumeData.FacialHair;
+			cbMasksEtc.SelectedIndex = CostumeData.MasksEtc;
+			cbHatsCaps.SelectedIndex = CostumeData.HatsCaps;
+			cccHatsCaps.SetColorNum(CostumeData.HatsCapsColor);
+			cbPortrait.SelectedIndex = CostumeData.Portrait;
 		}
 
 		private void buttonOK_Click(object sender, EventArgs e)
 		{
 			// make changes based on values
+			CostumeData.SkinColor = (byte)cbSkinColor.SelectedIndex;
+			CostumeData.HeadShape = (byte)cbHeadShape.SelectedIndex;
+			CostumeData.HairType = (byte)cbHairType.SelectedIndex;
+			CostumeData.HairColor = (byte)cbHairColor.SelectedIndex;
+			CostumeData.HatsCaps = (byte)cbHatsCaps.SelectedIndex;
 
 			DialogResult = DialogResult.OK;
 			Close();
