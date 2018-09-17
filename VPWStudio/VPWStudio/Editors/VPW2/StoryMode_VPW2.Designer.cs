e7d5414bfff5033c51a30853e47d7fd82debcfea
freem <aj187@ajworld.net>
2018-09-17 11:59:12 -0500

    start work on VPW2 Story Mode editor

---
+++ VPWStudio/VPWStudio/Editors/VPW2/StoryMode_VPW2.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace VPWStudio.Editors.VPW2
+{
+	partial class StoryMode_VPW2
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tpTeams = new System.Windows.Forms.TabPage();
+			this.tpChamps = new System.Windows.Forms.TabPage();
+			this.tlpChampions = new System.Windows.Forms.TableLayoutPanel();
+			this.labelTripleCrown = new System.Windows.Forms.Label();
+			this.labelWorldTag = new System.Windows.Forms.Label();
+			this.labelAsiaTag = new System.Windows.Forms.Label();
+			this.tbTripleCrown = new System.Windows.Forms.TextBox();
+			this.tlpWorldTag = new System.Windows.Forms.TableLayoutPanel();
+			this.tlpAsiaTag = new System.Windows.Forms.TableLayoutPanel();
+			this.tbWorldTag1 = new System.Windows.Forms.TextBox();
+			this.tbWorldTag2 = new System.Windows.Forms.TextBox();
+			this.tbAsiaTag1 = new System.Windows.Forms.TextBox();
+			this.tbAsiaTag2 = new System.Windows.Forms.TextBox();
+			this.lbTeams = new System.Windows.Forms.ListBox();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.labelWrestler1 = new System.Windows.Forms.Label();
+			this.labelWrestler2 = new System.Windows.Forms.Label();
+			this.labelUnknown = new System.Windows.Forms.Label();
+			this.tbWrestler1 = new System.Windows.Forms.TextBox();
+			this.tbWrestler2 = new System.Windows.Forms.TextBox();
+			this.tbUnknown = new System.Windows.Forms.TextBox();
+			this.tabControl1.SuspendLayout();
+			this.tpTeams.SuspendLayout();
+			this.tpChamps.SuspendLayout();
+			this.tlpChampions.SuspendLayout();
+			this.tlpWorldTag.SuspendLayout();
+			this.tlpAsiaTag.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tpTeams);
+			this.tabControl1.Controls.Add(this.tpChamps);
+			this.tabControl1.Location = new System.Drawing.Point(12, 12);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(368, 249);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tpTeams
+			// 
+			this.tpTeams.Controls.Add(this.tableLayoutPanel1);
+			this.tpTeams.Controls.Add(this.lbTeams);
+			this.tpTeams.Location = new System.Drawing.Point(4, 22);
+			this.tpTeams.Name = "tpTeams";
+			this.tpTeams.Padding = new System.Windows.Forms.Padding(3);
+			this.tpTeams.Size = new System.Drawing.Size(360, 223);
+			this.tpTeams.TabIndex = 0;
+			this.tpTeams.Text = "Teams/Participants";
+			this.tpTeams.UseVisualStyleBackColor = true;
+			// 
+			// tpChamps
+			// 
+			this.tpChamps.Controls.Add(this.tlpChampions);
+			this.tpChamps.Location = new System.Drawing.Point(4, 22);
+			this.tpChamps.Name = "tpChamps";
+			this.tpChamps.Padding = new System.Windows.Forms.Padding(3);
+			this.tpChamps.Size = new System.Drawing.Size(360, 223);
+			this.tpChamps.TabIndex = 1;
+			this.tpChamps.Text = "Default Champions";
+			this.tpChamps.UseVisualStyleBackColor = true;
+			// 
+			// tlpChampions
+			// 
+			this.tlpChampions.ColumnCount = 2;
+			this.tlpChampions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 36.49425F));
+			this.tlpChampions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63.50575F));
+			this.tlpChampions.Controls.Add(this.tlpAsiaTag, 1, 2);
+			this.tlpChampions.Controls.Add(this.labelTripleCrown, 0, 0);
+			this.tlpChampions.Controls.Add(this.labelWorldTag, 0, 1);
+			this.tlpChampions.Controls.Add(this.labelAsiaTag, 0, 2);
+			this.tlpChampions.Controls.Add(this.tbTripleCrown, 1, 0);
+			this.tlpChampions.Controls.Add(this.tlpWorldTag, 1, 1);
+			this.tlpChampions.Location = new System.Drawing.Point(6, 6);
+			this.tlpChampions.Name = "tlpChampions";
+			this.tlpChampions.RowCount = 3;
+			this.tlpChampions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tlpChampions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tlpChampions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tlpChampions.Size = new System.Drawing.Size(348, 211);
+			this.tlpChampions.TabIndex = 0;
+			// 
+			// labelTripleCrown
+			// 
+			this.labelTripleCrown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelTripleCrown.AutoSize = true;
+			this.labelTripleCrown.Location = new System.Drawing.Point(3, 28);
+			this.labelTripleCrown.Name = "labelTripleCrown";
+			this.labelTripleCrown.Size = new System.Drawing.Size(120, 13);
+			this.labelTripleCrown.TabIndex = 0;
+			this.labelTripleCrown.Text = "Triple Crown";
+			// 
+			// labelWorldTag
+			// 
+			this.labelWorldTag.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWorldTag.AutoSize = true;
+			this.labelWorldTag.Location = new System.Drawing.Point(3, 98);
+			this.labelWorldTag.Name = "labelWorldTag";
+			this.labelWorldTag.Size = new System.Drawing.Size(120, 13);
+			this.labelWorldTag.TabIndex = 1;
+			this.labelWorldTag.Text = "World Tag";
+			// 
+			// labelAsiaTag
+			// 
+			this.labelAsiaTag.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelAsiaTag.AutoSize = true;
+			this.labelAsiaTag.Location = new System.Drawing.Point(3, 169);
+			this.labelAsiaTag.Name = "labelAsiaTag";
+			this.labelAsiaTag.Size = new System.Drawing.Size(120, 13);
+			this.labelAsiaTag.TabIndex = 2;
+			this.labelAsiaTag.Text = "Asia Tag";
+			// 
+			// tbTripleCrown
+			// 
+			this.tbTripleCrown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbTripleCrown.Location = new System.Drawing.Point(129, 25);
+			this.tbTripleCrown.Name = "tbTripleCrown";
+			this.tbTripleCrown.ReadOnly = true;
+			this.tbTripleCrown.Size = new System.Drawing.Size(216, 20);
+			this.tbTripleCrown.TabIndex = 3;
+			// 
+			// tlpWorldTag
+			// 
+			this.tlpWorldTag.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tlpWorldTag.ColumnCount = 1;
+			this.tlpWorldTag.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tlpWorldTag.Controls.Add(this.tbWorldTag2, 0, 1);
+			this.tlpWorldTag.Controls.Add(this.tbWorldTag1, 0, 0);
+			this.tlpWorldTag.Location = new System.Drawing.Point(129, 73);
+			this.tlpWorldTag.Name = "tlpWorldTag";
+			this.tlpWorldTag.RowCount = 2;
+			this.tlpWorldTag.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tlpWorldTag.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tlpWorldTag.Size = new System.Drawing.Size(216, 64);
+			this.tlpWorldTag.TabIndex = 4;
+			// 
+			// tlpAsiaTag
+			// 
+			this.tlpAsiaTag.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tlpAsiaTag.ColumnCount = 1;
+			this.tlpAsiaTag.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tlpAsiaTag.Controls.Add(this.tbAsiaTag2, 0, 1);
+			this.tlpAsiaTag.Controls.Add(this.tbAsiaTag1, 0, 0);
+			this.tlpAsiaTag.Location = new System.Drawing.Point(129, 143);
+			this.tlpAsiaTag.Name = "tlpAsiaTag";
+			this.tlpAsiaTag.RowCount = 2;
+			this.tlpAsiaTag.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tlpAsiaTag.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tlpAsiaTag.Size = new System.Drawing.Size(216, 64);
+			this.tlpAsiaTag.TabIndex = 5;
+			// 
+			// tbWorldTag1
+			// 
+			this.tbWorldTag1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWorldTag1.Location = new System.Drawing.Point(3, 6);
+			this.tbWorldTag1.Name = "tbWorldTag1";
+			this.tbWorldTag1.ReadOnly = true;
+			this.tbWorldTag1.Size = new System.Drawing.Size(210, 20);
+			this.tbWorldTag1.TabIndex = 4;
+			// 
+			// tbWorldTag2
+			// 
+			this.tbWorldTag2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWorldTag2.Location = new System.Drawing.Point(3, 38);
+			this.tbWorldTag2.Name = "tbWorldTag2";
+			this.tbWorldTag2.ReadOnly = true;
+			this.tbWorldTag2.Size = new System.Drawing.Size(210, 20);
+			this.tbWorldTag2.TabIndex = 5;
+			// 
+			// tbAsiaTag1
+			// 
+			this.tbAsiaTag1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbAsiaTag1.Location = new System.Drawing.Point(3, 6);
+			this.tbAsiaTag1.Name = "tbAsiaTag1";
+			this.tbAsiaTag1.ReadOnly = true;
+			this.tbAsiaTag1.Size = new System.Drawing.Size(210, 20);
+			this.tbAsiaTag1.TabIndex = 4;
+			// 
+			// tbAsiaTag2
+			// 
+			this.tbAsiaTag2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbAsiaTag2.Location = new System.Drawing.Point(3, 38);
+			this.tbAsiaTag2.Name = "tbAsiaTag2";
+			this.tbAsiaTag2.ReadOnly = true;
+			this.tbAsiaTag2.Size = new System.Drawing.Size(210, 20);
+			this.tbAsiaTag2.TabIndex = 5;
+			// 
+			// lbTeams
+			// 
+			this.lbTeams.FormattingEnabled = true;
+			this.lbTeams.Location = new System.Drawing.Point(6, 6);
+			this.lbTeams.Name = "lbTeams";
+			this.lbTeams.Size = new System.Drawing.Size(96, 212);
+			this.lbTeams.TabIndex = 0;
+			this.lbTeams.SelectedIndexChanged += new System.EventHandler(this.lbTeams_SelectedIndexChanged);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 30F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+			this.tableLayoutPanel1.Controls.Add(this.labelWrestler1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.labelWrestler2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.labelUnknown, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.tbWrestler1, 1, 0);
+			this.tableLayoutPanel1.Controls.Add(this.tbWrestler2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.tbUnknown, 1, 2);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(108, 6);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(246, 211);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// labelWrestler1
+			// 
+			this.labelWrestler1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWrestler1.AutoSize = true;
+			this.labelWrestler1.Location = new System.Drawing.Point(3, 28);
+			this.labelWrestler1.Name = "labelWrestler1";
+			this.labelWrestler1.Size = new System.Drawing.Size(67, 13);
+			this.labelWrestler1.TabIndex = 0;
+			this.labelWrestler1.Text = "Wrestler 1";
+			// 
+			// labelWrestler2
+			// 
+			this.labelWrestler2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelWrestler2.AutoSize = true;
+			this.labelWrestler2.Location = new System.Drawing.Point(3, 98);
+			this.labelWrestler2.Name = "labelWrestler2";
+			this.labelWrestler2.Size = new System.Drawing.Size(67, 13);
+			this.labelWrestler2.TabIndex = 1;
+			this.labelWrestler2.Text = "Wrestler 2";
+			// 
+			// labelUnknown
+			// 
+			this.labelUnknown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.labelUnknown.AutoSize = true;
+			this.labelUnknown.Location = new System.Drawing.Point(3, 169);
+			this.labelUnknown.Name = "labelUnknown";
+			this.labelUnknown.Size = new System.Drawing.Size(67, 13);
+			this.labelUnknown.TabIndex = 2;
+			this.labelUnknown.Text = "Unknown";
+			// 
+			// tbWrestler1
+			// 
+			this.tbWrestler1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestler1.Location = new System.Drawing.Point(76, 25);
+			this.tbWrestler1.Name = "tbWrestler1";
+			this.tbWrestler1.ReadOnly = true;
+			this.tbWrestler1.Size = new System.Drawing.Size(167, 20);
+			this.tbWrestler1.TabIndex = 3;
+			// 
+			// tbWrestler2
+			// 
+			this.tbWrestler2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestler2.Location = new System.Drawing.Point(76, 95);
+			this.tbWrestler2.Name = "tbWrestler2";
+			this.tbWrestler2.ReadOnly = true;
+			this.tbWrestler2.Size = new System.Drawing.Size(167, 20);
+			this.tbWrestler2.TabIndex = 4;
+			// 
+			// tbUnknown
+			// 
+			this.tbUnknown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbUnknown.Location = new System.Drawing.Point(76, 165);
+			this.tbUnknown.Name = "tbUnknown";
+			this.tbUnknown.ReadOnly = true;
+			this.tbUnknown.Size = new System.Drawing.Size(167, 20);
+			this.tbUnknown.TabIndex = 5;
+			// 
+			// StoryMode_VPW2
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(392, 273);
+			this.Controls.Add(this.tabControl1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.Name = "StoryMode_VPW2";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "VPW2 Story Mode/Royal Road Succession";
+			this.tabControl1.ResumeLayout(false);
+			this.tpTeams.ResumeLayout(false);
+			this.tpChamps.ResumeLayout(false);
+			this.tlpChampions.ResumeLayout(false);
+			this.tlpChampions.PerformLayout();
+			this.tlpWorldTag.ResumeLayout(false);
+			this.tlpWorldTag.PerformLayout();
+			this.tlpAsiaTag.ResumeLayout(false);
+			this.tlpAsiaTag.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tpTeams;
+		private System.Windows.Forms.TabPage tpChamps;
+		private System.Windows.Forms.TableLayoutPanel tlpChampions;
+		private System.Windows.Forms.Label labelTripleCrown;
+		private System.Windows.Forms.Label labelWorldTag;
+		private System.Windows.Forms.Label labelAsiaTag;
+		private System.Windows.Forms.TableLayoutPanel tlpAsiaTag;
+		private System.Windows.Forms.TextBox tbTripleCrown;
+		private System.Windows.Forms.TableLayoutPanel tlpWorldTag;
+		private System.Windows.Forms.TextBox tbAsiaTag2;
+		private System.Windows.Forms.TextBox tbAsiaTag1;
+		private System.Windows.Forms.TextBox tbWorldTag2;
+		private System.Windows.Forms.TextBox tbWorldTag1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label labelWrestler1;
+		private System.Windows.Forms.Label labelWrestler2;
+		private System.Windows.Forms.Label labelUnknown;
+		private System.Windows.Forms.ListBox lbTeams;
+		private System.Windows.Forms.TextBox tbWrestler1;
+		private System.Windows.Forms.TextBox tbWrestler2;
+		private System.Windows.Forms.TextBox tbUnknown;
+	}
+}
---
+++ VPWStudio/VPWStudio/Editors/VPW2/StoryMode_VPW2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VPWStudio.GameSpecific;
+
+namespace VPWStudio.Editors.VPW2
+{
+	public partial class StoryMode_VPW2 : Form
+	{
+		/// <summary>
+		/// Story Mode teams
+		/// </summary>
+		public List<StoryModeTeam_Modern> StoryTeams;
+
+		/// <summary>
+		/// Default Champions
+		/// </summary>
+		public byte[] DefaultChampions = new byte[5];
+
+		public StoryMode_VPW2()
+		{
+			InitializeComponent();
+
+			// 18x teams (Z64 0xDB4F0)
+
+			// 5x champions (Z64 0xDB544)
+			// XXX: extremely hardcoded
+			DefaultChampions[0] = Program.CurrentInputROM.Data[0xDB544];
+			DefaultChampions[1] = Program.CurrentInputROM.Data[0xDB545];
+			DefaultChampions[2] = Program.CurrentInputROM.Data[0xDB546];
+			DefaultChampions[3] = Program.CurrentInputROM.Data[0xDB547];
+			DefaultChampions[4] = Program.CurrentInputROM.Data[0xDB548];
+
+			tbTripleCrown.Text = String.Format("0x{0:X2}", DefaultChampions[0]);
+			tbWorldTag1.Text = String.Format("0x{0:X2}", DefaultChampions[1]);
+			tbAsiaTag1.Text = String.Format("0x{0:X2}", DefaultChampions[2]);
+			tbWorldTag2.Text = String.Format("0x{0:X2}", DefaultChampions[3]);
+			tbAsiaTag2.Text = String.Format("0x{0:X2}", DefaultChampions[4]);
+		}
+
+		private void lbTeams_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (lbTeams.SelectedIndex < 0)
+			{
+				return;
+			}
+
+			// update values
+		}
+	}
+}
