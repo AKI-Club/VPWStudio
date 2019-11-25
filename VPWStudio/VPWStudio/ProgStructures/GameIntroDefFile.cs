070db68027655917b3997c0ff0a0c5fcc824b22b
freem <aj187@ajworld.net>
2019-11-25 11:12:00 -0600

    gameintrodef file isn't done yet but git is a fuck and so is visual studio

---
+++ VPWStudio/VPWStudio/ProgStructures/GameIntroDefFile.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace VPWStudio
+{
+	public class GameIntroDefFile
+	{
+		#region Members
+		/// <summary>
+		/// Game Type (defines what game intro defs are read/written)
+		/// </summary>
+		public VPWGames GameType;
+
+		// todo: the game-specific shit
+		#endregion
+
+		public GameIntroDefFile()
+		{
+			GameType = VPWGames.Invalid;
+		}
+
+		public GameIntroDefFile(VPWGames _game)
+		{
+			GameType = _game;
+		}
+
+		// todo: read data, write data
+	}
+}
