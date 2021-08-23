3de90d478f25f9e1124c7299ba707e2857447320
freem <aj187@ajworld.net>
2021-08-22 19:41:26 -0500

    initial work on PS1 CLUT and TIM support

---
+++ VPWStudio/VPWStudio/DataStructures/PS1/ClutData.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+
+namespace VPWStudio
+{
+	/// <summary>
+	/// CLUT (color lookup table; i.e. "palette") data
+	/// </summary>
+	public class ClutData
+	{
+		#region Class Members
+		/// <summary>
+		/// Length of the CLUT data, including the 4 bytes for this variable.
+		/// </summary>
+		public UInt32 DataLength;
+
+		/// <summary>
+		/// X coordinate for CLUT data in the framebuffer.
+		/// </summary>
+		public UInt16 XCoordinate;
+
+		/// <summary>
+		/// Y coordinate for CLUT data in the framebuffer.
+		/// </summary>
+		public UInt16 YCoordinate;
+
+		/// <summary>
+		/// Width of the CLUT data in the framebuffer.
+		/// </summary>
+		public UInt16 DataWidth;
+
+		/// <summary>
+		/// Height of the CLUT data in the framebuffer.
+		/// </summary>
+		public UInt16 DataHeight;
+
+		/// <summary>
+		/// CLUT values
+		/// </summary>
+		/// FEDCBA9876543210
+		/// ||___||___||___|
+		/// |  |    |    |
+		/// |  |    |    +--- Red
+		/// |  |    +-------- Green
+		/// |  +------------- Blue
+		/// +---------------- (Semi-)Transparent
+		public List<UInt16> Data;
+		#endregion
+
+		#region Constructors
+		/// <summary>
+		/// Default constructor. Not guaranteed to be usable.
+		/// </summary>
+		public ClutData()
+		{
+			DataLength = 12; // no data other than the header by default.
+			XCoordinate = 0;
+			YCoordinate = 0;
+			DataWidth = 16;
+			DataHeight = 1;
+			Data = new List<UInt16>();
+		}
+
+		/// <summary>
+		/// Constructor from BinaryReader, meant to be callled during TIM reading.
+		/// </summary>
+		/// <param name="br">BinaryReader instance to use.</param>
+		public ClutData(BinaryReader br)
+		{
+			ReadData(br);
+		}
+		#endregion
+
+		/// <summary>
+		/// Get the palette entries in the CLUT as System.Drawing.Color values.
+		/// </summary>
+		/// <returns>A List of System.Drawing.Color values representing the data in the CLUT.</returns>
+		public List<Color> GetColors()
+		{
+			List<Color> colors = new List<Color>();
+
+			foreach (UInt16 c in Data)
+			{
+				// assuming little endian...
+				colors.Add(Color.FromArgb((c & 0x8000) == 0 ? 255 : 128, (c & 0x1F) * 8, ((c & 0x3E0) >> 5) * 8, ((c & 0x7C00) >> 10) * 8));
+			}
+
+			return colors;
+		}
+
+		#region Binary Read/Write (CLUT inside of a TIM file)
+		/// <summary>
+		/// Read CLUT data from a TIM file using a BinaryReader.
+		/// </summary>
+		/// <param name="br">BinaryReader instance to use.</param>
+		public void ReadData(BinaryReader br)
+		{
+			byte[] len = br.ReadBytes(4);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(len);
+			}
+			DataLength = BitConverter.ToUInt32(len, 0);
+
+			byte[] xPos = br.ReadBytes(2);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(xPos);
+			}
+			XCoordinate = BitConverter.ToUInt16(xPos, 0);
+
+			byte[] yPos = br.ReadBytes(2);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(yPos);
+			}
+			YCoordinate = BitConverter.ToUInt16(yPos, 0);
+
+			byte[] dataWidth = br.ReadBytes(2);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(dataWidth);
+			}
+			DataWidth = BitConverter.ToUInt16(dataWidth, 0);
+
+			byte[] dataHeight = br.ReadBytes(2);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(dataHeight);
+			}
+			DataHeight = BitConverter.ToUInt16(dataHeight, 0);
+
+			for (int i = 0; i < (DataLength - 12) / 2; i++)
+			{
+				byte[] color = br.ReadBytes(2);
+				if (!BitConverter.IsLittleEndian)
+				{
+					Array.Reverse(color);
+				}
+				Data.Add(BitConverter.ToUInt16(color,0));
+			}
+		}
+
+		/// <summary>
+		/// Write CLUT data to a TIM file using a BinaryWriter.
+		/// </summary>
+		/// <param name="bw">BinaryWriter instance to use.</param>
+		public void WriteData(BinaryWriter bw)
+		{
+			// re-calculate DataLength
+			DataLength = (uint)((Data.Count * sizeof(UInt16)) + 12);
+			byte[] len = BitConverter.GetBytes(DataLength);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(len);
+			}
+			bw.Write(len);
+
+			byte[] xPos = BitConverter.GetBytes(XCoordinate);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(xPos);
+			}
+			bw.Write(xPos);
+
+			byte[] yPos = BitConverter.GetBytes(YCoordinate);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(yPos);
+			}
+			bw.Write(yPos);
+
+			byte[] dataWidth = BitConverter.GetBytes(DataWidth);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(dataWidth);
+			}
+			bw.Write(dataWidth);
+
+			byte[] dataHeight = BitConverter.GetBytes(DataHeight);
+			if (!BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(dataHeight);
+			}
+			bw.Write(dataHeight);
+
+			foreach (UInt16 c in Data)
+			{
+				byte[] value = BitConverter.GetBytes(c);
+				if (!BitConverter.IsLittleEndian)
+				{
+					Array.Reverse(value);
+				}
+				bw.Write(value);
+			}
+		}
+		#endregion
+	}
+}
