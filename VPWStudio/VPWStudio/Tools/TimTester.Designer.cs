9932fa1e83c0339155b403c5330136f64e4b42db
freem <aj187@ajworld.net>
2021-08-23 13:49:29 -0500

    [TimTester] set maximum size

--- VPWStudio/VPWStudio/Tools/TimTester.Designer.cs
+++ VPWStudio/VPWStudio/Tools/TimTester.Designer.cs
@@ -47,7 +47,7 @@ namespace VPWStudio
 			this.pictureBox1.Location = new System.Drawing.Point(12, 27);
 			this.pictureBox1.MaximumSize = new System.Drawing.Size(1024, 1024);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+			this.pictureBox1.Size = new System.Drawing.Size(256, 257);
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
@@ -81,14 +81,14 @@ namespace VPWStudio
 			// nextPaletteToolStripMenuItem
 			// 
 			this.nextPaletteToolStripMenuItem.Name = "nextPaletteToolStripMenuItem";
-			this.nextPaletteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.nextPaletteToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
 			this.nextPaletteToolStripMenuItem.Text = "&Next Palette";
 			this.nextPaletteToolStripMenuItem.Click += new System.EventHandler(this.nextPaletteToolStripMenuItem_Click);
 			// 
 			// previousPaletteToolStripMenuItem
 			// 
 			this.previousPaletteToolStripMenuItem.Name = "previousPaletteToolStripMenuItem";
-			this.previousPaletteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.previousPaletteToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
 			this.previousPaletteToolStripMenuItem.Text = "&Previous Palette";
 			this.previousPaletteToolStripMenuItem.Click += new System.EventHandler(this.previousPaletteToolStripMenuItem_Click);
 			// 
@@ -96,12 +96,14 @@ namespace VPWStudio
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(280, 296);
+			this.ClientSize = new System.Drawing.Size(280, 297);
 			this.Controls.Add(this.pictureBox1);
 			this.Controls.Add(this.menuStrip1);
 			this.MainMenuStrip = this.menuStrip1;
 			this.MaximizeBox = false;
+			this.MaximumSize = new System.Drawing.Size(1064, 1104);
 			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(296, 336);
 			this.Name = "TimTester";
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
 			this.Text = "TIM Tester";
