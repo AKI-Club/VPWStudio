fd0d756c0246f33d51ec4096b24ebe58f6e2da0f
freem <aj187@ajworld.net>
2021-08-25 18:01:10 -0500

    forgot to remove unnecessary "Dialogs" namespace when making this

--- VPWStudio/VPWStudio/Dialogs/SelectTextureDialog.cs
+++ VPWStudio/VPWStudio/Dialogs/SelectTextureDialog.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace VPWStudio.Dialogs
+namespace VPWStudio
 {
 	public partial class SelectTextureDialog : Form
 	{
