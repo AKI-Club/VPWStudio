43cee71917e59714711ce6cae914b3fbe3778935
freem <aj187@ajworld.net>
2021-08-30 16:19:11 -0500

    start making GameRegion less N64-centric

--- VPWStudio/VPWStudio/ProgStructures/GameRegion.cs
+++ VPWStudio/VPWStudio/ProgStructures/GameRegion.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 namespace VPWStudio
 {
 	/// <summary>
-	/// Possible game regions, and their codes.
+	/// Possible Nintendo 64 game regions, and their codes.
 	/// </summary>
 	public enum GameRegion
 	{
@@ -33,8 +33,73 @@ namespace VPWStudio
 	public static class GameRegionInfo
 	{
 		/// <summary>
+		/// Convert a GameRegion value to a Nintendo 64 region code.
+		/// </summary>
+		/// <param name="_region">GameRegion to get the region code for.</param>
+		/// <returns>A character representing the passed in GameRegion. Note: GameRegion.Custom needs to be checked manually.</returns>
+		public static char GameRegionToN64(GameRegion _region)
+		{
+			switch (_region)
+			{
+				case GameRegion.Japan: return 'J';
+				case GameRegion.NorthAmerica: return 'E';
+				case GameRegion.Europe: return 'P';
+				case GameRegion.France: return 'F';
+				case GameRegion.Canada: return 'N';
+				case GameRegion.Germany: return 'D';
+				case GameRegion.Dutch: return 'H';
+				case GameRegion.Italy: return 'I';
+				case GameRegion.Spain: return 'S';
+				case GameRegion.Scandanavia: return 'W';
+				case GameRegion.China: return 'C';
+				case GameRegion.Korea: return 'K';
+				case GameRegion.Brazil: return 'B';
+				case GameRegion.Australia: return 'U';
+				case GameRegion.Gateway_NTSC: return 'G';
+				case GameRegion.Gateway_PAL: return 'L';
+				case GameRegion.OtherX: return 'X';
+				case GameRegion.OtherY: return 'Y';
+				case GameRegion.OtherZ: return 'Z';
+				default: return '?';
+			}
+		}
+
+		/// <summary>
+		/// Convert a Nintendo 64 region code to a GameRegion value.
+		/// </summary>
+		/// <param name="_region">A character representing the Nintendo 64 region code.</param>
+		/// <returns>The corresponding GameRegion value, if found, or GameRegion.Custom if not found.</returns>
+		public static GameRegion GameRegionFromN64(char _region)
+		{
+			switch (_region)
+			{
+				case 'J': return GameRegion.Japan;
+				case 'E': return GameRegion.NorthAmerica;
+				case 'P': return GameRegion.Europe;
+				case 'F': return GameRegion.France;
+				case 'N': return GameRegion.Canada;
+				case 'D': return GameRegion.Germany;
+				case 'H': return GameRegion.Dutch;
+				case 'I': return GameRegion.Italy;
+				case 'S': return GameRegion.Spain;
+				case 'W': return GameRegion.Scandanavia;
+				case 'C': return GameRegion.China;
+				case 'K': return GameRegion.Korea;
+				case 'B': return GameRegion.Brazil;
+				case 'U': return GameRegion.Australia;
+				case 'G': return GameRegion.Gateway_NTSC;
+				case 'L': return GameRegion.Gateway_PAL;
+				case 'X': return GameRegion.OtherX;
+				case 'Y': return GameRegion.OtherY;
+				case 'Z': return GameRegion.OtherZ;
+				default: return GameRegion.Custom;
+			}
+		}
+
+		/// <summary>
 		/// Map GameRegion values to human-readable strings.
 		/// </summary>
+		/// Note: This is N64-centric
 		public static Dictionary<GameRegion, string> GameRegionNames = new Dictionary<GameRegion, string>()
 		{
 			{ GameRegion.Japan, "Japan" },
