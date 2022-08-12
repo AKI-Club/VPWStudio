35f5692ac82ac937cb96a1f2e0cb894a8dd72c14
freem <aj187@ajworld.net>
2022-08-12 16:45:28 -0500

    [AboutBox] Move the Month+Year for the testers list into its own string, since I no longer want to edit it in place

--- VPWStudio/VPWStudio/Dialogs/Program/AboutBox.cs
+++ VPWStudio/VPWStudio/Dialogs/Program/AboutBox.cs
@@ -45,6 +45,11 @@ namespace VPWStudio
 			"CurdleGames"
 		};
 
+		/// <summary>
+		/// Last updated month and year for program testing group information.
+		/// </summary>
+		private readonly string DateString = "August 2022";
+
 		public AboutBox()
 		{
 			InitializeComponent();
@@ -70,7 +75,7 @@ namespace VPWStudio
 			sb.AppendLine("This is an in-development version of VPW Studio.");
 			sb.AppendLine("It is not meant to be used in 'production'; always make backups.");
 			sb.AppendLine();
-			sb.AppendLine("This program is only meant to be used by the VPW Studio testing group. As of July 2022, this includes:");
+			sb.AppendLine(String.Format("This program is only meant to be used by the VPW Studio testing group. As of {0}, this includes:", DateString));
 			sb.AppendLine();
 			sb.AppendLine("- current \"AKI Wrestling Game Hacking\" Discord channel members");
 			sb.AppendLine("- GameGenie720");
