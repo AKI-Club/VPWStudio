68a67b9ae2aebc4e142c2c43991614afd6be3eec
freem <aj187@ajworld.net>
2022-09-24 11:54:55 -0500

    actually let demo match editor close on pressing ok and/or cancel

--- VPWStudio/VPWStudio/Editors/VPW2/DemoMatchEditor_VPW2.cs
+++ VPWStudio/VPWStudio/Editors/VPW2/DemoMatchEditor_VPW2.cs
@@ -78,11 +78,13 @@ namespace VPWStudio.Editors.VPW2
 		private void btnOK_Click(object sender, EventArgs e)
 		{
 			DialogResult = DialogResult.OK;
+			Close();
 		}
 
 		private void btnCancel_Click(object sender, EventArgs e)
 		{
 			DialogResult = DialogResult.Cancel;
+			Close();
 		}
 	}
 }
