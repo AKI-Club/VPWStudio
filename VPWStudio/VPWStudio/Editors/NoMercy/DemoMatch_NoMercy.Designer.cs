454ba2037f93cfc606b736f37f3764111bf04d0b
freem <aj187@ajworld.net>
2022-09-25 21:48:59 -0500

    add demo match "editor" for No Mercy. like the VPW2 one, you can't edit anything yet

---
+++ VPWStudio/VPWStudio/Editors/NoMercy/DemoMatch_NoMercy.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace VPWStudio.Editors.NoMercy
+{
+	partial class DemoMatch_NoMercy
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOK = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.cbDemoMatches = new System.Windows.Forms.ComboBox();
+			this.tbWrestler1 = new System.Windows.Forms.TextBox();
+			this.tbWrestler2 = new System.Windows.Forms.TextBox();
+			this.tbWrestler3 = new System.Windows.Forms.TextBox();
+			this.tbWrestler4 = new System.Windows.Forms.TextBox();
+			this.tbArena = new System.Windows.Forms.TextBox();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnOK
+			// 
+			this.btnOK.Location = new System.Drawing.Point(296, 193);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 0;
+			this.btnOK.Text = "&OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(377, 193);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 1;
+			this.btnCancel.Text = "&Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 80F));
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.cbDemoMatches, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(440, 35);
+			this.tableLayoutPanel1.TabIndex = 2;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 2;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
+			this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+			this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+			this.tableLayoutPanel2.Controls.Add(this.label4, 0, 2);
+			this.tableLayoutPanel2.Controls.Add(this.label5, 0, 3);
+			this.tableLayoutPanel2.Controls.Add(this.label6, 0, 4);
+			this.tableLayoutPanel2.Controls.Add(this.tbWrestler1, 1, 0);
+			this.tableLayoutPanel2.Controls.Add(this.tbWrestler2, 1, 1);
+			this.tableLayoutPanel2.Controls.Add(this.tbWrestler3, 1, 2);
+			this.tableLayoutPanel2.Controls.Add(this.tbWrestler4, 1, 3);
+			this.tableLayoutPanel2.Controls.Add(this.tbArena, 1, 4);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 53);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 5;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(440, 134);
+			this.tableLayoutPanel2.TabIndex = 3;
+			// 
+			// label1
+			// 
+			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(82, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Demo &Match";
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 6);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(104, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "Wrestler 1";
+			// 
+			// label3
+			// 
+			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(104, 13);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "Wrestler 2";
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(3, 58);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(104, 13);
+			this.label4.TabIndex = 2;
+			this.label4.Text = "Wrestler 3";
+			// 
+			// label5
+			// 
+			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(3, 84);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(104, 13);
+			this.label5.TabIndex = 3;
+			this.label5.Text = "Wrestler 4";
+			// 
+			// label6
+			// 
+			this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(3, 112);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(104, 13);
+			this.label6.TabIndex = 4;
+			this.label6.Text = "Arena and more";
+			// 
+			// cbDemoMatches
+			// 
+			this.cbDemoMatches.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbDemoMatches.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbDemoMatches.FormattingEnabled = true;
+			this.cbDemoMatches.Location = new System.Drawing.Point(91, 7);
+			this.cbDemoMatches.Name = "cbDemoMatches";
+			this.cbDemoMatches.Size = new System.Drawing.Size(346, 21);
+			this.cbDemoMatches.TabIndex = 1;
+			this.cbDemoMatches.SelectedIndexChanged += new System.EventHandler(this.cbDemoMatches_SelectedIndexChanged);
+			// 
+			// tbWrestler1
+			// 
+			this.tbWrestler1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestler1.Location = new System.Drawing.Point(113, 3);
+			this.tbWrestler1.Name = "tbWrestler1";
+			this.tbWrestler1.ReadOnly = true;
+			this.tbWrestler1.Size = new System.Drawing.Size(324, 20);
+			this.tbWrestler1.TabIndex = 5;
+			// 
+			// tbWrestler2
+			// 
+			this.tbWrestler2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestler2.Location = new System.Drawing.Point(113, 29);
+			this.tbWrestler2.Name = "tbWrestler2";
+			this.tbWrestler2.ReadOnly = true;
+			this.tbWrestler2.Size = new System.Drawing.Size(324, 20);
+			this.tbWrestler2.TabIndex = 6;
+			// 
+			// tbWrestler3
+			// 
+			this.tbWrestler3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestler3.Location = new System.Drawing.Point(113, 55);
+			this.tbWrestler3.Name = "tbWrestler3";
+			this.tbWrestler3.ReadOnly = true;
+			this.tbWrestler3.Size = new System.Drawing.Size(324, 20);
+			this.tbWrestler3.TabIndex = 7;
+			// 
+			// tbWrestler4
+			// 
+			this.tbWrestler4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbWrestler4.Location = new System.Drawing.Point(113, 81);
+			this.tbWrestler4.Name = "tbWrestler4";
+			this.tbWrestler4.ReadOnly = true;
+			this.tbWrestler4.Size = new System.Drawing.Size(324, 20);
+			this.tbWrestler4.TabIndex = 8;
+			// 
+			// tbArena
+			// 
+			this.tbArena.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbArena.Location = new System.Drawing.Point(113, 109);
+			this.tbArena.Name = "tbArena";
+			this.tbArena.ReadOnly = true;
+			this.tbArena.Size = new System.Drawing.Size(324, 20);
+			this.tbArena.TabIndex = 9;
+			// 
+			// DemoMatch_NoMercy
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(464, 228);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnOK);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(480, 267);
+			this.Name = "DemoMatch_NoMercy";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Demo Match Editor";
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox cbDemoMatches;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox tbWrestler1;
+		private System.Windows.Forms.TextBox tbWrestler2;
+		private System.Windows.Forms.TextBox tbWrestler3;
+		private System.Windows.Forms.TextBox tbWrestler4;
+		private System.Windows.Forms.TextBox tbArena;
+	}
+}
---
+++ VPWStudio/VPWStudio/Editors/NoMercy/DemoMatch_NoMercy.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio.Editors.NoMercy
+{
+	public partial class DemoMatch_NoMercy : Form
+	{
+		private int NUM_MATCHES = 33;
+		private int BYTES_PER_MATCH = 10;
+
+		public byte[] DemoMatchData;
+
+		public DemoMatch_NoMercy()
+		{
+			InitializeComponent();
+
+			DemoMatchData = new byte[NUM_MATCHES * BYTES_PER_MATCH];
+
+			MemoryStream romStream = new MemoryStream(Program.CurrentInputROM.Data);
+			BinaryReader romReader = new BinaryReader(romStream);
+
+			bool hasLocation = false;
+			if (Program.CurLocationFile != null)
+			{
+				LocationFileEntry sdEntry = Program.CurLocationFile.GetEntryFromComment(LocationFile.SpecialEntryStrings["AttractModeDemoMatches"]);
+				if (sdEntry != null)
+				{
+					romReader.BaseStream.Seek(sdEntry.Address, SeekOrigin.Begin);
+					hasLocation = true;
+				}
+			}
+			if (!hasLocation)
+			{
+				// fallback to hardcoded offset
+				Program.InfoMessageBox("Attract Mode Demo Match location not found; using hardcoded offset instead.");
+				romReader.BaseStream.Seek(DefaultGameData.DefaultLocations[SpecificGame.VPW2_NTSC_J].Locations["AttractModeDemoMatches"].Offset, SeekOrigin.Begin);
+			}
+
+			DemoMatchData = romReader.ReadBytes(NUM_MATCHES * BYTES_PER_MATCH);
+
+			romReader.Close();
+			PopulateMatchList();
+			cbDemoMatches.SelectedIndex = 0;
+		}
+
+		private void PopulateMatchList()
+		{
+			cbDemoMatches.BeginUpdate();
+			for (int i = 0; i < NUM_MATCHES; i++)
+			{
+				cbDemoMatches.Items.Add(String.Format("Match {0}", i + 1));
+			}
+			cbDemoMatches.EndUpdate();
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.OK;
+			Close();
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+			Close();
+		}
+
+		private void cbDemoMatches_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cbDemoMatches.SelectedIndex < 0)
+			{
+				return;
+			}
+
+			short wrestler1 = (short)((DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH)])<<8 | DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 1]);
+			short wrestler2 = (short)((DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 2])<<8 | DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 3]);
+			short wrestler3 = (short)((DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 4])<<8 | DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 5]);
+			short wrestler4 = (short)((DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 6])<<8 | DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 7]);
+			short arena = (short)((DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 8])<<8 | DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 9]);
+
+			tbWrestler1.Text = String.Format("0x{0:X4} (ID4 {1:X4}, costume {2})", wrestler1, (wrestler1&0xFFF), (wrestler1&0xF000)>>12);
+			tbWrestler2.Text = String.Format("0x{0:X4} (ID4 {1:X4}, costume {2})", wrestler2, (wrestler2 & 0xFFF), (wrestler2 & 0xF000) >> 12);
+			tbWrestler3.Text = String.Format("0x{0:X4} (ID4 {1:X4}, costume {2})", wrestler3, (wrestler3 & 0xFFF), (wrestler3 & 0xF000) >> 12);
+			tbWrestler4.Text = String.Format("0x{0:X4} (ID4 {1:X4}, costume {2})", wrestler4, (wrestler4 & 0xFFF), (wrestler4 & 0xF000) >> 12);
+			tbArena.Text = String.Format("0x{0:X4}", arena);
+		}
+	}
+}
