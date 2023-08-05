e9af34b5f711670a7c7c37c9972c2361b6351635
freem <aj187@ajworld.net>
2023-08-05 16:09:57 -0500

    [Move Damage Test Dialog] Include "0x" at beginning of all hex numbers.

--- VPWStudio/VPWStudio/Tools/MoveDamageTestDialog.cs
+++ VPWStudio/VPWStudio/Tools/MoveDamageTestDialog.cs
@@ -43,6 +43,7 @@ namespace VPWStudio
 				{
 					LoadDamageData();
 					PopulateEntries();
+					cbMoveDamageEntries.SelectedIndex = 0;
 				}
 				else
 				{
@@ -120,46 +121,46 @@ namespace VPWStudio
 			StringBuilder sb = new StringBuilder();
 
 			// A4AE0
-			sb.AppendLine(String.Format("+00: {0:X2}{1:X2}", mde.Data[0], mde.Data[1]));
+			sb.AppendLine(String.Format("+00: 0x{0:X2}{1:X2}", mde.Data[0], mde.Data[1]));
 			// A4AE2
-			sb.AppendLine(String.Format("+02: Flying Move Target Body Part {0:X2}{1:X2}", mde.Data[2], mde.Data[3]));
+			sb.AppendLine(String.Format("+02: Flying Move Target Body Part 0x{0:X2}{1:X2}", mde.Data[2], mde.Data[3]));
 			// A4AE4(Link)
-			sb.AppendLine(String.Format("+04: Link {0:X2}{1:X2}", mde.Data[4], mde.Data[5]));
+			sb.AppendLine(String.Format("+04: Link 0x{0:X2}{1:X2}", mde.Data[4], mde.Data[5]));
 			// A4AE6(Damage / Spirit Gained)
-			sb.AppendLine(String.Format("+06: Damage {0:X2}", mde.Data[6]));
-			sb.AppendLine(String.Format("+07: Spirit Gain {0:X2}", mde.Data[7]));
+			sb.AppendLine(String.Format("+06: Damage 0x{0:X2}", mde.Data[6]));
+			sb.AppendLine(String.Format("+07: Spirit Gain 0x{0:X2}", mde.Data[7]));
 			// A4AE8(Spirit Drained / Blood)
-			sb.AppendLine(String.Format("+08: Spirit Drain {0:X2} ({1})", mde.Data[8], (sbyte)mde.Data[8]));
-			sb.AppendLine(String.Format("+09: Blood Chance {0:X2}", mde.Data[9]));
+			sb.AppendLine(String.Format("+08: Spirit Drain 0x{0:X2} ({1})", mde.Data[8], (sbyte)mde.Data[8]));
+			sb.AppendLine(String.Format("+09: Blood Chance 0x{0:X2}", mde.Data[9]));
 			// A4AEA(KO / Off.Parameter)
-			sb.AppendLine(String.Format("+0A: KO Chance {0:X2}", mde.Data[10]));
-			sb.AppendLine(String.Format("+0B: Offensive Param. {0:X2} ({1})", mde.Data[11], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[11])));
+			sb.AppendLine(String.Format("+0A: KO Chance 0x{0:X2}", mde.Data[10]));
+			sb.AppendLine(String.Format("+0B: Offensive Param. 0x{0:X2} ({1})", mde.Data[11], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[11])));
 			// A4AEC(Def.Parameter / Attack With)
-			sb.AppendLine(String.Format("+0C: Defensive Param. {0:X2} ({1})", mde.Data[12], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[12])));
-			sb.AppendLine(String.Format("+0D: Attack With {0:X2}", mde.Data[13]));
+			sb.AppendLine(String.Format("+0C: Defensive Param. 0x{0:X2} ({1})", mde.Data[12], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[12])));
+			sb.AppendLine(String.Format("+0D: Attack With 0x{0:X2}", mde.Data[13]));
 			// A4AEE
-			sb.AppendLine(String.Format("+0E: (more body part target) {0:X2}", mde.Data[14]));
-			sb.AppendLine(String.Format("+0F: (more body part target) {0:X2}", mde.Data[15]));
+			sb.AppendLine(String.Format("+0E: (more body part target) 0x{0:X2}", mde.Data[14]));
+			sb.AppendLine(String.Format("+0F: (more body part target) 0x{0:X2}", mde.Data[15]));
 			// A4AF0(Attack to /)
-			sb.AppendLine(String.Format("+10: Attack To {0:X2}", mde.Data[16]));
-			sb.AppendLine(String.Format("+11: Strike Reaction 1? {0:X2}", mde.Data[17]));
+			sb.AppendLine(String.Format("+10: Attack To 0x{0:X2}", mde.Data[16]));
+			sb.AppendLine(String.Format("+11: Strike Reaction 1? 0x{0:X2}", mde.Data[17]));
 			// A4AF2(Head Damage / Body Damage)
-			sb.AppendLine(String.Format("+12: Head Damage {0:X2}", mde.Data[18]));
-			sb.AppendLine(String.Format("+13: Body Damage {0:X2}", mde.Data[19]));
+			sb.AppendLine(String.Format("+12: Head Damage 0x{0:X2}", mde.Data[18]));
+			sb.AppendLine(String.Format("+13: Body Damage 0x{0:X2}", mde.Data[19]));
 			// A4AF4(Arm Damage / Leg Damage)
-			sb.AppendLine(String.Format("+14: Arm Damage {0:X2}", mde.Data[20]));
-			sb.AppendLine(String.Format("+15: Leg Damage {0:X2}", mde.Data[21]));
+			sb.AppendLine(String.Format("+14: Arm Damage 0x{0:X2}", mde.Data[20]));
+			sb.AppendLine(String.Format("+15: Leg Damage 0x{0:X2}", mde.Data[21]));
 			// A4AF6(Speed Damage / Sell)
-			sb.AppendLine(String.Format("+16: Speed/Flying Damage {0:X2}", mde.Data[22]));
-			sb.AppendLine(String.Format("+17: Strike Reaction 2? {0:X2}", mde.Data[23]));
+			sb.AppendLine(String.Format("+16: Speed/Flying Damage 0x{0:X2}", mde.Data[22]));
+			sb.AppendLine(String.Format("+17: Strike Reaction 2? 0x{0:X2}", mde.Data[23]));
 			// A4AF8
-			sb.AppendLine(String.Format("+18: Missed Attack Spirit Drain {0:X2}", mde.Data[24]));
-			sb.AppendLine(String.Format("+19: {0:X2}", mde.Data[25]));
+			sb.AppendLine(String.Format("+18: Missed Attack Spirit Drain 0x{0:X2}", mde.Data[24]));
+			sb.AppendLine(String.Format("+19: 0x{0:X2}", mde.Data[25]));
 			// A4AFA / ?
-			sb.AppendLine(String.Format("+1A: {0:X2}", mde.Data[26]));
-			sb.AppendLine(String.Format("+1B: unused?? {0:X2}", mde.Data[27]));
+			sb.AppendLine(String.Format("+1A: 0x{0:X2}", mde.Data[26]));
+			sb.AppendLine(String.Format("+1B: unused?? 0x{0:X2}", mde.Data[27]));
 			// A4AFC
-			sb.AppendLine(String.Format("+1C: {0:X2}{1:X2}{2:X2}{3:X2}", mde.Data[28], mde.Data[29], mde.Data[30], mde.Data[31]));
+			sb.AppendLine(String.Format("+1C: 0x{0:X2}{1:X2}{2:X2}{3:X2}", mde.Data[28], mde.Data[29], mde.Data[30], mde.Data[31]));
 
 			tbOutput.Text = sb.ToString();
 		}
@@ -170,39 +171,39 @@ namespace VPWStudio
 			StringBuilder sb = new StringBuilder();
 
 			// mostly like VPW2 but with new entries
-			sb.AppendLine(String.Format("+00: {0:X2}{1:X2}", mde.Data[0], mde.Data[1]));
-			sb.AppendLine(String.Format("+02: Flying Move Target Body Part {0:X2}{1:X2}", mde.Data[2], mde.Data[3]));
-			sb.AppendLine(String.Format("+04: Link {0:X2}{1:X2}", mde.Data[4], mde.Data[5]));
-			sb.AppendLine(String.Format("+06: Damage {0:X2}", mde.Data[6]));
-			sb.AppendLine(String.Format("+07: Spirit Gain {0:X2}", mde.Data[7]));
-			sb.AppendLine(String.Format("+08: Spirit Drain {0:X2} ({1})", mde.Data[8], (sbyte)mde.Data[8]));
-			sb.AppendLine(String.Format("+09: Blood Chance {0:X2}", mde.Data[9]));
-			sb.AppendLine(String.Format("+0A: KO Chance {0:X2}", mde.Data[10]));
-			sb.AppendLine(String.Format("+0B: Offensive Param. {0:X2} ({1})", mde.Data[11], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[11])));
-			sb.AppendLine(String.Format("+0C: Defensive Param. {0:X2} ({1})", mde.Data[12], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[12])));
-			sb.AppendLine(String.Format("+0D: Attack With {0:X2}", mde.Data[13]));
-			sb.AppendLine(String.Format("+0E: {0:X2}", mde.Data[14]));
-			sb.AppendLine(String.Format("+0F: {0:X2}", mde.Data[15]));
-			sb.AppendLine(String.Format("+10: Attack To {0:X2}", mde.Data[16]));
-			sb.AppendLine(String.Format("+11: {0:X2}", mde.Data[17]));
-			sb.AppendLine(String.Format("+12: Head Damage {0:X2}", mde.Data[18]));
-			sb.AppendLine(String.Format("+13: Body Damage {0:X2}", mde.Data[19]));
-			sb.AppendLine(String.Format("+14: Arm Damage {0:X2}", mde.Data[20]));
-			sb.AppendLine(String.Format("+15: Leg Damage {0:X2}", mde.Data[21]));
-			sb.AppendLine(String.Format("+16: Speed/Flying Damage {0:X2}", mde.Data[22]));
-			sb.AppendLine(String.Format("+17: Strike Reaction {0:X2}", mde.Data[23]));
-			sb.AppendLine(String.Format("+18: {0:X2}", mde.Data[24]));
-			sb.AppendLine(String.Format("+19: {0:X2}", mde.Data[25]));
-			sb.AppendLine(String.Format("+1A: {0:X2}", mde.Data[26]));
-			sb.AppendLine(String.Format("+1B: Special Damage {0:X2}", mde.Data[27]));
-			sb.AppendLine(String.Format("+1C: Previous Move {0:X2}", mde.Data[28]));
-			sb.AppendLine(String.Format("+1D: {0:X2}", mde.Data[29]));
-			sb.AppendLine(String.Format("+1E: {0:X2}", mde.Data[30]));
-			sb.AppendLine(String.Format("+1F: {0:X2}", mde.Data[31]));
-			sb.AppendLine(String.Format("+20: Move Type {0:X2}", mde.Data[32]));
-			sb.AppendLine(String.Format("+21: Additional Properties {0:X2}", mde.Data[33]));
-			sb.AppendLine(String.Format("+22: {0:X2}", mde.Data[34]));
-			sb.AppendLine(String.Format("+23: {0:X2}", mde.Data[35]));
+			sb.AppendLine(String.Format("+00: 0x{0:X2}{1:X2}", mde.Data[0], mde.Data[1]));
+			sb.AppendLine(String.Format("+02: Flying Move Target Body Part 0x{0:X2}{1:X2}", mde.Data[2], mde.Data[3]));
+			sb.AppendLine(String.Format("+04: Link 0x{0:X2}{1:X2}", mde.Data[4], mde.Data[5]));
+			sb.AppendLine(String.Format("+06: Damage 0x{0:X2}", mde.Data[6]));
+			sb.AppendLine(String.Format("+07: Spirit Gain 0x{0:X2}", mde.Data[7]));
+			sb.AppendLine(String.Format("+08: Spirit Drain 0x{0:X2} ({1})", mde.Data[8], (sbyte)mde.Data[8]));
+			sb.AppendLine(String.Format("+09: Blood Chance 0x{0:X2}", mde.Data[9]));
+			sb.AppendLine(String.Format("+0A: KO Chance 0x{0:X2}", mde.Data[10]));
+			sb.AppendLine(String.Format("+0B: Offensive Param. 0x{0:X2} ({1})", mde.Data[11], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[11])));
+			sb.AppendLine(String.Format("+0C: Defensive Param. 0x{0:X2} ({1})", mde.Data[12], Enum.GetName(typeof(DamageParamBodyTypes), mde.Data[12])));
+			sb.AppendLine(String.Format("+0D: Attack With 0x{0:X2}", mde.Data[13]));
+			sb.AppendLine(String.Format("+0E: 0x{0:X2}", mde.Data[14]));
+			sb.AppendLine(String.Format("+0F: 0x{0:X2}", mde.Data[15]));
+			sb.AppendLine(String.Format("+10: Attack To 0x{0:X2}", mde.Data[16]));
+			sb.AppendLine(String.Format("+11: 0x{0:X2}", mde.Data[17]));
+			sb.AppendLine(String.Format("+12: Head Damage 0x{0:X2}", mde.Data[18]));
+			sb.AppendLine(String.Format("+13: Body Damage 0x{0:X2}", mde.Data[19]));
+			sb.AppendLine(String.Format("+14: Arm Damage 0x{0:X2}", mde.Data[20]));
+			sb.AppendLine(String.Format("+15: Leg Damage 0x{0:X2}", mde.Data[21]));
+			sb.AppendLine(String.Format("+16: Speed/Flying Damage 0x{0:X2}", mde.Data[22]));
+			sb.AppendLine(String.Format("+17: Strike Reaction 0x{0:X2}", mde.Data[23]));
+			sb.AppendLine(String.Format("+18: 0x{0:X2}", mde.Data[24]));
+			sb.AppendLine(String.Format("+19: 0x{0:X2}", mde.Data[25]));
+			sb.AppendLine(String.Format("+1A: 0x{0:X2}", mde.Data[26]));
+			sb.AppendLine(String.Format("+1B: Special Damage 0x{0:X2}", mde.Data[27]));
+			sb.AppendLine(String.Format("+1C: Previous Move 0x{0:X2}", mde.Data[28]));
+			sb.AppendLine(String.Format("+1D: 0x{0:X2}", mde.Data[29]));
+			sb.AppendLine(String.Format("+1E: 0x{0:X2}", mde.Data[30]));
+			sb.AppendLine(String.Format("+1F: 0x{0:X2}", mde.Data[31]));
+			sb.AppendLine(String.Format("+20: Move Type 0x{0:X2}", mde.Data[32]));
+			sb.AppendLine(String.Format("+21: Additional Properties 0x{0:X2}", mde.Data[33]));
+			sb.AppendLine(String.Format("+22: 0x{0:X2}", mde.Data[34]));
+			sb.AppendLine(String.Format("+23: 0x{0:X2}", mde.Data[35]));
 
 			tbOutput.Text = sb.ToString();
 		}
