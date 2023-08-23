31d14de42dd6e11e27ba28340d8edd6622dc909a
freem <aj187@ajworld.net>
2023-08-23 15:27:09 -0500

    VERY early no mercy story mode dialog. don't expect anything

---
+++ VPWStudio/VPWStudio/Editors/NoMercy/StoryMode_NoMercy.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace VPWStudio.Editors.NoMercy
+{
+	partial class StoryMode_NoMercy
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.cbStoryPath = new System.Windows.Forms.ComboBox();
+			this.lblStoryPath = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// cbStoryPath
+			// 
+			this.cbStoryPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.cbStoryPath.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbStoryPath.FormattingEnabled = true;
+			this.cbStoryPath.Items.AddRange(new object[] {
+            "World Heavyweight",
+            "Tag Team",
+            "Intercontinental",
+            "European",
+            "Hardcore",
+            "Light Heavyweight",
+            "Women\'s",
+            "No Mercy GBC"});
+			this.cbStoryPath.Location = new System.Drawing.Point(74, 12);
+			this.cbStoryPath.Name = "cbStoryPath";
+			this.cbStoryPath.Size = new System.Drawing.Size(538, 21);
+			this.cbStoryPath.TabIndex = 0;
+			this.cbStoryPath.SelectedIndexChanged += new System.EventHandler(this.cbStoryPath_SelectedIndexChanged);
+			// 
+			// lblStoryPath
+			// 
+			this.lblStoryPath.AutoSize = true;
+			this.lblStoryPath.Location = new System.Drawing.Point(12, 15);
+			this.lblStoryPath.Name = "lblStoryPath";
+			this.lblStoryPath.Size = new System.Drawing.Size(56, 13);
+			this.lblStoryPath.TabIndex = 1;
+			this.lblStoryPath.Text = "Story &Path";
+			// 
+			// StoryMode_NoMercy
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(624, 361);
+			this.Controls.Add(this.lblStoryPath);
+			this.Controls.Add(this.cbStoryPath);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MinimizeBox = false;
+			this.Name = "StoryMode_NoMercy";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "No Mercy Story Mode";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox cbStoryPath;
+		private System.Windows.Forms.Label lblStoryPath;
+	}
+}
---
+++ VPWStudio/VPWStudio/Editors/NoMercy/StoryMode_NoMercy.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VPWStudio.Editors.NoMercy
+{
+	public partial class StoryMode_NoMercy : Form
+	{
+		// todo: figure out where these file IDs are defined, and if possible, read them from ROM
+
+		// chapter titles file ID 01DD
+
+		// match information file IDs
+		/*
+		 * 01DE - World Heavyweight
+		 * 01DF - Tag Team
+		 * 01E0 - Intercontinental
+		 * 01E1 - European
+		 * 01E2 - Hardcore
+		 * 01E3 - Light Heavyweight
+		 * 01E4 - Women's
+		 * 01E5 - No Mercy GBC
+		 */
+
+		// dialog file IDs
+		/*
+		 * 01E6 - World Heavyweight
+		 * 01E7 - Tag Team
+		 * 01E8 - Intercontinental
+		 * 01E9 - European
+		 * 01EA - Hardcore
+		 * 01EB - Light Heavyweight
+		 * 01EC - Women's
+		 * 01ED - No Mercy GBC
+		 */
+
+		public StoryMode_NoMercy()
+		{
+			InitializeComponent();
+			cbStoryPath.SelectedIndex = 0;
+
+			// todo: get names from file ID 01DC, entries 12-19, instead of assuming them
+		}
+
+		private void cbStoryPath_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if (cbStoryPath.SelectedIndex >= 0)
+			{
+			}
+		}
+	}
+}
