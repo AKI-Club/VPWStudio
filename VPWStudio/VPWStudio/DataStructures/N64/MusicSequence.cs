673dd696c6212804944e82d443fde14a2cf4dd56
freem <aj187@ajworld.net>
2023-09-14 20:19:52 -0500

    very very very early, ignore me

---
+++ VPWStudio/VPWStudio/DataStructures/N64/MusicSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPWStudio
+{
+	public enum MusicSequenceType
+	{
+		// todo: figure out World Tour and VPW64; songs are not in the filetable
+		OldSng, // WCW/nWo Revenge
+		Binary  // WM2K and later; files start with 0x00,0x00,0x02,0x15
+	};
+
+	public class MusicSequence
+	{
+		public MusicSequenceType SeqType;
+
+		public MusicSequence()
+		{
+		}
+	}
+}
