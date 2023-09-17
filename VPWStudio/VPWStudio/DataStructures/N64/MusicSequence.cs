0e1a07a0ab73b503f2c1c3ec0d3ece84b98ebb15
freem <aj187@ajworld.net>
2023-09-17 12:48:32 -0500

    [MusicSequence] add "unknown" type, used for fallback until I figure out how World Tour and VPW64 should be handled

--- VPWStudio/VPWStudio/DataStructures/N64/MusicSequence.cs
+++ VPWStudio/VPWStudio/DataStructures/N64/MusicSequence.cs
@@ -10,15 +10,20 @@ namespace VPWStudio
 	{
 		// todo: figure out World Tour and VPW64; songs are not in the filetable
 		OldSng, // WCW/nWo Revenge
-		Binary  // WM2K and later; files start with 0x00,0x00,0x02,0x15
+		Binary,  // WM2K and later; files start with 0x00,0x00,0x02,0x15
+		Unknown // fallback
 	};
 
 	public class MusicSequence
 	{
 		public MusicSequenceType SeqType;
 
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
 		public MusicSequence()
 		{
+			SeqType = MusicSequenceType.Unknown;
 		}
 	}
 }
