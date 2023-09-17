dd2c7e4e777a2dc22a9f8fe60de2958283a1e7cf
freem <aj187@ajworld.net>
2023-09-16 23:43:27 -0500

    [VPW2 Demo Match Dialog] Add wrestler names

--- VPWStudio/VPWStudio/Editors/VPW2/DemoMatch_VPW2.cs
+++ VPWStudio/VPWStudio/Editors/VPW2/DemoMatch_VPW2.cs
@@ -12,8 +12,14 @@ namespace VPWStudio.Editors.VPW2
 {
 	public partial class DemoMatch_VPW2 : Form
 	{
-		private int NUM_MATCHES = 38;
-		private int BYTES_PER_MATCH = 5;
+		private readonly int NUM_MATCHES = 38;
+
+		private readonly int BYTES_PER_MATCH = 5;
+
+		/// <summary>
+		/// Default wrestler names.
+		/// </summary>
+		private AkiText DefaultNames;
 
 		public byte[] DemoMatchData;
 
@@ -25,6 +31,29 @@ namespace VPWStudio.Editors.VPW2
 
 			MemoryStream romStream = new MemoryStream(Program.CurrentInputROM.Data);
 			BinaryReader romReader = new BinaryReader(romStream);
+
+			// load wrestler names
+			FileTableEntry defWrestlerNames = Program.CurrentProject.ProjectFileTable.Entries[0x006C];
+			if (!String.IsNullOrEmpty(defWrestlerNames.ReplaceFilePath))
+			{
+				FileStream fs = new FileStream(Program.ConvertRelativePath(defWrestlerNames.ReplaceFilePath), FileMode.Open);
+				BinaryReader br = new BinaryReader(fs);
+				DefaultNames = new AkiText(br);
+				br.Close();
+			}
+			else
+			{
+				MemoryStream outStream = new MemoryStream();
+				BinaryWriter outWriter = new BinaryWriter(outStream);
+
+				Program.CurrentProject.ProjectFileTable.ExtractFile(romReader, outWriter, 0x006C);
+
+				outStream.Seek(0, SeekOrigin.Begin);
+				BinaryReader outReader = new BinaryReader(outStream);
+				DefaultNames = new AkiText(outReader);
+				outReader.Close();
+				outWriter.Close();
+			}
 
 			bool hasLocation = false;
 			if (Program.CurLocationFile != null)
@@ -67,10 +96,14 @@ namespace VPWStudio.Editors.VPW2
 				return;
 			}
 
-			tbWrestler1.Text = String.Format("0x{0:X2}", DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH)]);
-			tbWrestler2.Text = String.Format("0x{0:X2}", DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH)+1]);
-			tbWrestler3.Text = String.Format("0x{0:X2}", DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH)+2]);
-			tbWrestler4.Text = String.Format("0x{0:X2}", DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH)+3]);
+			byte wres1 = DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH)];
+			byte wres2 = DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 1];
+			byte wres3 = DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 2];
+			byte wres4 = DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 3];
+			tbWrestler1.Text = String.Format("0x{0:X2} {1}", wres1, wres1 != 0 ? DefaultNames.Entries[wres1 * 2].Text : String.Empty);
+			tbWrestler2.Text = String.Format("0x{0:X2} {1}", wres2, wres2 != 0 ? DefaultNames.Entries[wres2 * 2].Text : String.Empty);
+			tbWrestler3.Text = String.Format("0x{0:X2} {1}", wres3, wres3 != 0 ? DefaultNames.Entries[wres3 * 2].Text : String.Empty);
+			tbWrestler4.Text = String.Format("0x{0:X2} {1}", wres4, wres4 != 0 ? DefaultNames.Entries[wres4 * 2].Text : String.Empty);
 			byte arenaRules = DemoMatchData[(cbDemoMatches.SelectedIndex * BYTES_PER_MATCH) + 4];
 			tbRuleset.Text = String.Format("0x{0:X2} (arena {1}, ruleset {2})", arenaRules, (arenaRules&0xF0)>>4, (arenaRules&0x0F)|0x10);
 		}
